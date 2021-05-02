914825f8b40df66930d2932b2dd13befa1900ea4
ANFINII <anfinii19870506@gmail.com>
2021-05-02 19:13:45 +0900

    いいね機能(非同期)実装

---
+++ static/js/ajax_like.js
@@ -0,0 +1,33 @@
+// いいねボタンクリック時の処理を定義
+$(document).ready(function() {
+    $(document).on('click', '.like_form', function(event) {
+        event.preventDefault();
+        let pk = $(this).attr('value');
+        let url = $(this).parent().attr('action');
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {'videomodel_id': pk, 'csrfmiddlewaretoken': '{{ csrf_token }}'},
+            dataType: 'json',
+        })
+        .done(function(response) {
+            if (response['liked']) {
+                $('.like_color').removeClass('bi-hand-thumbs-up');
+                $('.like_color').parent().removeClass('like_no');
+                $('.like_color').addClass('bi-hand-thumbs-up-fill')
+                $('.like_color').parent().addClass('like_fill');
+                $('.like_count').html(response['total_like'])
+            } else {
+                $('.like_color').removeClass('bi-hand-thumbs-up-fill');
+                $('.like_color').parent().removeClass('like_fill');
+                $('.like_color').addClass('bi-hand-thumbs-up');
+                $('.like_color').parent().addClass('like_no');
+                $('.like_count').html(response['total_like'])
+            }
+            console.log(response)
+        })
+        .fail(function(response) {
+            console.log(response);
+        })
+    });
+});
