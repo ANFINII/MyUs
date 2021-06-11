0b3653edda3212ce37223b735ba97f9e41ff792c
ANFINII <anfinii19870506@gmail.com>
2021-06-11 23:42:59 +0900

    コード整理

--- static/js/textarea.js
+++ static/js/textarea.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     const target = $('.textarea');
     const rawTarget = target.get(0);
     let lineHeight = Number(target.attr('rows'));
@@ -8,16 +8,16 @@ $(function () {
     }
 });
 
-$(function () {
+$(function() {
     const $textarea = $('.textarea');
     const lineHeight = parseInt($textarea.css('lineHeight'));
-    $textarea.on('input', function (evt) {
+    $textarea.on('input', function(evt) {
         const lines = ($(this).val() + '\n').match(/\n/g).length;
         $(this).height(lineHeight * lines);
     });
 });
 
-$(function () {
+$(function() {
     const target = $('.textarea_br');
     const rawTarget = target.get(0);
     let lineHeight = Number(target.attr('rows'));
@@ -27,10 +27,10 @@ $(function () {
     }
 });
 
-$(function () {
+$(function() {
     const $textarea = $('.textarea_br');
     const lineHeight = parseInt($textarea.css('lineHeight'));
-    $textarea.on('input', function (evt) {
+    $textarea.on('input', function(evt) {
         const lines = ($(this).val() + '\n').match(/\n/g).length;
         $(this).height(lineHeight * lines);
     });
