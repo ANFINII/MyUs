d5b3985946f429d0cf74367b54f2f02b86b40197
ANFINII <anfinii19870506@email.com>
2021-06-26 16:40:52 +0900

    views, urls コード整理

--- myusapp/urls.py
+++ myusapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import Index, Recommend, searchtag_create, like_form, comment_form, reply_form
-from .views import signup_form, login_form, logout_form, userpolicy, knowledge, Withdrawal
-from .views import profile, ProfileUpdate, mypage, MyPageUpdate, UserPage
-from .views import FollowList, FollowerList, follow_create
+from .views import Index, Recommend, UserPage
+from .views import profile, ProfileUpdate, mypage, MyPageUpdate
+from .views import FollowList, FollowerList, follow_create, userpolicy, knowledge
 from .views import VideoList, VideoCreate, VideoDetail
 from .views import LiveList, LiveCreate, LiveDetail
 from .views import MusicList, MusicCreate, MusicDetail
@@ -11,22 +10,20 @@ from .views import BlogList, BlogCreate, BlogDetail
 from .views import ChatList, ChatCreate, ChatDetail, chat_message, chat_reply, chat_thread
 from .views import CollaboList, CollaboCreate, CollaboDetail
 from .views import TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
+from .views import searchtag_create, like_form, comment_form, reply_form
+from .views import signup_form, login_form, logout_form, Withdrawal
 
 app_name = 'myus'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('recommend/', Recommend.as_view(), name='recommend'),
-    path('searchtag_create/', searchtag_create, name='searchtag_create'),
-    path('like_form/', like_form, name='like_form'),
-    path('comment_form/', comment_form, name='comment_form'),
-    path('reply_form/', reply_form, name='reply_form'),
-
-    path('signup/', signup_form, name='signup'),
-    path('login/', login_form, name='login'),
-    path('logout/', logout_form, name='logout'),
-    path('withdrawal/', Withdrawal.as_view(), name='withdrawal'),
-    path('withdrawal/<str:token>', Withdrawal.as_view(), name='withdrawal'),
+    path('userpage/<str:nickname>', UserPage.as_view(), name='userpage'),
+    
+    path('profile/', profile, name='profile'),
+    path('profile_update/', ProfileUpdate.as_view(), name='profile_update'),
+    path('mypage/', mypage, name='mypage'),
+    path('mypage_update/', MyPageUpdate.as_view(), name='mypage_update'),
     
     path('follow/', FollowList.as_view(), name='follow_list'),
     path('follower/', FollowerList.as_view(), name='follower_list'),
@@ -34,12 +31,6 @@ urlpatterns = [
     path('userpolicy/', userpolicy, name='userpolicy'),
     path('knowledge/', knowledge, name='knowledge'),
     
-    path('profile/', profile, name='profile'),
-    path('profile_update/', ProfileUpdate.as_view(), name='profile_update'),
-    path('mypage/', mypage, name='mypage'),
-    path('mypage_update/', MyPageUpdate.as_view(), name='mypage_update'),
-    path('userpage/<str:nickname>', UserPage.as_view(), name='userpage'),
-
     path('video/', VideoList.as_view(), name='video_list'),
     path('video_create/', VideoCreate.as_view(), name='video_create'),
     path('video_detail/<int:pk>', VideoDetail.as_view(), name='video_detail'),
@@ -76,4 +67,15 @@ urlpatterns = [
     path('todo_detail/<int:pk>', TodoDetail.as_view(), name='todo_detail'),
     path('todo_update/<int:pk>', TodoUpdate.as_view(), name='todo_update'),
     path('todo_delete/<int:pk>', TodoDelete.as_view(), name='todo_delete'),
+    
+    path('searchtag_create/', searchtag_create, name='searchtag_create'),
+    path('like_form/', like_form, name='like_form'),
+    path('comment_form/', comment_form, name='comment_form'),
+    path('reply_form/', reply_form, name='reply_form'),
+    
+    path('signup/', signup_form, name='signup'),
+    path('login/', login_form, name='login'),
+    path('logout/', logout_form, name='logout'),
+    path('withdrawal/', Withdrawal.as_view(), name='withdrawal'),
+    path('withdrawal/<str:token>', Withdrawal.as_view(), name='withdrawal'),
 ]
