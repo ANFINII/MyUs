c9e07fb41f6b75b05a4c5a61cb2526053636d606
ANFINII <anfinii19870506@email.com>
2021-07-10 00:21:25 +0900

    validationモジュール作成

--- myusapp/views.py
+++ myusapp/views.py
@@ -20,8 +20,8 @@ from itertools import chain, islice
 from .models import SearchTagModel, TagModel, CommentModel, FollowModel, TodoModel, AdvertiseModel
 from .models import VideoModel, LiveModel, MusicModel, PictureModel, BlogModel, ChatModel, CollaboModel
 from .forms import SearchTagForm
+from .modules.validation import has_username, has_email, has_phone, has_alphabet, has_number
 from .modules.search import search_follow, search_models, search_music, search_blog, search_todo
-import re
 import datetime
 import string
 import random
@@ -30,38 +30,6 @@ import json
 # Create your views here.
 
 User = get_user_model()
-
-# バリデーション関数定義
-def has_username(text):
-    if re.fullmatch('^[a-zA-Z0-9_]+$', text) != None:
-        return False
-    return True
-
-def has_email(text):
-    if re.fullmatch('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z.]+$', text) != None:
-        return False
-    return True
-
-def has_phone(text):
-    if re.fullmatch('\d{2,4}-?\d{2,4}-?\d{3,4}', text) != None:
-        return False
-    return True
-
-def has_alphabet(text):
-    if re.search('[a-zA-Z]', text) != None:
-        return True
-    return False
-
-def has_number(text):
-    if re.search('[0-9０-９]', text) != None:
-        return True
-    return False
-
-def has_tag(text):
-    if re.search('.{1,12}', text) != None:
-        return True
-    return False
-
 
 # Signup
 def signup_form(request):
