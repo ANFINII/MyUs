9f3df62b7df3311b58ed5193acd0f502e50e6adb
ANFINII <anfinii19870506@gmail.com>
2021-08-07 23:08:21 +0900

    searchモジュールにコード分割

--- myusapp/modules/search.py
+++ myusapp/modules/search.py
@@ -1,8 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from django.db.models import Q, F, Count
 from functools import reduce
 from operator import and_
+from itertools import chain
+from myusapp.models import VideoModel, LiveModel, MusicModel, PictureModel, BlogModel, ChatModel
+
+User = get_user_model()
 
 class Search:
+    def search_index(self):
+        search = self.request.GET.get('search', None)
+        if search is not None:
+            result1 = VideoModel.objects.search(search)
+            result2 = LiveModel.objects.search(search)
+            result3 = MusicModel.objects.search(search)
+            result4 = PictureModel.objects.search(search)
+            result5 = BlogModel.objects.search(search)
+            result6 = ChatModel.objects.search(search)
+            queryset_chain = chain(result1, result2, result3, result4, result5, result6)
+            result = sorted(queryset_chain, key=lambda instance: instance.score(), reverse=True)
+            self.count = len(result)
+            return result
+        return VideoModel.objects.none()
+
+    def search_userpage(self):
+        search = self.request.GET.get('search', None)
+        author = get_object_or_404(User, nickname=self.kwargs['nickname'])
+        if search is not None:
+            result1 = VideoModel.objects.filter(author_id=author, publish=True).search(search)
+            result2 = LiveModel.objects.filter(author_id=author, publish=True).search(search)
+            result3 = MusicModel.objects.filter(author_id=author, publish=True).search(search)
+            result4 = PictureModel.objects.filter(author_id=author, publish=True).search(search)
+            result5 = BlogModel.objects.filter(author_id=author, publish=True).search(search)
+            result6 = ChatModel.objects.filter(author_id=author, publish=True).search(search)
+            queryset_chain = chain(result1, result2, result3, result4, result5, result6)
+            result = sorted(queryset_chain, key=lambda instance: instance.score(), reverse=True)
+            self.count = len(result)
+            return result
+        return VideoModel.objects.none()
+
     def search_follower(self, model):
         result = model.objects.filter(following_id=self.request.user.id).exclude(follower_id=self.request.user.id)
         search = self.request.GET.get('search')
--- myusapp/views.py
+++ myusapp/views.py
@@ -8,15 +8,12 @@ from django.core.signing import TimestampSigner, SignatureExpired, BadSignature
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect, QueryDict
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
-from django.utils import timezone
 from django.utils.html import urlize as urlize_impl
 from django.db.models import Q, F, Count, Max, Min, Avg, Value
 from django.template.loader import render_to_string
 from django.template.defaultfilters import linebreaksbr, linebreaks
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import View, TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView, FormView
-from dateutil.relativedelta import relativedelta
-from itertools import chain, islice
+from django.views.generic import View, TemplateView, FormView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import SearchTagForm
 from .models import SearchTagModel, TagModel, CommentModel, FollowModel, TodoModel, AdvertiseModel
 from .models import VideoModel, LiveModel, MusicModel, PictureModel, BlogModel, ChatModel, CollaboModel
@@ -500,19 +497,7 @@ class Index(ListView):
         return context
 
     def get_queryset(self):
-        search = self.request.GET.get('search', None)
-        if search is not None:
-            result1 = VideoModel.objects.search(search)
-            result2 = LiveModel.objects.search(search)
-            result3 = MusicModel.objects.search(search)
-            result4 = PictureModel.objects.search(search)
-            result5 = BlogModel.objects.search(search)
-            result6 = ChatModel.objects.search(search)
-            queryset_chain = chain(result1, result2, result3, result4, result5, result6)
-            result = sorted(queryset_chain, key=lambda instance: instance.score(), reverse=True)
-            self.count = len(result)
-            return result
-        return VideoModel.objects.none()
+        return Search.search_index(self)
 
 class Recommend(ListView):
     """急上昇機能、すべてのメディアmodelを表示"""
@@ -567,20 +552,7 @@ class UserPage(ListView):
         return context
 
     def get_queryset(self):
-        search = self.request.GET.get('search', None)
-        author = get_object_or_404(User, nickname=self.kwargs['nickname'])
-        if search is not None:
-            result1 = VideoModel.objects.filter(author_id=author, publish=True).search(search)
-            result2 = LiveModel.objects.filter(author_id=author, publish=True).search(search)
-            result3 = MusicModel.objects.filter(author_id=author, publish=True).search(search)
-            result4 = PictureModel.objects.filter(author_id=author, publish=True).search(search)
-            result5 = BlogModel.objects.filter(author_id=author, publish=True).search(search)
-            result6 = ChatModel.objects.filter(author_id=author, publish=True).search(search)
-            queryset_chain = chain(result1, result2, result3, result4, result5, result6)
-            result = sorted(queryset_chain, key=lambda instance: instance.score(), reverse=True)
-            self.count = len(result)
-            return result
-        return VideoModel.objects.none()
+        return Search.search_userpage(self)
 
 
 # Follow
