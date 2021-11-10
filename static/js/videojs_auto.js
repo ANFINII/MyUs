7d657ea3d0cd3f277a78e6cfa2a1a2599946343d
ANFINII <anfinii19870506@gmail.com>
2021-11-10 09:16:01 +0900

    ホバー再生の仕様変更

--- static/js/videojs_auto.js
+++ static/js/videojs_auto.js
@@ -1,9 +1,11 @@
 let figure = $(".video").hover(hoverVideo, hideVideo);
 
+// ホバーすると再生開始
 function hoverVideo(e) {
     $('video', this).get(0).play();
 }
 
+// ホバーアウトするとサムネイル表示
 function hideVideo(e) {
-    $('video', this).get(0).pause();
+    $('video', this).get(0).load();
 }
