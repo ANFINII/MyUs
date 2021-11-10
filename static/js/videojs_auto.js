41f2a5c1ec315c3dfac5bbfcbccc9233c2d5ab6c
ANFINII <anfinii19870506@gmail.com>
2021-11-10 10:16:14 +0900

    サムネイルホバー自動再生、再生速度を２倍に修正

--- static/js/videojs_auto.js
+++ static/js/videojs_auto.js
@@ -3,6 +3,12 @@ let figure = $(".video").hover(hoverVideo, hideVideo);
 // ホバーすると再生開始
 function hoverVideo(e) {
     $('video', this).get(0).play();
+    const targetElem = document.querySelectorAll('video');
+    const targetCount = targetElem.length;
+    if (targetElem) {
+        for (let i = 0; i < targetCount; i++)
+        targetElem[i].playbackRate = 2.0;
+    }
 }
 
 // ホバーアウトするとサムネイル表示
