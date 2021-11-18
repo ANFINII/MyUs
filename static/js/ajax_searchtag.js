5a471cc94602b9026185fd4a890409454df8f1f7
ANFINII <anfinii19870506@gmail.com>
2021-11-18 18:17:02 +0900

    Searchtagショートカット機能追加、その他リファクタリング

--- static/js/ajax_searchtag.js
+++ static/js/ajax_searchtag.js
@@ -1,8 +1,10 @@
 // 検索タグの送信ボタンにイベントリスナーを設定。内部に Ajax 処理を記述
-$('#tag_form').submit(function(event) {
+$(document).on('click', '.main_tag_2', function(event) {
     event.preventDefault();
-    const url = $(this).attr('action');
+    const url = $(this).closest('form').attr('action');
     const searchtag = $('form [name=searchtag]').val();
+    $('#tag_form')[0].reset();
+    document.querySelector('.main_tag_2').setAttribute('disabled', true);
     $.ajax({
         url: url,
         type: 'POST',
@@ -11,9 +13,9 @@ $('#tag_form').submit(function(event) {
         timeout: 10000,
     })
     .done(function(response) {
+        document.querySelector('.main_tag_2').setAttribute('disabled', true);
         let searchtag_add =
             '<a class="tag_n_add" href="?search=' + response.searchtag + '">' + response.searchtag + '</a>'
-        $('#tag_form')[0].reset();
         $('.tag_n_list').append(searchtag_add);
         console.log(response)
     })
