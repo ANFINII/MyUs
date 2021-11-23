e949bdccdf1ccd44a83b6706daef7df6e0c4da20
ANFINII <anfinii19870506@email.com>
2021-11-23 11:40:58 +0900

    websocket対応のため、chatのurlを変更

--- myusapp/routing.py
+++ myusapp/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from myusapp.consumers import ChatConsumer
+from myusapp.consumers import ChatConsumer, ChatThereadConsumer
 
 websocket_urlpatterns = [
-    path('ws/Index2/', ChatConsumer.as_asgi()),
+    path('ws/chat/detail/<pk>', ChatConsumer.as_asgi()),
+    path('ws/chat/detail/thread/<comment_id>', ChatThereadConsumer.as_asgi()),
 ]
