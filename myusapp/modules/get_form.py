a8b7be574529b25cb7848c52fc1c3cd6ddf9d8a0
ANFINII <anfinii19870506@gmail.com>
2021-12-07 18:32:04 +0900

    再生数の通知機能実装

--- myusapp/modules/get_form.py
+++ myusapp/modules/get_form.py
@@ -1,6 +1,22 @@
+from django.shortcuts import get_object_or_404
+from myusapp.models import NotificationModel
+
 def get_detail(self):
     self.object = self.get_object()
     self.object.read += 1
     self.object.save()
     context = self.get_context_data(object=self.object)
+    if self.object.read == 10000:
+        NotificationModel.objects.create(
+            user_from_id=self.object.author.id,
+            user_to_id=self.object.author.id,
+            type_no=11,
+            content_object=self.object,
+        )
+    if self.object.read == 100000 or 1000000 or 10000000 or 100000000 or 1000000000:
+        notification_obj = get_object_or_404(NotificationModel, type_no=11, object_id=self.object.id)
+        if notification_obj.confirmed.filter(id=self.object.author.id).exists():
+            notification_obj.confirmed.remove(self.object.author)
+        if notification_obj.delete.filter(id=self.object.author.id).exists():
+            notification_obj.delete.remove(self.object.author)
     return self.render_to_response(context)
