81a0272f618ea77f62a20da187bcdebdc7c06daa
ANFINII <anfinii19870506@gmail.com>
2021-12-08 13:18:29 +0900

    通知のリンクを踏んだ時の機能を実装

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,3 +1,22 @@
+// 通知リンクをクリックした時の処理
+$(document).on('click', '.notification_aria_anker', function() {
+    const url = $(this).attr('action');
+    const notification_id = $(this).attr('notification-id');
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: {'notification_id': notification_id, 'csrfmiddlewaretoken': '{{ csrf_token }}'},
+        dataType: 'json',
+        timeout: 10000,
+    })
+    .done(function(response) {
+        console.log(response);
+    })
+    .fail(function(response) {
+        console.log(response);
+    })
+});
+
 // バツボタンをクリックした時の処理
 $(document).on('click', '.notification_aria_list_2', function(event) {
     event.preventDefault();
