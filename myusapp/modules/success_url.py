5a6a11a42600e99f90272e3376898ed6728ac6ff
ANFINII <anfinii19870506@gmail.com>
2021-12-11 23:20:41 +0900

    NotificationModel1にフィールドを追加したため、作成時の項目追加

--- myusapp/modules/success_url.py
+++ myusapp/modules/success_url.py
@@ -4,12 +4,13 @@ from myusapp.models import NotificationModel
 
 User = get_user_model()
 
-def success_url(self, myus_detail, type_no):
+def success_url(self, myus_detail, type_no, type_name):
     obj = self.object
     user = self.request.user
     NotificationModel.objects.create(
         user_from=user,
         type_no=type_no,
+        type_name=type_name,
         content_object=obj,
     )
     if 'myus:chat_detail' in str(myus_detail):
