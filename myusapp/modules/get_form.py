1167abee1a2d03e454344e1c2146fb23c6ea4979
ANFINII <anfinii19870506@gmail.com>
2021-12-12 09:47:39 +0900

    閲覧数通知の機能を実装

--- myusapp/modules/get_form.py
+++ myusapp/modules/get_form.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from myusapp.models import NotificationModel
+from myusapp.models import VideoModel, LiveModel, MusicModel, PictureModel, BlogModel, ChatModel, CollaboModel
 
 def get_detail(self):
     self.object = self.get_object()
@@ -7,11 +8,26 @@ def get_detail(self):
     self.object.save()
     context = self.get_context_data(object=self.object)
     if self.object.read == 10000:
+        type_name = ''
+        if self.object.__class__ == VideoModel:
+            type_name = 'video'
+        if self.object.__class__ == LiveModel:
+            type_name = 'live'
+        if self.object.__class__ == MusicModel:
+            type_name = 'music'
+        if self.object.__class__ == PictureModel:
+            type_name = 'picture'
+        if self.object.__class__ == BlogModel:
+            type_name = 'blog'
+        if self.object.__class__ == ChatModel:
+            type_name = 'chat'
+        if self.object.__class__ == CollaboModel:
+            type_name = 'collabo'
         NotificationModel.objects.create(
             user_from_id=self.object.author.id,
             user_to_id=self.object.author.id,
             type_no=11,
-            tpye_name='views',
+            type_name=type_name,
             content_object=self.object,
         )
     if self.object.read in (100000, 1000000, 10000000, 100000000, 1000000000):
