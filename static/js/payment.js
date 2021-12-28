5c9aa13f3265d17488f055c2499c778cc6eb21b3
ANFINII <anfinii19870506@gmail.com>
2021-12-28 17:16:39 +0900

    Stripeのjs追加

---
+++ static/js/payment.js
@@ -0,0 +1,36 @@
+// 発行可能なAPIキーでStripeオブジェクトのインスタンスを作成します
+let stripe = Stripe(publicKey);
+let checkoutButtons = document.querySelectorAll('.checkout-button');
+
+checkoutButtons.forEach(function(checkoutButton) {
+    checkoutButton.addEventListener('click', function() {
+        fetch('/payment/create_checkout_session', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8',
+                'X-CSRFToken': '{{ csrf_token }}'
+            },
+            body: JSON.stringify({
+                priceId: checkoutButton.value
+            })
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(session) {
+            return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function(result) {
+        // ブラウザやネットワークが原因でredirectToCheckoutが失敗した場合
+        // ローカライズされたエラーメッセージを表示する必要があります
+        // error.messageを使用しています
+            if (result.error) {
+                alert(result.error.message);
+            }
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+        });
+    });
+});
