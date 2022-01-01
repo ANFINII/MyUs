124d53d0013b4dbffb8f9a1282d7d14e4e75a7cd
ANFINII <anfinii19870506@gmail.com>
2022-01-01 14:57:29 +0900

    不要コード削除

--- myus/modules/context_data.py
+++ myus/modules/context_data.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.db.models import F, Count, Exists, OuterRef
