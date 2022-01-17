88d07fd5e44ef96a74d80b94f1ec2477b1c60532
ANFINII <anfinii19870506@gmail.com>
2022-01-17 23:42:00 +0900

    follow機能をjsファイルに切り出し

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -242,35 +242,6 @@ $(document).on('click', '.like_form_comment', function(event) {
     })
 });
 
-// フォローボタンクリック時の処理を定義
-$(document).on('click', '.follow_form', function(event) {
-    event.preventDefault();
-    const url = $(this).attr('action');
-    const nickname = $(this).children().attr('value');
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {'nickname': nickname, 'csrfmiddlewaretoken': '{{ csrf_token }}'},
-        dataType: 'json',
-    })
-    .done(function(response) {
-        if (response['followed']) {
-            $('.follow_change').removeClass('btn-success');
-            $('.follow_change').addClass('btn-danger');
-            $('.btn-danger').html('解除する');
-            $('.follower_count').html(response['follower_count']);
-        } else {
-            $('.follow_change').removeClass('btn-danger');
-            $('.follow_change').addClass('btn-success');
-            $('.btn-success').html('フォローする');
-            $('.follower_count').html(response['follower_count']);
-        }
-    })
-    .fail(function(response) {
-        console.log(response);
-    })
-});
-
 // 広告のリンクをクリックした時の処理
 $(document).on('click', '.advertise_anker', function() {
     const url = $(this).closest('form').attr('action');
