cd5c864611b600442a0e3456eea7b2a2fea199b2
ANFINII <anfinii19870506@gmail.com>
2022-01-22 12:59:06 +0900

    Pjax処理リファクタリング

--- static/js/pjax.js
+++ static/js/pjax.js
@@ -8,6 +8,13 @@ function pjax_dict(href) {
     history.pushState(null, null, href);
 }
 
+window.addEventListener('popstate', e => {
+    const back_url = location.pathname;
+    const back_obj = object_dict[back_url];
+    $('.main_contents').html(back_obj);
+});
+
+
 // pjax_button ボタンを押した時の処理
 $(document).on('click', '.pjax_button', function(event) {
     event.preventDefault();
@@ -20,24 +27,15 @@ $(document).on('click', '.pjax_button', function(event) {
         dataType: 'json',
     })
     .done(function(response) {
-        window.addEventListener('popstate', e => {
-            const back_url = location.pathname;
-            const back_obj = object_dict[back_url];
-            $('.main_contents').html(back_obj);
-            document.querySelector('.side_menu').checked = false;
-            document.querySelector('.dropdown_menu_cloud').checked = false;
-            document.querySelector('.dropdown_menu_notice').checked = false;
-            document.querySelector('.dropdown_menu_profile').checked = false;
-        });
         $('.main_contents').html(response.html);
-        if (document.querySelector('.chat_remove') != null) {
-            document.querySelector('.chat_remove').remove();
-        }
         document.querySelector('.side_menu').checked = false;
         document.querySelector('.dropdown_menu_cloud').checked = false;
         document.querySelector('.dropdown_menu_notice').checked = false;
         document.querySelector('.dropdown_menu_profile').checked = false;
-        pjax_dict(href)
+        if (document.querySelector('.chat_remove') != null) {
+            document.querySelector('.chat_remove').remove();
+        }
+        pjax_dict(href);
     })
     .fail(function(response) {
         console.log(response);
@@ -58,19 +56,11 @@ $(document).on('click', '.pjax_button_userpage', function(event) {
         dataType: 'json',
     })
     .done(function(response) {
-        window.addEventListener('popstate', e => {
-            const back_url = location.pathname;
-            const back_obj = object_dict[back_url];
-            $('.main_contents').html(back_obj);
-            if (document.querySelector('.chat_remove') != null) {
-                document.querySelector('.chat_remove').remove();
-            }
-        });
         $('.main_contents').html(response.html);
         if (document.querySelector('.chat_remove') != null) {
             document.querySelector('.chat_remove').remove();
         }
-        pjax_dict(href)
+        pjax_dict(href);
     })
     .fail(function(response) {
         console.log(response);
