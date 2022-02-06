2c698b8daf556b76ad4595bfb22de5c031abf708
ANFINII <anfinii19870506@gmail.com>
2022-02-06 21:11:16 +0900

    不要のため、削除

--- static/js/file_button.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '#select_file_button', function() {
-    $("#file_button").click();
-})
-
-$(document).on('change', '#file_button', function() {
-    $('#filename').val($(this).val().replace(/^.*\\/, ""));
-})
