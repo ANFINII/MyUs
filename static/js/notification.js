3a1d8a9ded96df26f916e406c8add03062ab5534
ANFINII <anfinii19870506@gmail.com>
2022-02-06 20:58:38 +0900

    MyPageのトグルボタンの処理を共通化

--- static/js/notification.js
+++ static/js/notification.js
@@ -61,3 +61,24 @@ $(document).on('click', '.toggle_button', function(event) {
         console.log(response);
     })
 });
+
+
+// MyPageの全体広告のトグルボタンをクリックした時の処理
+$(document).on('click', '.toggle_mypage', function(event) {
+    event.preventDefault();
+    const url = $(this).closest('form').attr('action');
+    const advertise = $(this).closest('form').attr('advertise');
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: {'advertise': advertise, 'csrfmiddlewaretoken': '{{ csrf_token }}'},
+        dataType: 'json',
+        timeout: 10000,
+    })
+    .done(function(response) {
+        $('#auto_advertise').html(response.advertise);
+    })
+    .fail(function(response) {
+        console.log(response);
+    })
+});
