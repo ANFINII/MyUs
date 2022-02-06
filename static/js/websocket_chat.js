123a0597fa050e66ae644e074bbf562d7d0c3c06
ANFINII <anfinii19870506@gmail.com>
2022-02-06 17:32:24 +0900

    ajaxを切り分けリファクタリング

--- static/js/websocket_chat.js
+++ static/js/websocket_chat.js
@@ -244,38 +244,6 @@ $(document).on('click', '.edit_delete_reply', function(event) {
     }));
 });
 
-// いいねボタンクリック時の処理を定義
-$(document).on('click', '.like_form', function(event) {
-    event.preventDefault();
-    const id = $(this).attr('value');
-    const url = $(this).parent().attr('action');
-    const path = $(this).parent().attr('path');
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {'id': id, 'path': path, 'csrfmiddlewaretoken': '{{ csrf_token }}'},
-        dataType: 'json',
-    })
-    .done(function(response) {
-        if (response['liked']) {
-            $('.like_color').removeClass('bi-hand-thumbs-up');
-            $('.like_color').parent().removeClass('like_no');
-            $('.like_color').addClass('bi-hand-thumbs-up-fill');
-            $('.like_color').parent().addClass('like_fill');
-            $('#like_count_object_' + id).html(response['total_like']);
-        } else {
-            $('.like_color').removeClass('bi-hand-thumbs-up-fill');
-            $('.like_color').parent().removeClass('like_fill');
-            $('.like_color').addClass('bi-hand-thumbs-up');
-            $('.like_color').parent().addClass('like_no');
-            $('#like_count_object_' + id).html(response['total_like']);
-        }
-    })
-    .fail(function(response) {
-        console.log(response);
-    })
-});
-
 // replyショートカット
 $(document).on('focus', '#reply_form_area', function(event) {
     event.preventDefault();
