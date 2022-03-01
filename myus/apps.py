a493f4b93759894baa88cb49e11c87bb755453c5
ANFINII <anfinii19870506@gmail.com>
2022-03-01 23:23:59 +0900

    削除モジュール

--- myus/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.contrib.admin.apps import AdminConfig
-
-class MyUsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myus'
-    verbose_name = 'MyUs'
--- myus/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from myus.consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/chat/detail/<pk>', ChatConsumer.as_asgi()),
-]
