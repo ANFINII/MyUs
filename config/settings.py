d8d3ad1827ea44223cc5179da23ad2560677c475
ANFINII <anfinii19870506@gmail.com>
2022-03-07 23:45:41 +0900

    CORS_ALLOWED_ORIGINS設定

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,10 @@ MIDDLEWARE += [
 ROOT_URLCONF = 'config.urls'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
 
 TEMPLATES = [
     {
