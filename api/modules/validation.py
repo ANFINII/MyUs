f939508b09050aa5bdbecfa9882b1bb106ac7789
ANFINII <anfinii19870506@gmail.com>
2022-04-02 20:04:27 +0900

    validation リファクタリング

--- api/modules/validation.py
+++ api/modules/validation.py
@@ -2,26 +2,26 @@ import re
 
 # バリデーション関数定義
 def has_username(text):
-    if re.fullmatch('^[a-zA-Z0-9_]+$', text) != None:
+    if re.fullmatch('^[a-zA-Z0-9_]+$', text) is not None:
         return False
     return True
 
 def has_email(text):
-    if re.fullmatch('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z.]+$', text) != None:
+    if re.fullmatch('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z.]+$', text) is not None:
         return False
     return True
 
 def has_phone(text):
-    if re.fullmatch('\d{2,4}-?\d{2,4}-?\d{3,4}', text) != None:
+    if re.fullmatch('\d{2,4}-?\d{2,4}-?\d{3,4}', text) is not None:
         return False
     return True
 
 def has_alphabet(text):
-    if re.search('[a-zA-Z]', text) != None:
+    if re.search('[a-zA-Z]', text) is not None:
         return True
     return False
 
 def has_number(text):
-    if re.search('[0-9０-９]', text) != None:
+    if re.search('[0-9０-９]', text) is not None:
         return True
     return False
