ed016a5ac55f706e0893f13120bdc5ae469ed9c2
ANFINII <anfinii19870506@gmail.com>
2022-04-29 17:20:13 +0900

    get_form コードリファクタリング

--- api/modules/get_form.py
+++ api/modules/get_form.py
@@ -7,24 +7,25 @@ from api.modules import contains
 def get_detail(self):
     self.object = self.get_object()
     obj = self.object
-    obj.read += 1
-    obj.read.save()
+    obj_id = self.kwargs['pk']
+    obj_class = obj.__class__
+    obj_class.objects.filter(id=obj_id).update(read=obj.read + 1)
     context = self.get_context_data(object=obj)
     if obj.read == 10000:
         type_name = ''
-        if obj.__class__ == Video:
+        if obj_class == Video:
             type_name = 'video'
-        if obj.__class__ == Live:
+        if obj_class == Live:
             type_name = 'live'
-        if obj.__class__ == Music:
+        if obj_class == Music:
             type_name = 'music'
-        if obj.__class__ == Picture:
+        if obj_class == Picture:
             type_name = 'picture'
-        if obj.__class__ == Blog:
+        if obj_class == Blog:
             type_name = 'blog'
-        if obj.__class__ == Chat:
+        if obj_class == Chat:
             type_name = 'chat'
-        if obj.__class__ == Collabo:
+        if obj_class == Collabo:
             type_name = 'collabo'
         Notification.objects.create(
             user_from_id=obj.author.id,
@@ -34,7 +35,7 @@ def get_detail(self):
             content_object=obj,
         )
     if obj.read in (100000, 1000000, 10000000, 100000000, 1000000000):
-        notification_obj = get_object_or_404(Notification, type_no=contains.notification_type_no['views'], object_id=obj.id)
+        notification_obj = get_object_or_404(Notification, type_no=contains.notification_type_no['views'], object_id=obj_id)
         if notification_obj.confirmed.filter(id=obj.author.id).exists():
             notification_obj.confirmed.remove(obj.author)
         if notification_obj.deleted.filter(id=obj.author.id).exists():
