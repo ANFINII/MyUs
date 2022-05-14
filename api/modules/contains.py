2f2d7f5356f5b6fb60425a686971955224c68a21
ANFINII <anfinii19870506@gmail.com>
2022-05-14 22:23:42 +0900

    dictをcontainファイルに収納

--- api/modules/contains.py
+++ api/modules/contains.py
@@ -1,3 +1,35 @@
+from api.models import Video, Live, Music, Picture, Blog, Chat, Collabo, Todo
+
+models_dict = {
+    Video: 'video',
+    Live: 'live',
+    Music: 'music',
+    Picture: 'picture',
+    Blog: 'blog',
+    Chat: 'chat',
+    Collabo: 'collabo',
+}
+
+models_like_dict = {
+    'video/detail': Video,
+    'live/detail': Live,
+    'music/detail': Music,
+    'picture/detail': Picture,
+    'blog/detail': Blog,
+    'chat/detail': Chat,
+    'collabo/detail': Collabo,
+}
+
+models_comment_dict = {
+    'video/detail': Video,
+    'live/detail': Live,
+    'music/detail': Music,
+    'picture/detail': Picture,
+    'blog/detail': Blog,
+    'collabo/detail': Collabo,
+    'todo/detail': Todo,
+}
+
 notification_type_no = {
     'video'  : 1,
     'live'   : 2,
