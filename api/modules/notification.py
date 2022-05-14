e5fd464ba8d526a7670a491938bf0b06d0524143
ANFINII <anfinii19870506@gmail.com>
2022-05-15 00:46:33 +0900

    notificationのリファクタリング

--- api/modules/notification.py
+++ api/modules/notification.py
@@ -1,17 +1,16 @@
 from django.db.models import Exists, OuterRef
-from itertools import chain
 from api.models import NotificationSetting, Notification, Follow
 from api.modules import contains
 
 
 def notification_data(self):
     user_id = self.request.user.id
-    notification_obj_list_1, notification_obj_list_2 = [], []
-    notification_list_1, notification_list_confirmed = [], []
+    notification_obj_list_1 = []
+    notification_obj_list_2 = []
     confirmed_kwargs = {}
     confirmed_kwargs['id'] = OuterRef('pk')
     confirmed_kwargs['confirmed'] = user_id
-    subquery_confirmed = Notification.objects.filter(**confirmed_kwargs)
+    subquery = Notification.objects.filter(**confirmed_kwargs)
     following_list = list(Follow.objects.filter(follower_id=user_id).values_list('following_id', 'created'))
     if user_id is not None:
         notification_obj = NotificationSetting.objects.get(user_id=user_id)
@@ -38,12 +37,11 @@ def notification_data(self):
         if notification_obj.is_views:
             notification_obj_list_2 += [contains.notification_type_no['views']]
         for id, dates in following_list:
-            notification_list_1 += Notification.objects.filter(user_from_id__in=[id], created__gt=dates, user_to_id=None, type_no__in=notification_obj_list_1).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery_confirmed)).order_by('-created')
-            notification_list_confirmed += Notification.objects.filter(user_from_id__in=[id], created__gt=dates, user_to_id=None, type_no__in=notification_obj_list_1).exclude(deleted=user_id, confirmed=user_id).annotate(user_confirmed=Exists(subquery_confirmed)).order_by('-created')
-        notification_list_2 = Notification.objects.filter(user_to_id=user_id, type_no__in=notification_obj_list_2).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery_confirmed)).order_by('-created')
+            qs_1 = Notification.objects.filter(user_from_id__in=[id], created__gt=dates, user_to_id=None, type_no__in=notification_obj_list_1).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery))
+            qs_2 = Notification.objects.filter(user_to_id=user_id, type_no__in=notification_obj_list_2).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery))
         notification_list_data = {
-            'notification_list': list(chain(notification_list_1, notification_list_2)),
-            'notification_count': len(list(chain(notification_list_confirmed, notification_list_2.exclude(confirmed=user_id)))),
+            'notification_list': qs_1.union(qs_2).order_by('-created'),
+            'notification_count': qs_1.exclude(confirmed=user_id).union(qs_2.exclude(confirmed=user_id)).count(),
         }
         return notification_list_data
 
