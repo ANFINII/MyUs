259587fae06aa15a43a6842560d639658c40771c
ANFINII <anfinii19870506@gmail.com>
2022-05-14 22:29:18 +0900

    media詳細画面のリファクタリング

--- api/modules/get_form.py
+++ api/modules/get_form.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from api.models import Notification
-from api.models import Video, Live, Music, Picture, Blog, Chat, Collabo
 from api.modules import contains
 
 
@@ -12,21 +11,7 @@ def get_detail(self):
     obj_class.objects.filter(id=obj_id).update(read=obj.read + 1)
     context = self.get_context_data(object=obj)
     if obj.read == 10000:
-        type_name = ''
-        if obj_class == Video:
-            type_name = 'video'
-        if obj_class == Live:
-            type_name = 'live'
-        if obj_class == Music:
-            type_name = 'music'
-        if obj_class == Picture:
-            type_name = 'picture'
-        if obj_class == Blog:
-            type_name = 'blog'
-        if obj_class == Chat:
-            type_name = 'chat'
-        if obj_class == Collabo:
-            type_name = 'collabo'
+        type_name = [value for key, value in contains.models_dict.items() if obj_class == key]
         Notification.objects.create(
             user_from_id=obj.author.id,
             user_to_id=obj.author.id,
