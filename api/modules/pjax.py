7bb10ba362b9dcc26be47c06bf47bc218d955fbc
ANFINII <anfinii19870506@gmail.com>
2022-05-15 11:53:15 +0900

    viewsからPjax処理のロジック分離

---
+++ api/modules/pjax.py
@@ -0,0 +1,155 @@
+import datetime
+from django.contrib.auth import get_user_model
+from django.db.models import Count, F
+from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
+from api.forms import BlogForm
+from api.models import Video, Live, Music, Picture, Blog, Chat, Collabo, Todo
+from api.models import NotificationSetting, Follow, Advertise
+
+User = get_user_model()
+
+def pjax_context(request, href):
+    context = dict()
+    if '/' == href:
+        context['html'] = render_to_string('index_list.html', {
+            'video_list': Video.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+            'live_list': Live.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+            'music_list': Music.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+            'picture_list': Picture.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+            'blog_list': Blog.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+            'chat_list': Chat.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
+        }, request=request)
+    if '/recommend' == href:
+        aggregation_date = datetime.datetime.today() - datetime.timedelta(days=100)
+        context['html'] = render_to_string('index_list.html', {
+            'Recommend': 'Recommend',
+            'video_list': Video.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+            'live_list': Live.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+            'music_list': Music.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+            'picture_list': Picture.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+            'blog_list': Blog.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+            'chat_list': Chat.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
+        }, request=request)
+    if '/userpage/post' in href:
+        nickname = request.GET.get('nickname')
+        author = get_object_or_404(User, nickname=nickname)
+        author_id = author.id
+        follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
+        followed = False
+        if follow.exists():
+            followed = True
+        context['html'] = render_to_string('userpage/userpage_list.html', {
+            'followed': followed,
+            'author_name': author.nickname,
+            'user_list': User.objects.filter(id=author_id),
+            'video_list': Video.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+            'live_list': Live.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+            'music_list': Music.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+            'picture_list': Picture.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+            'blog_list': Blog.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+            'chat_list': Chat.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
+        }, request=request)
+    if '/userpage/information' in href:
+        nickname = request.GET.get('nickname')
+        author = get_object_or_404(User, nickname=nickname)
+        author_id = author.id
+        follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
+        followed = False
+        if follow.exists():
+            followed = True
+        context['html'] = render_to_string('userpage/userpage_information_content.html', {
+            'followed': followed,
+            'author_name': author.nickname,
+            'user_list': User.objects.filter(id=author_id),
+        }, request=request)
+    if '/userpage/advertise' in href:
+        nickname = request.GET.get('nickname')
+        author = get_object_or_404(User, nickname=nickname)
+        author_id = author.id
+        follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
+        followed = False
+        if follow.exists():
+            followed = True
+        context['html'] = render_to_string('userpage/userpage_advertise_list.html', {
+            'followed': followed,
+            'author_name': author.nickname,
+            'user_list': User.objects.filter(id=author_id),
+            'advertise_list': Advertise.objects.filter(author_id=author_id, publish=True),
+        }, request=request)
+    if '/video' == href:
+        context['html'] = render_to_string('video/video_list.html', {
+            'video_list': Video.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
+        }, request=request)
+    if '/live' == href:
+        context['html'] = render_to_string('live/live_list.html', {
+            'live_list': Live.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
+        }, request=request)
+    if '/music' == href:
+        context['html'] = render_to_string('music/music_list.html', {
+            'music_list': Music.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
+        }, request=request)
+    if '/picture' == href:
+        context['html'] = render_to_string('picture/picture_list.html', {
+            'picture_list': Picture.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
+        }, request=request)
+    if '/blog' == href:
+        context['html'] = render_to_string('blog/blog_list.html', {
+            'blog_list': Blog.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
+        }, request=request)
+    if '/chat' == href:
+        context['html'] = render_to_string('chat/chat_list.html', {
+            'chat_list': Chat.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
+        }, request=request)
+    if '/collabo' == href:
+        context['html'] = render_to_string('collabo/collabo_list.html', {
+            'collabo_list': Collabo.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
+        }, request=request)
+    if '/todo' == href:
+        context['html'] = render_to_string('todo/todo_list.html', {
+            'todo_list': Todo.objects.filter(author_id=request.user.id).order_by('-created')[:100],
+        }, request=request)
+    if '/follow' == href:
+        context['html'] = render_to_string('follow/follow_list.html', {
+            'follow_list': Follow.objects.filter(follower_id=request.user.id).select_related('following__mypage').order_by('created')[:100],
+        }, request=request)
+    if '/follower' == href:
+        context['html'] = render_to_string('follow/follower_list.html', {
+            'follower_list': Follow.objects.filter(following_id=request.user.id).select_related('follower__mypage').order_by('created')[:100],
+        }, request=request)
+    if '/notification' == href:
+        context['html'] = render_to_string('common/notification_content.html', {
+            'notification_setting_list': NotificationSetting.objects.filter(user_id=request.user.id),
+        }, request=request)
+    if '/userpolicy' == href:
+        context['html'] = render_to_string('common/userpolicy_content.html', request=request)
+    if '/knowledge' == href:
+        context['html'] = render_to_string('common/knowledge_content.html', request=request)
+    if '/payment' == href:
+        context['html'] = render_to_string('payment/payment_content.html', request=request)
+    if '/profile' == href:
+        context['html'] = render_to_string('registration/profile_content.html', request=request)
+    if '/mypage' == href:
+        context['html'] = render_to_string('registration/mypage_content.html', request=request)
+    if '/withdrawal' == href:
+        EXPIRED_SECONDS = 60
+        context['html'] = render_to_string('registration/withdrawal_content.html', {
+            'expired_seconds': EXPIRED_SECONDS,
+        }, request=request)
+    if '/video/create' == href:
+        context['html'] = render_to_string('video/video_create_content.html', request=request)
+    if '/live/create' == href:
+        context['html'] = render_to_string('live/live_create_content.html', request=request)
+    if '/music/create' == href:
+        context['html'] = render_to_string('music/music_create_content.html', request=request)
+    if '/picture/create' == href:
+        context['html'] = render_to_string('picture/picture_create_content.html', request=request)
+    if '/blog/create' == href:
+        context['html'] = render_to_string('blog/blog_create_content.html', {'form' :BlogForm(),}, request=request)
+    if '/chat/create' == href:
+        context['html'] = render_to_string('chat/chat_create_content.html', request=request)
+    if '/collabo/create' == href:
+        context['html'] = render_to_string('collabo/collabo_create_content.html', request=request)
+    if '/todo/create' == href:
+        context['html'] = render_to_string('todo/todo_create_content.html', request=request)
+    return context
--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
+import datetime
+import json
+import os
+import random
+import string
+import stripe
+
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.core.signing import TimestampSigner, SignatureExpired, BadSignature
-from django.db.models import Count, F
+from django.db.models import Count
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template.loader import render_to_string
@@ -18,24 +24,19 @@ from rest_framework import authentication, permissions, views
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from api.serializers import UserSerializer
-from api.forms import SearchTagForm, BlogForm
+from api.forms import SearchTagForm
 from api.models import MyPage, SearchTag, NotificationSetting, Notification, Comment, Follow
 from api.models import Video, Live, Music, Picture, Blog, Chat, Collabo, Todo, Advertise
 from api.modules import contains
 from api.modules.context_data import ContextData
 from api.modules.get_form import get_detail
+from api.modules.pjax import pjax_context
 from api.modules.notification import notification_data, notification_setting_update
 from api.modules.search import Search
 from api.modules.success_url import success_url
 from api.modules.convert_hls import convert_hls, convert_mp4
 from api.modules.follow import follow_update_data
 from api.modules.validation import has_username, has_email, has_phone, has_alphabet, has_number
-import datetime
-import json
-import os
-import random
-import string
-import stripe
 
 # Create your views here.
 
@@ -194,150 +195,9 @@ def logout_form(request):
 
 # pjax
 def pjax(request):
-    context = dict()
     if request.method == 'GET':
         href = request.GET.get('href')
-        if '/' == href:
-            context['html'] = render_to_string('index_list.html', {
-                'video_list': Video.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-                'live_list': Live.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-                'music_list': Music.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-                'picture_list': Picture.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-                'blog_list': Blog.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-                'chat_list': Chat.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:8],
-            }, request=request)
-        if '/recommend' == href:
-            aggregation_date = datetime.datetime.today() - datetime.timedelta(days=100)
-            context['html'] = render_to_string('index_list.html', {
-                'Recommend': 'Recommend',
-                'video_list': Video.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-                'live_list': Live.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-                'music_list': Music.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-                'picture_list': Picture.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-                'blog_list': Blog.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-                'chat_list': Chat.objects.filter(publish=True).filter(created__gte=aggregation_date).annotate(score=F('read') + Count('like')*10 + F('read')*Count('like')/F('read')*20).filter(score__gte=50).select_related('author').prefetch_related('like').order_by('-score')[:8],
-            }, request=request)
-        if '/userpage/post' in href:
-            nickname = request.GET.get('nickname')
-            author = get_object_or_404(User, nickname=nickname)
-            author_id = author.id
-            follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
-            followed = False
-            if follow.exists():
-                followed = True
-            context['html'] = render_to_string('userpage/userpage_list.html', {
-                'followed': followed,
-                'author_name': author.nickname,
-                'user_list': User.objects.filter(id=author_id),
-                'video_list': Video.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-                'live_list': Live.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-                'music_list': Music.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-                'picture_list': Picture.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-                'blog_list': Blog.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-                'chat_list': Chat.objects.filter(author_id=author_id, publish=True).select_related('author').prefetch_related('like'),
-            }, request=request)
-        if '/userpage/information' in href:
-            nickname = request.GET.get('nickname')
-            author = get_object_or_404(User, nickname=nickname)
-            author_id = author.id
-            follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
-            followed = False
-            if follow.exists():
-                followed = True
-            context['html'] = render_to_string('userpage/userpage_information_content.html', {
-                'followed': followed,
-                'author_name': author.nickname,
-                'user_list': User.objects.filter(id=author_id),
-            }, request=request)
-        if '/userpage/advertise' in href:
-            nickname = request.GET.get('nickname')
-            author = get_object_or_404(User, nickname=nickname)
-            author_id = author.id
-            follow = Follow.objects.filter(follower=request.user.id).filter(following=author_id)
-            followed = False
-            if follow.exists():
-                followed = True
-            context['html'] = render_to_string('userpage/userpage_advertise_list.html', {
-                'followed': followed,
-                'author_name': author.nickname,
-                'user_list': User.objects.filter(id=author_id),
-                'advertise_list': Advertise.objects.filter(author_id=author_id, publish=True),
-            }, request=request)
-        if '/video' == href:
-            context['html'] = render_to_string('video/video_list.html', {
-                'video_list': Video.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
-            }, request=request)
-        if '/live' == href:
-            context['html'] = render_to_string('live/live_list.html', {
-                'live_list': Live.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
-            }, request=request)
-        if '/music' == href:
-            context['html'] = render_to_string('music/music_list.html', {
-                'music_list': Music.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:50],
-            }, request=request)
-        if '/picture' == href:
-            context['html'] = render_to_string('picture/picture_list.html', {
-                'picture_list': Picture.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
-            }, request=request)
-        if '/blog' == href:
-            context['html'] = render_to_string('blog/blog_list.html', {
-                'blog_list': Blog.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
-            }, request=request)
-        if '/chat' == href:
-            context['html'] = render_to_string('chat/chat_list.html', {
-                'chat_list': Chat.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
-            }, request=request)
-        if '/collabo' == href:
-            context['html'] = render_to_string('collabo/collabo_list.html', {
-                'collabo_list': Collabo.objects.filter(publish=True).select_related('author').prefetch_related('like').order_by('-created')[:100],
-            }, request=request)
-        if '/todo' == href:
-            context['html'] = render_to_string('todo/todo_list.html', {
-                'todo_list': Todo.objects.filter(author_id=request.user.id).order_by('-created')[:100],
-            }, request=request)
-        if '/follow' == href:
-            context['html'] = render_to_string('follow/follow_list.html', {
-                'follow_list': Follow.objects.filter(follower_id=request.user.id).select_related('following__mypage').order_by('created')[:100],
-            }, request=request)
-        if '/follower' == href:
-            context['html'] = render_to_string('follow/follower_list.html', {
-                'follower_list': Follow.objects.filter(following_id=request.user.id).select_related('follower__mypage').order_by('created')[:100],
-            }, request=request)
-        if '/notification' == href:
-            context['html'] = render_to_string('common/notification_content.html', {
-                'notification_setting_list': NotificationSetting.objects.filter(user_id=request.user.id),
-            }, request=request)
-        if '/userpolicy' == href:
-            context['html'] = render_to_string('common/userpolicy_content.html', request=request)
-        if '/knowledge' == href:
-            context['html'] = render_to_string('common/knowledge_content.html', request=request)
-        if '/payment' == href:
-            context['html'] = render_to_string('payment/payment_content.html', request=request)
-        if '/profile' == href:
-            context['html'] = render_to_string('registration/profile_content.html', request=request)
-        if '/mypage' == href:
-            context['html'] = render_to_string('registration/mypage_content.html', request=request)
-        if '/withdrawal' == href:
-            EXPIRED_SECONDS = 60
-            context['html'] = render_to_string('registration/withdrawal_content.html', {
-                'expired_seconds': EXPIRED_SECONDS,
-            }, request=request)
-        if '/video/create' == href:
-            context['html'] = render_to_string('video/video_create_content.html', request=request)
-        if '/live/create' == href:
-            context['html'] = render_to_string('live/live_create_content.html', request=request)
-        if '/music/create' == href:
-            context['html'] = render_to_string('music/music_create_content.html', request=request)
-        if '/picture/create' == href:
-            context['html'] = render_to_string('picture/picture_create_content.html', request=request)
-        if '/blog/create' == href:
-            context['html'] = render_to_string('blog/blog_create_content.html', {'form' :BlogForm(),}, request=request)
-        if '/chat/create' == href:
-            context['html'] = render_to_string('chat/chat_create_content.html', request=request)
-        if '/collabo/create' == href:
-            context['html'] = render_to_string('collabo/collabo_create_content.html', request=request)
-        if '/todo/create' == href:
-            context['html'] = render_to_string('todo/todo_create_content.html', request=request)
+        context = pjax_context(request, href)
         return JsonResponse(context)
 
 
