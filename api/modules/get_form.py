18defd87ddd25da1675e5bdb9d6fabffa6f1744e
ANFINII <anfinii19870506@gmail.com>
2022-05-20 20:07:31 +0900

    get_form.py 不要コードの削除

--- api/modules/get_form.py
+++ api/modules/get_form.py
@@ -6,9 +6,8 @@ from api.modules import contains
 def get_detail(self):
     self.object = self.get_object()
     obj = self.object
-    obj_id = self.kwargs['pk']
     obj_class = obj.__class__
-    obj_class.objects.filter(id=obj_id).update(read=obj.read + 1)
+    obj_class.objects.filter(id=obj.id).update(read=obj.read + 1)
     context = self.get_context_data(object=obj)
     if obj.read == 10000:
         type_name = [value for key, value in contains.models_dict.items() if obj_class == key]
@@ -20,7 +19,7 @@ def get_detail(self):
             content_object=obj,
         )
     if obj.read in (100000, 1000000, 10000000, 100000000, 1000000000):
-        notification_obj = get_object_or_404(Notification, type_no=contains.notification_type_no['views'], object_id=obj_id)
+        notification_obj = get_object_or_404(Notification, type_no=contains.notification_type_no['views'], object_id=obj.id)
         if notification_obj.confirmed.filter(id=obj.author.id).exists():
             notification_obj.confirmed.remove(obj.author)
         if notification_obj.deleted.filter(id=obj.author.id).exists():
