d6019dd8dc70aecdcb634b3f89444e5f369ab7e1
ANFINII <anfinii19870506@gmail.com>
2022-05-20 12:32:17 +0900

    notification.py リファクタリング

--- api/modules/notification.py
+++ api/modules/notification.py
@@ -4,44 +4,44 @@ from api.modules import contains
 
 
 def notification_data(self):
-    user_id = self.request.user.id
-    notification_obj_list_1 = []
-    notification_obj_list_2 = []
+    user = self.request.user
+    notification_type_list_1, notification_type_list_2 = [], []
+    notification_obj_1 = None
     confirmed_kwargs = {}
     confirmed_kwargs['id'] = OuterRef('pk')
-    confirmed_kwargs['confirmed'] = user_id
+    confirmed_kwargs['confirmed'] = user
     subquery = Notification.objects.filter(**confirmed_kwargs)
-    following_list = list(Follow.objects.filter(follower_id=user_id).values_list('following_id', 'created'))
-    if user_id is not None:
-        notification_obj = NotificationSetting.objects.get(user_id=user_id)
-        if notification_obj.is_video:
-            notification_obj_list_1 += [contains.notification_type_no['video']]
-        if notification_obj.is_live:
-            notification_obj_list_1 += [contains.notification_type_no['live']]
-        if notification_obj.is_music:
-            notification_obj_list_1 += [contains.notification_type_no['music']]
-        if notification_obj.is_picture:
-            notification_obj_list_1 += [contains.notification_type_no['picture']]
-        if notification_obj.is_blog:
-            notification_obj_list_1 += [contains.notification_type_no['blog']]
-        if notification_obj.is_chat:
-            notification_obj_list_1 += [contains.notification_type_no['chat']]
-        if notification_obj.is_collabo:
-            notification_obj_list_1 += [contains.notification_type_no['collabo']]
-        if notification_obj.is_follow:
-            notification_obj_list_2 += [contains.notification_type_no['follow']]
-        if notification_obj.is_like:
-            notification_obj_list_2 += [contains.notification_type_no['like']]
-        if notification_obj.is_reply:
-            notification_obj_list_2 += [contains.notification_type_no['reply']]
-        if notification_obj.is_views:
-            notification_obj_list_2 += [contains.notification_type_no['views']]
+    following_list = Follow.objects.filter(follower=user).values_list('following_id', 'created')
+    if user is not None:
+        notification_setting_obj = NotificationSetting.objects.get(user=user)
+        if notification_setting_obj.is_video:
+            notification_type_list_1 += [contains.notification_type_no['video']]
+        if notification_setting_obj.is_live:
+            notification_type_list_1 += [contains.notification_type_no['live']]
+        if notification_setting_obj.is_music:
+            notification_type_list_1 += [contains.notification_type_no['music']]
+        if notification_setting_obj.is_picture:
+            notification_type_list_1 += [contains.notification_type_no['picture']]
+        if notification_setting_obj.is_blog:
+            notification_type_list_1 += [contains.notification_type_no['blog']]
+        if notification_setting_obj.is_chat:
+            notification_type_list_1 += [contains.notification_type_no['chat']]
+        if notification_setting_obj.is_collabo:
+            notification_type_list_1 += [contains.notification_type_no['collabo']]
+        if notification_setting_obj.is_follow:
+            notification_type_list_2 += [contains.notification_type_no['follow']]
+        if notification_setting_obj.is_like:
+            notification_type_list_2 += [contains.notification_type_no['like']]
+        if notification_setting_obj.is_reply:
+            notification_type_list_2 += [contains.notification_type_no['reply']]
+        if notification_setting_obj.is_views:
+            notification_type_list_2 += [contains.notification_type_no['views']]
         for id, dates in following_list:
-            qs_1 = Notification.objects.filter(user_from_id__in=[id], created__gt=dates, user_to_id=None, type_no__in=notification_obj_list_1).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery))
-            qs_2 = Notification.objects.filter(user_to_id=user_id, type_no__in=notification_obj_list_2).exclude(deleted=user_id).annotate(user_confirmed=Exists(subquery))
+            notification_obj_1 = Notification.objects.filter(user_from__in=[id], user_to=None, type_no__in=notification_type_list_1, created__gt=dates).exclude(deleted=user).annotate(user_confirmed=Exists(subquery)).order_by('-created')
+        notification_obj_2 = Notification.objects.filter(user_to=user, type_no__in=notification_type_list_2).exclude(deleted=user).annotate(user_confirmed=Exists(subquery)).order_by('-created')
         notification_list_data = {
-            'notification_list': qs_1.union(qs_2).order_by('-created'),
-            'notification_count': qs_1.exclude(confirmed=user_id).union(qs_2.exclude(confirmed=user_id)).count(),
+            'notification_list': list(notification_obj_1) + list(notification_obj_2),
+            'notification_count': notification_obj_1.exclude(confirmed=user).count() + notification_obj_2.exclude(confirmed=user).count(),
         }
         return notification_list_data
 
