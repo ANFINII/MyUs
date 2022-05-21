b7a4f91f3b828db51030dccc807232d647e4288e
ANFINII <anfinii19870506@gmail.com>
2022-05-21 13:26:46 +0900

    contains.py リファクタリング

--- api/modules/contains.py
+++ api/modules/contains.py
@@ -1,45 +1,45 @@
 from api.models import Video, Live, Music, Picture, Blog, Chat, Collabo, Todo
 
 models_dict = {
-    Video: 'video',
-    Live: 'live',
-    Music: 'music',
+    Video  : 'video',
+    Live   : 'live',
+    Music  : 'music',
     Picture: 'picture',
-    Blog: 'blog',
-    Chat: 'chat',
+    Blog   : 'blog',
+    Chat   : 'chat',
     Collabo: 'collabo',
 }
 
 models_like_dict = {
-    'video/detail': Video,
-    'live/detail': Live,
-    'music/detail': Music,
+    'video/detail'  : Video,
+    'live/detail'   : Live,
+    'music/detail'  : Music,
     'picture/detail': Picture,
-    'blog/detail': Blog,
-    'chat/detail': Chat,
+    'blog/detail'   : Blog,
+    'chat/detail'   : Chat,
     'collabo/detail': Collabo,
 }
 
 models_comment_dict = {
-    'video/detail': Video,
-    'live/detail': Live,
-    'music/detail': Music,
+    'video/detail'  : Video,
+    'live/detail'   : Live,
+    'music/detail'  : Music,
     'picture/detail': Picture,
-    'blog/detail': Blog,
+    'blog/detail'   : Blog,
     'collabo/detail': Collabo,
-    'todo/detail': Todo,
+    'todo/detail'   : Todo,
 }
 
-notification_type_no = {
-    'video'  : 1,
-    'live'   : 2,
-    'music'  : 3,
-    'picture': 4,
-    'blog'   : 5,
-    'chat'   : 6,
-    'collabo': 7,
-    'follow' : 8,
-    'like'   : 9,
-    'reply'  : 10,
-    'views'  : 11,
+notification_type_dict = {
+    'video'  : [1 , 'is_video'],
+    'live'   : [2 , 'is_live'],
+    'music'  : [3 , 'is_music'],
+    'picture': [4 , 'is_picture'],
+    'blog'   : [5 , 'is_blog'],
+    'chat'   : [6 , 'is_chat'],
+    'collabo': [7 , 'is_collabo'],
+    'follow' : [8 , 'is_follow'],
+    'like'   : [9 , 'is_like'],
+    'reply'  : [10, 'is_reply'],
+    'views'  : [11, 'is_views'],
 }
