85f6e657cf85a0f4372ee62eeb028832c55d3e62
ANFINII <anfinii19870506@gmail.com>
2022-06-12 13:26:36 +0900

    Pjax.js リファクタリング

--- frontend/static/js/pjax.js
+++ frontend/static/js/pjax.js
@@ -2,17 +2,36 @@
 let object_dict = {};
 history.replaceState(null, null, location.href);
 
+window.addEventListener('popstate', e => {
+  const back_url = location.pathname;
+  const back_obj = object_dict[back_url];
+  $('.main_contents').html(back_obj);
+});
+
 function pjax_dict(href) {
   const obj = document.querySelector('.main_contents').innerHTML;
   object_dict[href] = obj;
   history.pushState(null, null, href);
 }
 
-window.addEventListener('popstate', e => {
-  const back_url = location.pathname;
-  const back_obj = object_dict[back_url];
-  $('.main_contents').html(back_obj);
-});
+function ajax_data(href, data) {
+  $.ajax({
+    url: '/pjax',
+    type: 'GET',
+    data: data,
+    dataType: 'json',
+  })
+    .done(function (response) {
+      $('.main_contents').html(response.html);
+      if (document.querySelector('.chat_remove') != null) {
+        document.querySelector('.chat_remove').remove();
+      }
+      pjax_dict(href);
+    })
+    .fail(function (response) {
+      console.log(response);
+    })
+}
 
 
 // pjax_button ボタンを押した時の処理
@@ -47,73 +66,28 @@ $(document).on('click', '.pjax_button_userpage', function (event) {
   event.preventDefault();
   const href = $(this).attr('href');
   const nickname = $(this).attr('data');
-  $.ajax({
-    url: '/pjax',
-    type: 'GET',
-    data: { 'href': href, 'nickname': nickname },
-    dataType: 'json',
-  })
-    .done(function (response) {
-      $('.main_contents').html(response.html);
-      if (document.querySelector('.chat_remove') != null) {
-        document.querySelector('.chat_remove').remove();
-      }
-      pjax_dict(href);
-    })
-    .fail(function (response) {
-      console.log(response);
-    })
+  data = { 'href': href, 'nickname': nickname }
+  ajax_data(href, data)
 });
 
 
 // 検索ボタンを押した時の処理
 $(document).on('click', '.search_icon', function (event) {
   event.preventDefault();
-  const path = location.pathname;
+  const href = location.pathname;
   const search = document.querySelector('.search_input').value;
-  const url = `${path}?search=${search}`
-  $.ajax({
-    url: '/pjax',
-    type: 'GET',
-    data: { 'href': path, 'search': search },
-    dataType: 'json',
-    timeout: 10000,
-  })
-    .done(function (response) {
-      $('.main_contents').html(response.html);
-      if (document.querySelector('.chat_remove') != null) {
-        document.querySelector('.chat_remove').remove();
-      }
-      pjax_dict(url);
-    })
-    .fail(function (response) {
-      console.log(response);
-    })
+  const url = `${href}?search=${search}`
+  data = { 'href': href, 'search': search }
+  ajax_data(url, data)
 });
 
 
 // 検索タグを押した時の処理
 $(document).on('click', '.pjax_search_button', function (event) {
   event.preventDefault();
-  const path = location.pathname;
-  const href = $(this).attr('href');
-  const url = `${path}${href}`
+  const href = location.pathname;
   const search = $(this).attr('search');
-  $.ajax({
-    url: '/pjax',
-    type: 'GET',
-    data: { 'href': path, 'search': search },
-    dataType: 'json',
-    timeout: 10000,
-  })
-    .done(function (response) {
-      $('.main_contents').html(response.html);
-      if (document.querySelector('.chat_remove') != null) {
-        document.querySelector('.chat_remove').remove();
-      }
-      pjax_dict(url);
-    })
-    .fail(function (response) {
-      console.log(response);
-    })
+  const url = `${href}?search=${search}`
+  data = { 'href': href, 'search': search }
+  ajax_data(url, data)
 });
