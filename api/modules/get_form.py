d553b13b475767f25e4c41bd14e18de258c57e59
ANFINII <anfinii19870506@gmail.com>
2022-07-30 20:29:26 +0900

    get_form.pyの修正

--- api/modules/get_form.py
+++ api/modules/get_form.py
@@ -1,14 +1,34 @@
-from django.shortcuts import get_object_or_404
-from api.models import Notification
+from datetime import datetime, timezone, timedelta
+from api.models import Notification, AccessLog
 from api.modules.contains import NotificationTypeNo, models_dict
 
 
-def get_detail(self):
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip_address = x_forwarded_for.split(',')[0]
+    else:
+        ip_address = request.META.get('REMOTE_ADDR')
+    return ip_address
+
+
+def get_detail(self, request):
     self.object = self.get_object()
     obj = self.object
-    obj.read += 1
-    obj.save(update_fields=['read'])
     obj_class = obj.__class__
+    obj_name = obj_class.__name__
+    ip_address = get_client_ip(request)
+    access_log_obj = AccessLog.objects.filter(ip_address=ip_address, type=obj_name, type_id=obj.id).first()
+    JST = timezone(timedelta(hours=9), 'JST')
+    if access_log_obj:
+        if datetime.now(JST) - access_log_obj.updated > timedelta(hours=2):
+            obj.read += 1
+            obj.save(update_fields=['read'])
+            access_log_obj.save(update_fields=['updated'])
+    else:
+        AccessLog.objects.create(ip_address=ip_address, type=obj_name, type_id=obj.id)
+        obj.read += 1
+        obj.save(update_fields=['read'])
     author = obj.author
     context = self.get_context_data(object=obj)
     if obj.read == 10000:
