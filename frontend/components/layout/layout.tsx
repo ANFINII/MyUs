e2c140e00fd58009241ccc03fc2e52535093b500
ANFINII <anfinii19870506@gmail.com>
2022-09-02 09:06:54 +0900

    Head追加

--- frontend/components/layout/layout.tsx
+++ frontend/components/layout/layout.tsx
@@ -8,13 +8,34 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <Head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="description" content="MyUsとはMy Idea Know Usが名前の由来です。総合メディアサイトであり、アイディア想像サイトになります!!!"/>
+        <meta name="keywords" content="MyUs, My Idea Know Us, SNS"/>
+        <meta name="author" content="ANFINII"/>
+        <meta name="generator" content="Django"/>
+        <meta name="application-name" content="MyUs"/>
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-title" content="MyUs"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta property="og:title" content="My Idea Know Us"/>
+        <meta property="og:site_name" content="MyUs"/>
+        <meta property="og:url" content="https://www.myus.com"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="{'img/My_Idea_Know_Us.png'}"/>
+        <meta property="og:description" content="MyUsとはMy Idea Know Usが名前の由来です。総合メディアサイトであり、アイディア想像サイトになります!!!"/>
+        <link rel="shortcut icon" href="{'img/MyUs.png'}"/>
+        <link rel="icon" type="image/png" href="{'img/MyUs.png'}"/>
+        <link rel="apple-touch-icon" href="{'img/MyUs.png'}"/>
         <title>MyUs</title>
       </Head>
       <Header />
       <SideBar />
       <SerchTag />
-      <main>
-        {children}
+      <main className="main_contents">
+        { children }
       </main>
     </>
   )
