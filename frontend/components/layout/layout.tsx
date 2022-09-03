e28fc970949f096db84f8b7d213d9151f9d8a863
ANFINII <anfinii19870506@gmail.com>
2022-09-03 14:47:57 +0900

    Next faviconの追加

--- frontend/components/layout/layout.tsx
+++ frontend/components/layout/layout.tsx
@@ -26,9 +26,14 @@ export default function Layout({ children }: Props) {
         <meta property="og:type" content="website"/>
         <meta property="og:image" content="{'img/My_Idea_Know_Us.png'}"/>
         <meta property="og:description" content="MyUsとはMy Idea Know Usが名前の由来です。総合メディアサイトであり、アイディア想像サイトになります!!!"/>
-        <link rel="shortcut icon" href="{'img/MyUs.png'}"/>
-        <link rel="icon" type="image/png" href="{'img/MyUs.png'}"/>
-        <link rel="apple-touch-icon" href="{'img/MyUs.png'}"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="manifest" href="/favicon/site.webmanifest"/>
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000"/>
+        <meta name="theme-color" content="#ffffff"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
         <title>MyUs</title>
       </Head>
       <Header/>
