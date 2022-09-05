3a2f282bb9c2d08b42aad50c29f84a25d900fd73
ANFINII <anfinii19870506@gmail.com>
2022-09-06 08:23:17 +0900

    collaboのリストと作成画面追加

--- frontend/pages/collabo/index.tsx
+++ frontend/pages/collabo/index.tsx
@@ -1,3 +1,27 @@
+import AuthorSpace from 'components/elements/author_space'
+import ContentTitle from 'components/elements/content_title'
+
 export default function Collabo() {
-  return <h1>Collabo</h1>
+  return (
+    <>
+      <h1>Collabo
+        {/* {% if query %} */}
+        {/* <section className="messages_search">「{ query }」の検索結果「{{ count }}」件</section> */}
+        {/* {% endif %} */}
+      </h1>
+
+      <article className="main_article">
+        {/* {% for item in collabo_list %} */}
+        <section className="main_content_other">
+          <div className="main_decolation">
+            <a href="{% url 'myus:collabo_detail' item.pk item.title %}" className="author_space">
+              <AuthorSpace/>
+              <ContentTitle/>
+            </a>
+          </div>
+        </section>
+        {/* {% endfor %} */}
+      </article>
+    </>
+  )
 }
