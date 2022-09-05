630264fc6b6c4d144e5a5d15bd124c78cb080e23
ANFINII <anfinii19870506@gmail.com>
2022-09-06 08:19:46 +0900

    ディレクトリの配置変更

---
+++ frontend/components/layouts/header/side_menu.tsx
@@ -0,0 +1,92 @@
+import Link from 'next/link'
+
+export default function SideMenu() {
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <>
+      <input type="checkbox" id="burger_menu" className="side_menu"/>
+      <label htmlFor="burger_menu" className="side_open">
+        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="1.7em" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </label>
+      <label htmlFor="burger_menu" className="side_nemu_back"></label>
+
+      <aside className="side_menu">
+        <label htmlFor="burger_menu" className="side_close color_side_menu">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-arrow-left color_side_nemu_bi" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+          </svg>
+          <img src="img/MyUs.png" width="30em" height="30em"/>
+        </label>
+
+        <nav>
+          <div className="side_menu_item side_nemu_indent color_side_menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-house-door color_side_nemu_bi" viewBox="0 0 16 16">
+              <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+            </svg>
+            <div>ホーム</div>
+            <Link href="/">
+              <a className="icon_link" role="button" aria-disabled="true"></a>
+            </Link>
+          </div>
+
+          <div className="side_menu_item side_nemu_indent color_side_menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-lightning-charge color_side_nemu_bi" viewBox="0 0 16 16">
+              <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41 4.157 8.5z"/>
+            </svg>
+            <div>急上昇</div>
+            <Link href="/recommend">
+              <a className="icon_link" role="button" aria-disabled="true"></a>
+            </Link>
+          </div>
+
+          <div className="side_menu_item side_nemu_indent color_side_menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-person-check color_side_nemu_bi" viewBox="0 0 16 16">
+              <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+              <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            </svg>
+            <div>フォロー</div>
+            <Link href="/follow/follow" as="/follow">
+              <a className="icon_link" role="button" aria-disabled="true"></a>
+            </Link>
+          </div>
+        </nav>
+
+        <footer className="side_menu_footer">
+          <nav>
+            <div className="side_menu_item side_nemu_indent color_side_menu">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-file-earmark-text color_side_nemu_bi" viewBox="0 0 16 16">
+                <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+              </svg>
+              <div>利用規約</div>
+              <Link href="/common/userpolicy" as="/userpolicy">
+                <a className="icon_link" role="button" aria-disabled="true"></a>
+              </Link>
+            </div>
+
+            <div className="side_menu_item side_nemu_indent color_side_menu">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-globe color_side_nemu_bi" viewBox="0 0 16 16">
+                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
+              </svg>
+              <div>Knowledge Base</div>
+              <Link href="/common/knowledge" as="/knowledge">
+                <a className="icon_link" role="button" aria-disabled="true"></a>
+              </Link>
+            </div>
+
+            <div className="side_menu_footer_item side_nemu_indent color_side_menu">
+              <img src="img/MyUs.png" width="24px" height="24px" className="myus_img"/>
+              <div className="side_menu_footer_item">© {year} MyUs Co.,Ltd</div>
+              <Link href="/">
+                <a className="icon_link" role="button" aria-disabled="true"></a>
+              </Link>
+            </div>
+          </nav>
+        </footer>
+      </aside>
+    </>
+  )
+}
--- frontend/components/layouts/layout.tsx
+++ frontend/components/layouts/layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Header from 'components/layout/header'
-import SideBar from 'components/layout/sidebar'
-import SearchTag from 'components/layout/searchtag'
+import Header from 'components/layouts/header/header'
+import SideBar from 'components/layouts/sidebar'
+import SearchTag from 'components/layouts/searchtag'
 type Props = {children: React.ReactNode;}
 
 export default function Layout({ children }: Props) {
@@ -24,16 +24,16 @@ export default function Layout({ children }: Props) {
         <meta property="og:site_name" content="MyUs"/>
         <meta property="og:url" content="https://www.myus.com"/>
         <meta property="og:type" content="website"/>
-        <meta property="og:image" content="img/My_Idea_Know_Us.png"/>
+        <meta property="og:image" content="/img/My_Idea_Know_Us.png"/>
         <meta property="og:description" content="MyUsとはMy Idea Know Usが名前の由来です。総合メディアサイトであり、アイディア想像サイトになります!!!"/>
+        <meta name="theme-color" content="#ffffff"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
         <link rel="manifest" href="/favicon/site.webmanifest"/>
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000"/>
-        <meta name="theme-color" content="#ffffff"/>
-        <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
         <title>MyUs</title>
       </Head>
       <Header/>
