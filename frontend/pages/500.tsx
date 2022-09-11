f071a7289ccb538b1a2072c36017c934d8a827dd
ANFINII <anfinii19870506@gmail.com>
2022-09-11 21:19:44 +0900

    Next 500 page 追加

--- frontend/pages/500.tsx
+++ frontend/pages/500.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 import Footer from 'components/layouts/footer'
 
-export default function Custom404() {
+export default function Custom500() {
   return (
     <article className="article_pass">
       <Head>
-        <title>MyUs404</title>
+        <title>MyUs500</title>
       </Head>
 
-      <h1>404 Not Found</h1>
-      <h2 className="login_required">ページが見つかりませんでした!</h2>
+      <h1>500 Server Error</h1>
+      <h2 className="login_required">このページは動作しませんでした!</h2>
 
       <Footer/>
     </article>
