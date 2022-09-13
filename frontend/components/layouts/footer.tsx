a9c0f4b834699238991fddb3884b0edca5737725
ANFINII <anfinii19870506@gmail.com>
2022-09-14 00:14:23 +0900

    css footer クラス名変更

--- frontend/components/layouts/footer.tsx
+++ frontend/components/layouts/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const today = new Date();
   const year = today.getFullYear();
   return (
-    <footer className="footer-myus" style={{textAlign: "center"}}>
+    <footer className="footer" style={{textAlign: "center"}}>
       <small>© {year} MyUs Co.,Ltd</small>
     </footer>
   )
