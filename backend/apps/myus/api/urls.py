cb46dbda367636b3b58db0374826bd5a86dbc92a
ANFINII <anfinii19870506@gmail.com>
2022-09-20 00:59:36 +0900

    url api 修正

--- backend/apps/myus/api/urls.py
+++ backend/apps/myus/api/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from apps.myus.api.views import UserAPI
-from apps.myus.api.views import LoginAPI, SignUpAPI, CustomAuthToken
+from apps.myus.api.views import SignUpAPI, UserAPI, UserAPIView, LoginView
+
 
 app_name = 'myus'
 
+
 urlpatterns = [
-    path('user', UserAPI.as_view(), name='user'),
-    path('signupapi', SignUpAPI.as_view(), name='signupapi'),
-    path('loginapi', LoginAPI.as_view(), name='loginapi'),
-    path('auth/login', CustomAuthToken.as_view()),
+    # path('api/login/', TokenObtainPairView.as_view()),
+    path('api/refresh/', TokenRefreshView.as_view()),
+    path('api/verify/', TokenVerifyView.as_view()),
+    path('api/user', UserAPI.as_view(), name='userapi'),
+    path('api/UserAPI', UserAPIView.as_view(), name='UserAPIView'),
+    path('api/signup', SignUpAPI.as_view(), name='signupapi'),
+    path('api/login', LoginView.as_view(), name='login'),
 ]
