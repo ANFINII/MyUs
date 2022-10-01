c45d4c59fbfa8193d777929021443ee550d49010
ANFINII <anfinii19870506@gmail.com>
2022-10-01 18:12:22 +0900

    urls.py 修正

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-from apps.api.views import UserAPI, SignUpAPI, LoginAPI, LogoutAPI
-from apps.api.views import IndexAPI
+from apps.api.views import SignUpAPI, LoginAPI, LogoutAPI, VerifyAPI, RefreshAPI
+from apps.api.views import UserAPI, MyPageAPI
 
 
-app_name = 'myus'
+app_name = 'api'
 
 urlpatterns = [
     path('user', UserAPI.as_view()),
+    path('mypage', MyPageAPI.as_view()),
     path('signup', SignUpAPI.as_view()),
     path('login', LoginAPI.as_view()),
     path('logout', LogoutAPI.as_view()),
-    path('verify', TokenVerifyView.as_view()),
-    path('refresh', TokenRefreshView.as_view()),
+    path('verify', VerifyAPI.as_view()),
+    path('refresh', RefreshAPI.as_view()),
 ]
