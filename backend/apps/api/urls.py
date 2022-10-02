f03b60a73b51a22e115e33fc46a093a27dcde850
ANFINII <anfinii19870506@gmail.com>
2022-10-02 13:03:55 +0900

    api urls リファクタリング

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from apps.api.views import SignUpAPI, LoginAPI, LogoutAPI, VerifyAPI, RefreshAPI
-from apps.api.views import UserAPI, MyPageAPI
+from apps.api.views import AuthAPI, SignUpAPI, LoginAPI, LogoutAPI, RefreshAPI
+from apps.api.views import ProfileAPI, MyPageAPI
 
 
 app_name = 'api'
 
 urlpatterns = [
-    path('user', UserAPI.as_view()),
-    path('mypage', MyPageAPI.as_view()),
+    path('auth', AuthAPI.as_view()),
     path('signup', SignUpAPI.as_view()),
     path('login', LoginAPI.as_view()),
     path('logout', LogoutAPI.as_view()),
-    path('verify', VerifyAPI.as_view()),
     path('refresh', RefreshAPI.as_view()),
+
+    path('profile', ProfileAPI.as_view()),
+    path('mypage', MyPageAPI.as_view()),
 ]
