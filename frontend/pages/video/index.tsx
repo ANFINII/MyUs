01db31bfc0b1c8a71faf780be32cf863753583dc
ANFINII <anfinii19870506@gmail.com>
2022-10-23 00:51:06 +0900

    Next video list add

--- frontend/pages/video/index.tsx
+++ frontend/pages/video/index.tsx
@@ -1,7 +1,22 @@
 import Head from 'next/head'
 import VideoArticle from 'components/elements/article/video'
+import { GetServerSideProps } from 'next'
 
-export default function Video(props) {
+export async function getVideoList() {
+  const BASEURL = process.env.NEXT_PUBLIC_API_URL
+  const res = await fetch(BASEURL + '/api/video')
+  const data = await res.json()
+  return data
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await getVideoList()
+  return {
+    props: { data },
+  };
+}
+
+export default function Video(data: any) {
   return (
     <>
       <Head>
@@ -10,13 +25,13 @@ export default function Video(props) {
 
       <h1>Video
         {/* {% if query %} */}
-        <section className="search_message">
+        {/* <section className="search_message"> */}
           {/* 「{{ props.query }}」の検索結果「{{ props.count }}」件 */}
-        </section>
+        {/* </section> */}
         {/* {% endif %} */}
       </h1>
 
-      <VideoArticle/>
+      <VideoArticle datas={data} />
 
       {/* {% block extrajs %}
       <script src="https://vjs.zencdn.net/7.19.2/video.min.js"></script>
