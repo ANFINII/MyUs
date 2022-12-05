ee8bf279933e48d8587f84ad3c57dfc0949733af
ANFINII <anfinii19870506@gmail.com>
2022-12-06 00:30:51 +0900

    consumers.py リファクタリング

--- backend/apps/myus/consumers.py
+++ backend/apps/myus/consumers.py
@@ -3,24 +3,30 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth import get_user_model
 from django.template.loader import render_to_string
-from django.template.defaultfilters import linebreaksbr
-from apps.myus.views import ChatDetail, ChatThread
-from apps.myus.models import Chat, Comment, Notification
+from apps.myus.views import ChatDetail, ChatThread, get_delta
+from apps.myus.models import Chat, Message, Notification
 from apps.myus.modules.contains import NotificationTypeNo
 
 User = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
+
     def create_message(self, data):
-        obj = Chat.objects.get(id=data['obj_id'])
-        message = Comment.objects.create(
+        chat = Chat.objects.get(id=data['chat_id'])
+        delta = data['delta']
+        html = data['message']
+        print('delta+++++++', delta)
+        print('html=========', html)
+        print('get_delta++======', get_delta(delta, html))
+        message = Message.objects.create(
+            chat=chat,
             author=self.scope['user'],
-            text=data['message'],
-            content_object=obj,
+            text=html,
+            delta=get_delta(delta, html),
         )
-        obj.thread = obj.comment.filter(parent__isnull=True).count()
-        obj.joined = obj.comment.values_list('author').distinct().count()
-        obj.save(update_fields=['thread', 'joined'])
+        chat.thread = chat.message.filter(parent__isnull=True).count()
+        chat.joined = chat.message.values_list('author').distinct().count()
+        chat.save(update_fields=['thread', 'joined'])
         content = {
             'command': 'create_message',
             'message': self.create_message_to_json(message)
@@ -29,19 +35,19 @@ class ChatConsumer(WebsocketConsumer):
 
     def create_reply_message(self, data):
         user = self.scope['user']
-        obj = Chat.objects.get(id=data['obj_id'])
-        message = Comment.objects.create(
+        chat = Chat.objects.get(id=data['chat_id'])
+        message = Message.objects.create(
+            chat=chat,
             author=user,
             text=data['message'],
-            content_object=obj,
             parent_id=data['parent_id'],
         )
         author = message.parent.author
-        obj.joined = obj.comment.values_list('author').distinct().count()
-        obj.save(update_fields=['joined'])
-        parent_obj = Comment.objects.get(id=data['parent_id'])
-        parent_obj.reply_num = Comment.objects.filter(parent=data['parent_id']).count()
-        parent_obj.save(update_fields=['reply_num'])
+        chat.joined = chat.message.values_list('author').distinct().count()
+        chat.save(update_fields=['joined'])
+        parent = Message.objects.get(id=data['parent_id'])
+        parent.reply_num = Message.objects.filter(parent=data['parent_id']).count()
+        parent.save(update_fields=['reply_num'])
         if user != author and author.notificationsetting.is_reply:
             Notification.objects.create(
                 user_from=user,
@@ -57,7 +63,7 @@ class ChatConsumer(WebsocketConsumer):
         return self.send_chat_message(content)
 
     def update_message(self, data):
-        message = Comment.objects.get(id=data['comment_id'])
+        message = Message.objects.get(id=data['message_id'])
         message.text = data['message']
         message.save(update_fields=['text', 'updated'])
         content = {
@@ -67,37 +73,37 @@ class ChatConsumer(WebsocketConsumer):
         return self.send_chat_message(content)
 
     def delete_message(self, data):
-        message = Comment.objects.get(id=data['comment_id'])
+        message = Message.objects.get(id=data['message_id'])
         content = {
             'command': 'delete_message',
             'message': self.delete_message_to_json(message)
         }
         message_dict = content['message']
-        message_dict['comment_id'] = data['comment_id']
+        message_dict['message_id'] = data['message_id']
         return self.send_chat_message(content)
 
     def delete_reply_message(self, data):
-        message = Comment.objects.get(id=data['comment_id'])
+        message = Message.objects.get(id=data['message_id'])
         content = {
             'command': 'delete_reply_message',
             'message': self.delete_reply_message_to_json(message)
         }
         message_dict = content['message']
-        message_dict['comment_id'] = data['comment_id']
+        message_dict['message_id'] = data['message_id']
         return self.send_chat_message(content)
 
     def create_message_to_json(self, message):
         message_data = ChatDetail.get_message_data(self, message)
         context = {
             'user_id': self.scope['user'].id,
-            'comment_id': message.id,
+            'message_id': message.id,
             'joined': message_data['joined'],
             'thread': message_data['thread'],
-            'comment_lists': render_to_string('chat/chat_comment/chat_comment.html', {
+            'message_lists': render_to_string('chat/chat_message/chat_message.html', {
                 'user_id': self.scope['user'].id,
-                'obj_id': message.object_id,
-                'comment_id': message.id,
-                'comment_list': message_data['comment_obj'],
+                'chat_id': message.chat_id,
+                'message_id': message.id,
+                'message_list': message_data['message'],
             })
         }
         return context
@@ -106,60 +112,63 @@ class ChatConsumer(WebsocketConsumer):
         reply_data = ChatThread.get_reply_data(self, message)
         context = {
             'user_id': self.scope['user'].id,
-            'comment_id': message.id,
+            'message_id': message.id,
             'parent_id': message.parent.id,
             'joined': reply_data['joined'],
             'reply_num': reply_data['reply_num'],
             'reply_lists': render_to_string('chat/chat_reply/chat_reply.html', {
                 'user_id': self.scope['user'].id,
-                'obj_id': message.object_id,
-                'reply_list': reply_data['reply_obj'],
+                'chat_id': message.chat_id,
+                'reply_list': reply_data['reply'],
             })
         }
         return context
 
     def update_message_to_json(self, message):
         context = {
-            'text': linebreaksbr(message.text),
-            'comment_id': message.id,
+            'text': message.text,
+            'message_id': message.id,
         }
         return context
 
     def delete_message_to_json(self, message):
         message.delete()
-        obj = Chat.objects.get(id=message.object_id)
-        obj.thread = obj.comment.filter(parent__isnull=True).count()
-        obj.joined = obj.comment.values_list('author').distinct().count()
-        obj.save(update_fields=['thread', 'joined'])
+        chat = Chat.objects.get(id=message.chat_id)
+        chat.thread = chat.message.filter(parent__isnull=True).count()
+        chat.joined = chat.message.values_list('author').distinct().count()
+        chat.save(update_fields=['thread', 'joined'])
         context = {
-            'joined': obj.joined,
-            'thread': obj.thread,
+            'joined': chat.joined,
+            'thread': chat.thread,
         }
         return context
 
     def delete_reply_message_to_json(self, message):
         Notification.objects.filter(type_no=NotificationTypeNo.reply, object_id=message.id).delete()
         message.delete()
-        obj = Chat.objects.get(id=message.object_id)
-        obj.joined = obj.comment.values_list('author').distinct().count()
-        obj.save(update_fields=['joined'])
-        comment_obj = Comment.objects.get(id=message.parent_id)
-        comment_obj.reply_num = Comment.objects.filter(parent=message.parent_id).count()
-        comment_obj.save(update_fields=['reply_num'])
+        chat = Chat.objects.get(id=message.id)
+        chat.joined = chat.message.values_list('author').distinct().count()
+        chat.save(update_fields=['joined'])
+        message = Message.objects.get(id=message.parent_id)
+        message.reply_num = Message.objects.filter(parent=message.parent_id).count()
+        message.save(update_fields=['reply_num'])
         context = {
-            'joined': obj.joined,
+            'joined': chat.joined,
             'parent_id': message.parent_id,
-            'reply_num': comment_obj.reply_num,
+            'reply_num': message.reply_num,
         }
         return context
 
-    commands = {
-        'create_message': create_message,
-        'create_reply_message': create_reply_message,
-        'update_message': update_message,
-        'delete_message': delete_message,
-        'delete_reply_message': delete_reply_message,
-    }
+
+
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {'type': 'chat_message', 'message': message}
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
 
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['pk']
@@ -180,11 +189,10 @@ class ChatConsumer(WebsocketConsumer):
         data = json.loads(text_data)
         self.commands[data['command']](self, data)
 
-    def send_chat_message(self, message):
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {'type': 'chat_message', 'message': message}
-        )
-
-    def chat_message(self, event):
-        message = event['message']
-        self.send(text_data=json.dumps(message))
+    commands = {
+        'create_message': create_message,
+        'create_reply_message': create_reply_message,
+        'update_message': update_message,
+        'delete_message': delete_message,
+        'delete_reply_message': delete_reply_message,
+    }
