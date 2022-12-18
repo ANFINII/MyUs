6d0bcb26a93b6ecbc9258e93fcec1bbba1a977f8
ANFINII <anfinii19870506@gmail.com>
2022-12-18 22:12:27 +0900

    next version up add src dhirectory

--- frontend/components/layouts/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  const today = new Date();
-  const year = today.getFullYear();
-  return (
-    <footer className="footer" style={{textAlign: "center"}}>
-      <small>© {year} MyUs Co.,Ltd</small>
-    </footer>
-  )
-}
