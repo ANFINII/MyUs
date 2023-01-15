751174899592208d9534f5ad1087f0af8ee54771
ANFINII <anfinii19870506@gmail.com>
2023-01-16 02:32:37 +0900

    footer fix

--- frontend/src/components/layouts/Footer/index.tsx
+++ frontend/src/components/layouts/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const today = new Date()
   const year = today.getFullYear()
   return (
-    <footer className="footer" style={{textAlign: "center"}}>
+    <footer className="footer">
       <small>© {year} MyUs Co.,Ltd</small>
     </footer>
   )
