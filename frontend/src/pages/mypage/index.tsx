a8e8f52c6d01274e6ef6bb2168651f82e830272d
ANFINII <anfinii19870506@gmail.com>
2023-01-17 02:31:35 +0900

    mypage page

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import config from 'api/config'
 import {MypageResponse} from 'utils/type'
 import Footer from 'components/layouts/Footer'
+import Button from 'components/parts/Button'
 
 // export const getServerSideProps: GetServerSideProps = async (context) => {
 //   const cookie = context.req?.headers.cookie
@@ -42,12 +43,10 @@ export default function MyPage() {
           <h1>Myページ設定</h1>
           <div className="btn-column">
             <div className="btn-column1">
-              <Link href="/mypage/update" className="btn btn-success btn-sm">編集</Link>
+              <Link href="/mypage/update"><Button blue size='xs'>編集</Button></Link>
             </div>
             <div className="btn-column2">
-              <Link href={`/userpage/${mypage.nickname}`} data-nickname={ mypage.nickname } className="btn btn-success btn-sm pjax_button_userpage">
-                ユーザページ
-              </Link>
+              <Link href={`/userpage/${mypage.nickname}`}><Button blue size='xs'>ユーザページ</Button></Link>
             </div>
           </div>
 
@@ -56,25 +55,26 @@ export default function MyPage() {
               <tr><td className="td-color">バナー画像</td>
                 <td>
                   <label htmlFor="account_image_input" className="mypage_image">
-                    <a href={bannerUrl} data-lightbox="group">
-                      <Image src={bannerUrl} title={ mypage.nickname } width={270} height={56} alt="" data-lightbox="group"/>
-                    </a>
+                    {mypage.banner &&
+                      <a href={bannerUrl} data-lightbox="group">
+                        <Image src={bannerUrl} title={mypage.nickname} width={270} height={56} alt="" data-lightbox="group"/>
+                      </a>
+                    }
                   </label>
                 </td>
               </tr>
-              <tr><td className="td-color">投稿者名</td><td className="td-indent">{ mypage.nickname }</td></tr>
-              <tr><td className="td-color">メールアドレス</td><td className="td-indent">{ mypage.email }</td></tr>
-              <tr><td className="td-color">フォロー数</td><td className="td-indent">{ mypage.following_num }</td></tr>
-              <tr><td className="td-color">フォロワー数</td><td className="td-indent">{ mypage.follower_num }</td></tr>
-              <tr><td className="td-color">料金プラン</td><td className="td-indent">{ mypage.plan }</td></tr>
+              <tr><td className="td-color">投稿者名</td><td className="td-indent">{mypage.nickname}</td></tr>
+              <tr><td className="td-color">メールアドレス</td><td className="td-indent">{mypage.email}</td></tr>
+              <tr><td className="td-color">フォロー数</td><td className="td-indent">{mypage.following_num}</td></tr>
+              <tr><td className="td-color">フォロワー数</td><td className="td-indent">{mypage.follower_num}</td></tr>
+              <tr><td className="td-color">料金プラン</td><td className="td-indent">{mypage.plan}</td></tr>
               <tr><td className="td-color">全体広告</td><td className="td-indent" id="toggle_mypage">
                 <form method="POST" action="" data-advertise="{{ mypage.is_advertise }}" data-csrf="{{ csrf_token }}">
-                  {mypage.plan === '0' &&
+                  {mypage.plan === '0' ?
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-toggle-disable" viewBox="0 0 16 16">
                       <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
                     </svg>
-                  }
-                  {mypage.is_advertise ?
+                  :mypage.is_advertise ?
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-toggle-on toggle_mypage" viewBox="0 0 16 16">
                       <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
                     </svg>
@@ -85,7 +85,7 @@ export default function MyPage() {
                   }
                 </form>
               </td></tr>
-              <tr><td className="td-color">概要</td><td className="td-indent">{ mypage.content }</td></tr>
+              <tr><td className="td-color">概要</td><td className="td-indent">{mypage.content}</td></tr>
             </tbody>
           </table>
 
--- frontend/src/pages/mypage/update.tsx
+++ frontend/src/pages/mypage/update.tsx
@@ -2,41 +2,52 @@ import {GetServerSideProps} from 'next'
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
-import Footer from 'components/layouts/Footer'
-import axios from 'api/axios'
+import config from 'api/config'
 import {MypageResponse} from 'utils/type'
+import Footer from 'components/layouts/Footer'
+import Button from 'components/parts/Button'
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const cookie = context.req?.headers.cookie
-  const res = await axios.get('/api/mypage', {
-    headers: {cookie: cookie!}
-  })
-  const data: MypageResponse = res.data
-  return {
-    props: {mypage: data}
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const cookie = context.req?.headers.cookie
+//   const res = await axios.get('/api/mypage', {
+//     headers: {cookie: cookie!}
+//   })
+//   const data: MypageResponse = res.data
+//   return {
+//     props: {mypage: data}
+//   }
+// }
+
+export default function MyPageUpdate() {
+  // const {banner, nickname, email, follower_num, following_num, plan, plan_date, is_advertise, content} = props
+  const mypage: MypageResponse = {
+    "banner": "/media/users/images_mypage/user_5/MyUs_banner.png",
+    "nickname": "アン",
+    "email": "abc@gmail.com",
+    "content": "MyUs開発者です。",
+    "follower_num": 0,
+    "following_num": 1,
+    "plan": "free",
+    "plan_date": "2021-12-31T15:00:00Z",
+    "is_advertise": true
   }
-}
 
-export default function MyPageUpdate(props: MypageResponse) {
-  const {banner, nickname, email, follower_num, following_num, plan, plan_date, is_advertise, content} = props
-
+  const bannerUrl = config.baseUrl + mypage.banner
   return (
     <>
       <Head>
         <title>MyUsマイページ設定</title>
       </Head>
 
-      {props ?
+      {mypage ?
         <article className="article_account">
           <h1>Myページ設定</h1>
           <div className="btn-column">
             <div className="btn-column1">
-              <Link href="/mypage/update">
-                {/* <a className="btn btn-success btn-sm" role="button">編集</a> */}
-              </Link>
+              <Button purple size="xs" type="submit">登録</Button>
             </div>
             <div className="btn-column2">
-              <Link href="/userpage/{user.nickname}" className="btn btn-success btn-sm pjax_button_userpage" data-nickname={nickname}></Link>
+              <Link href="/mypage"><Button blue size="xs">戻る</Button></Link>
             </div>
           </div>
 
@@ -45,27 +56,26 @@ export default function MyPageUpdate(props: MypageResponse) {
               <tr><td className="td-color">バナー画像</td>
                 <td>
                   <label htmlFor="account_image_input" className="mypage_image">
-                    {banner &&
-                      <a href={banner} data-lightbox="group">
-                        <Image src={banner} title={nickname} width={270} height={56} alt="" data-lightbox="group"/>
+                    {mypage.banner &&
+                      <a href={bannerUrl} data-lightbox="group">
+                        <Image src={bannerUrl} title={mypage.nickname} width={270} height={56} alt="" data-lightbox="group"/>
                       </a>
                     }
                   </label>
                 </td>
               </tr>
-              <tr><td className="td-color">投稿者名</td><td className="td-indent">{nickname}</td></tr>
-              <tr><td className="td-color">メールアドレス</td><td className="td-indent">{email}</td></tr>
-              <tr><td className="td-color">フォロー数</td><td className="td-indent">{following_num}</td></tr>
-              <tr><td className="td-color">フォロワー数</td><td className="td-indent">{follower_num}</td></tr>
-              <tr><td className="td-color">料金プラン</td><td className="td-indent">{plan}</td></tr>
+              <tr><td className="td-color">投稿者名</td><td className="td-indent">{mypage.nickname}</td></tr>
+              <tr><td className="td-color">メールアドレス</td><td className="td-indent"><input type="text" name="email" value={mypage.email} maxLength={120} className="form-control"/></td></tr>
+              <tr><td className="td-color">フォロー数</td><td className="td-indent">{mypage.following_num}</td></tr>
+              <tr><td className="td-color">フォロワー数</td><td className="td-indent">{mypage.follower_num}</td></tr>
+              <tr><td className="td-color">料金プラン</td><td className="td-indent">{mypage.plan}</td></tr>
               <tr><td className="td-color">全体広告</td><td className="td-indent" id="toggle_mypage">
                 <form method="POST" action="" data-advertise="{{is_advertise}}" ddata-csrf="{{csrf_token}}">
-                  {plan === '0' &&
+                  {mypage.plan === '0' ?
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-toggle-disable" viewBox="0 0 16 16">
                       <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
                     </svg>
-                  }
-                  {is_advertise ?
+                  :mypage.is_advertise ?
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-toggle-on toggle_mypage" viewBox="0 0 16 16">
                       <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
                     </svg>
@@ -76,7 +86,9 @@ export default function MyPageUpdate(props: MypageResponse) {
                   }
                 </form>
               </td></tr>
-              <tr><td className="td-color">概要</td><td className="td-indent">{content}</td></tr>
+              <tr><td className="td-color">概要</td><td>
+                <textarea name="introduction" rows={1} className="form-control textarea">{mypage.content}</textarea>
+              </td></tr>
             </tbody>
           </table>
 
