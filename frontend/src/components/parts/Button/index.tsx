226e275fdc2e82c942a4383e060cd4afa00acfed
ANFINII <anfinii19870506@gmail.com>
2023-01-17 02:29:43 +0900

    button components

--- frontend/src/components/parts/Button/index.tsx
+++ frontend/src/components/parts/Button/index.tsx
@@ -1,36 +1,82 @@
+import style from 'components/parts/Button/button.module.css'
+
 interface Color {
   blue?: boolean
-  green?: boolean
   purple?: boolean
   red?: boolean
+  green?: boolean
   light?: boolean
 }
 
 interface Props extends Color {
-  name: string
-  type?: 'submit' | 'reset' | 'button'
+  children?: string | string[]
   className?: string
   value?: string
-  onClick?: () => void
+  size?: 'xl' | 'xs'
+  type?: 'submit' | 'reset' | 'button'
+  onClick?(): void
 }
 
 export default function Button(props: Props) {
-  const {blue, green, purple, red, light} = props
-  const {name, type, className, value, onClick} = props
+  const {blue, purple, red, green, light} = props
+  const {children, className, type, value, size, onClick} = props
+
   return (
     <>
       {blue ?
-        <button type={type} value={value} className={`button_base blue ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+        className={`${style.button_base} ${style.blue} `
+          + (size === 'xl' ? `${style.xl} ` : '')
+          + (size === 'xs' ? `${style.xs} ` : '')
+          + (className ? className : '' )
+        }
+        >{children}
+        </button>
       :purple ?
-        <button type={type} value={value} className={`button_base purple ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+          className={`${style.button_base} ${style.purple} `
+            + (size === 'xl' ? `${style.xl} ` : '')
+            + (size === 'xs' ? `${style.xs} ` : '')
+            + (className ? className : '' )
+          }
+        >{children}
+        </button>
       :red ?
-        <button type={type} value={value} className={`button_base red ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+          className={`${style.button_base} ${style.red} `
+            + (size === 'xl' ? `${style.xl} ` : '')
+            + (size === 'xs' ? `${style.xs} ` : '')
+            + (className ? className : '' )
+          }
+        >{children}
+        </button>
       :green ?
-        <button type={type} value={value} className={`button_base green ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+          className={`${style.button_base} ${style.green} `
+            + (size === 'xl' ? `${style.xl} ` : '')
+            + (size === 'xs' ? `${style.xs} ` : '')
+            + (className ? className : '' )
+          }
+        >{children}
+        </button>
       :light ?
-        <button type={type} value={value} className={`button_base light ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+          className={`${style.button_base} ${style.light} `
+            + (size === 'xl' ? `${style.xl} ` : '')
+            + (size === 'xs' ? `${style.xs} ` : '')
+            + (className ? className : '' )
+          }
+        >{children}
+        </button>
       :
-        <button type={type} value={value} className={`button_base ${className}`} onClick={onClick}>{name}</button>
+        <button type={type} value={value} onClick={onClick}
+          className={`${style.button_base} `
+            + (size === 'xl' ? `${style.xl} ` : '')
+            + (size === 'xs' ? `${style.xs} ` : '')
+            + (className ? className : '' )
+          }
+        >{children}
+        </button>
       }
     </>
   )
