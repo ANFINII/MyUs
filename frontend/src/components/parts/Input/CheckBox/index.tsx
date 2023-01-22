f5fda592ca92ea2b47d91c5092aff8dd8a5ae25e
ANFINII <anfinii19870506@gmail.com>
2023-01-23 07:55:18 +0900

    add checkout component

---
+++ frontend/src/components/parts/Input/CheckBox/index.tsx
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import style from 'components/parts/Input/CheckBox/CheckBox.module.css'
+
+interface Props {
+  checked?: boolean
+  name?: string
+  id?: string
+  className?: string
+  children: React.ReactNode
+}
+
+export default function CheckBox(props: Props) {
+  const {checked, name, id, className, children} = props
+  const [checkValue, setCheckValue] = useState(checked)
+  const handleChange = () => {setCheckValue(false)}
+
+  return (
+    <>
+      {checkValue ?
+        <div className={style.check_group}>
+          <input type="checkbox" name={name} id={id} onClick={handleChange}
+            className={`${style.checkbox} ` + (className ? className : '' )} checked />
+          <label htmlFor={id}>{children}</label>
+        </div>
+      :
+        <div className={style.check_group}>
+          <input type="checkbox" name={name} id={id} onClick={handleChange}
+            className={`${style.checkbox} ` + (className ? className : '' )} />
+          <label htmlFor={id}>{children}</label>
+        </div>
+      }
+    </>
+  )
+}
