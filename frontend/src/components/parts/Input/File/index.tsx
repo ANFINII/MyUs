5352bb6126b3d7726bd189fae632dd6a4e73739e
ANFINII <anfinii19870506@gmail.com>
2023-01-31 08:59:28 +0900

    add Inputfile

--- frontend/src/components/parts/Input/File/index.tsx
+++ frontend/src/components/parts/Input/File/index.tsx
@@ -1,24 +1,28 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import style from 'components/parts/Input/File/File.module.css'
-import Input from 'components/parts/Input'
 
 interface Props {
-  checked?: boolean
-  name?: string
   id?: string
   className?: string
-  children: React.ReactNode
+  accept?: string
 }
 
-export default function CheckBox(props: Props) {
-  const {checked, name, id, className, children} = props
-  const [checkValue, setCheckValue] = useState(checked)
-  const handleChange = () => {setCheckValue(false)}
+export default function InputFile(props: Props) {
+  const {id, className, accept} = props
+  const inputEl = useRef<HTMLInputElement>(null)
+  const [fileName, setFileName] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFileName((e.target.value.replace('C:\\fakepath\\', '')))
+  }
+  const handleClick = () => inputEl.current.click()
 
   return (
     <>
-      <Input type="file" name="music" accept="audio/*" id="custom_file_1" className={style.none} required />
-      <Input type="text" id="file_1" placeholder="ファイル選択..." onClick={handleChange} />
+      <input type="file" id={id} accept={accept} ref={inputEl} onChange={handleChange} required hidden />
+      <input placeholder="ファイル選択..." value={fileName} onClick={handleClick}
+        className={`${style.input} ` + (className ? className : '' )}
+      />
     </>
   )
 }
--- frontend/src/components/parts/Input/index.tsx
+++ frontend/src/components/parts/Input/index.tsx
@@ -1,30 +1,28 @@
 import style from 'components/parts/Input/Input.module.css'
 
 interface Props {
-  type?: "text" | "file"
   name?: string
   value?: string
   placeholder?: string
   id?: string
   className?: string
-  accept?: string
   required?: boolean
   disabled?: boolean
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
-  onClick?: (event: React.MouseEventHandler<HTMLInputElement>) => void
+  onClick?: (event: React.MouseEvent<HTMLInputElement>) => void
 }
 
 export default function Input(props: Props) {
-  const {type, name, value, placeholder, id, className, accept, required, disabled, onChange, onClick} = props
+  const {name, value, placeholder, id, className, required, disabled, onChange, onClick} = props
   return (
     <>
       {required ?
-        <input type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} onClick={onClick} disabled={disabled}
-          id={id} className={`${style.input} ` + (className ? className : '' )} accept={accept} required
+        <input name={name} value={value} placeholder={placeholder} disabled={disabled} onChange={onChange} onClick={onClick}
+          id={id} className={`${style.input} ` + (className ? className : '' )} required
         />
       :
-        <input type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} onClick={onClick} disabled={disabled}
-          id={id} className={`${style.input} ` + (className ? className : '' )} accept={accept}
+        <input name={name} value={value} placeholder={placeholder} disabled={disabled} onChange={onChange} onClick={onClick}
+          id={id} className={`${style.input} ` + (className ? className : '' )}
         />
       }
     </>
