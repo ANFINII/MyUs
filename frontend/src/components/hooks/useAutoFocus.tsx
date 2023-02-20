0d73e06b958fa2274aa6449cf92452b736b768f3
ANFINII <anfinii19870506@gmail.com>
2023-02-21 08:55:44 +0900

    add autoFocus hooks

---
+++ frontend/src/components/hooks/useAutoFocus.tsx
@@ -0,0 +1,11 @@
+import {useRef, useEffect} from 'react'
+
+export default function useAutoFocus() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+  return inputRef
+}
