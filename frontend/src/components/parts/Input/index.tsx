954e1042400e2914b545f0a6dfc9db751f61fdea
ANFINII <anfinii19870506@gmail.com>
2023-02-21 08:54:46 +0900

    fix input.tsx

--- frontend/src/components/parts/Input/index.tsx
+++ frontend/src/components/parts/Input/index.tsx
@@ -1,30 +1,32 @@
 import style from 'components/parts/Input/Input.module.css'
+import useAutoFocus from 'components/hooks/useAutoFocus'
 
 interface Props {
+  type?: string
   name?: string
   value?: string
   placeholder?: string
   id?: string
   className?: string
+  minLength?: number
+  maxLength?: number
   required?: boolean
   disabled?: boolean
+  autoFocus?: boolean
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   onClick?: (event: React.MouseEvent<HTMLInputElement>) => void
 }
 
 export default function Input(props: Props) {
-  const {name, value, placeholder, id, className, required, disabled, onChange, onClick} = props
+  const {type, name, value, placeholder, id, className, onChange, onClick} = props
+  const {minLength, maxLength, required, disabled, autoFocus} = props
+  const inputFocus = useAutoFocus()
+
   return (
-    <>
-      {required ?
-        <input name={name} value={value} placeholder={placeholder} disabled={disabled} onChange={onChange} onClick={onClick}
-          id={id} className={`${style.input} ` + (className ? className : '' )} required
-        />
-      :
-        <input name={name} value={value} placeholder={placeholder} disabled={disabled} onChange={onChange} onClick={onClick}
-          id={id} className={`${style.input} ` + (className ? className : '' )}
-        />
-      }
-    </>
+    <input type={type} name={name} value={value} placeholder={placeholder}
+      minLength={minLength} maxLength={maxLength} required={required} disabled={disabled}
+      onChange={onChange} onClick={onClick} ref={autoFocus ? inputFocus : undefined}
+      id={id} className={`${style.input} ` + (className ? className : '' )}
+    />
   )
 }
