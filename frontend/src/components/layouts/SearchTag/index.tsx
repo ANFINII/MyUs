cb8a21f51ab935db38a368f24bd1fe67642cc020
ANFINII <anfinii19870506@gmail.com>
2023-02-25 23:42:20 +0900

    add square button

--- frontend/src/components/layouts/SearchTag/index.tsx
+++ frontend/src/components/layouts/SearchTag/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import {useState} from 'react'
+import ButtonSquare from 'components/parts/Button/Square'
 
 interface searchtag {
   name: string
@@ -12,28 +14,23 @@ interface Props {
 
 export default function SearchTag(props: Props) {
   const {isAuthenticated, csrfToken, searchtag} = props
+  const [isSearchtag, setIsSearchtag] = useState(false)
+  const handleSearchtag = () => {setIsSearchtag(!isSearchtag)}
 
   return (
     <nav className="searchtag">
-      <form method="POST" action="" id="tag_form" className="searchtag_grid">
-        <input type="checkbox" id="searchtag_add" className="searchtag_menu"/>
-        <label htmlFor="searchtag_add" className="searchtag_1"><span className="searchtag_center">タグ</span></label>
-        <input type="submit" className="searchtag_2 form_button" value="追加" data-csrf={csrfToken}/>
-        {isAuthenticated ?
-          <input type="text" name="searchtag" className="searchtag_3" maxLength={30} placeholder="タグ名"/>
-        :
-          <input type="text" name="searchtag" className="searchtag_3" maxLength={30} placeholder="タグ名" disabled/>
-        }
-        <div className="searchtag_n">
+      <form method="POST" action="" className="searchtag_grid">
+        <ButtonSquare emerald type="button" className={isSearchtag ? 'searchtag_1 active' : 'searchtag_1'} onClick={handleSearchtag}>タグ</ButtonSquare>
+        <ButtonSquare sakura className={isSearchtag ? 'searchtag_2 active' : 'searchtag_2'} data-csrf={csrfToken}>追加</ButtonSquare>
+        <input type="text" name="searchtag" className={isSearchtag ? 'searchtag_3 active' : 'searchtag_3'} maxLength={30} placeholder="タグ名" disabled={isAuthenticated}/>
+        <div className={isSearchtag ? 'searchtag_n active' : 'searchtag_n'}>
           <div className="searchtag_n_list">
-          {searchtag.map((tag: searchtag, index) => {
-            return (
-              <Link href={`?search=${tag.name}`} data-search={tag.name} key={index}>{tag.name}</Link>
-            )
-          })}
+            {searchtag.map((tag: searchtag, index) => {
+              return (<Link href={`?search=${tag.name}`} data-search={tag.name} key={index}>{tag.name}</Link>)
+            })}
           </div>
         </div>
-        <label htmlFor="searchtag_add" className="searchtag_4"><span className="searchtag_center">完了</span></label>
+        <ButtonSquare emerald type="button" className={isSearchtag ? 'searchtag_4 active' : 'searchtag_4'} onClick={handleSearchtag} data-csrf={csrfToken}>完了</ButtonSquare>
         <div className="searchtag_left">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
---
+++ frontend/src/components/parts/Button/Square/index.tsx
@@ -0,0 +1,33 @@
+import style from 'components/parts/Button/Square/Square.module.css'
+
+interface Color {
+  emerald?: boolean
+  red?: boolean
+  sakura?: boolean
+}
+
+interface Props extends Color {
+  type?: 'submit' | 'reset' | 'button'
+  value?: string
+  className?: string
+  children?: string | string[]
+  onClick?(): void
+}
+
+export default function ButtonSquare(props: Props) {
+  const {emerald, sakura, red} = props
+  const {type, value, className, children, onClick} = props
+
+  return (
+    <button type={type} value={value} onClick={onClick}
+      className={
+        `${style.button} `
+        + (emerald ? `${style.emerald} ` : '')
+        + (sakura ? `${style.sakura} ` : '')
+        + (red ? `${style.red} ` : '')
+        + (className ? className : '' )
+      }
+    >{children}
+    </button>
+  )
+}
