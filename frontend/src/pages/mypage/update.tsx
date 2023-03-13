8d45026fc5370e6aa1279e51f6dc959f456415d5
ANFINII <anfinii19870506@gmail.com>
2023-03-13 14:07:52 +0900

    fix mypage_update

--- frontend/src/pages/mypage/update.tsx
+++ frontend/src/pages/mypage/update.tsx
@@ -29,12 +29,11 @@ export default function MyPageUpdate() {
     "content": "MyUs開発者です。",
     "follower_num": 0,
     "following_num": 1,
-    "plan": "free",
+    "plan": "Free",
     "plan_date": "2021-12-31T15:00:00Z",
     "is_advertise": true
   }
 
-  const bannerUrl = config.baseUrl + mypage.banner
   return (
     <>
       <Head>
@@ -57,12 +56,12 @@ export default function MyPageUpdate() {
             <tbody>
               <tr><td className="td-color">バナー画像</td>
                 <td>
-                  <label htmlFor="account_image_input" className="mypage_image">
-                    {mypage.banner &&
-                      <a href={bannerUrl} data-lightbox="group">
-                        <Image src={bannerUrl} title={mypage.nickname} width={270} height={56} alt="" data-lightbox="group"/>
-                      </a>
-                    }
+                  <label htmlFor="account_image_input" className="update_account_image">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="3.5em" height="3.5em" fill="currentColor" className="bi bi-image" viewBox="0 0 16 16">
+                      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                      <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+                    </svg>
+                    <input type="file" name="banner" accept="image/*" id="account_image_input" className="custom-file-input" />
                   </label>
                 </td>
               </tr>
