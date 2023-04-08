930181ba278ffd6de4700cb4ba5d3f6f394f1af7
ANFINII <anfinii19870506@gmail.com>
2023-04-08 12:57:26 +0900

    add jquery

--- backend/static/js/textarea.js
+++ backend/static/js/textarea.js
@@ -1,37 +1,41 @@
-$(function () {
-  const target = $('.textarea');
-  const rawTarget = target.get(0);
-  let lineHeight = Number(target.attr('rows'));
-  while (rawTarget.scrollHeight > rawTarget.offsetHeight) {
-    lineHeight++;
-    target.attr('rows', lineHeight);
+window.addEventListener('DOMContentLoaded', function () {
+  const target = $('.textarea')
+  const rawTarget = target.get(0)
+  let lineHeight = Number(target.attr('rows'))
+  if (rawTarget) {
+    while (rawTarget.scrollHeight > rawTarget.offsetHeight) {
+      lineHeight++
+      target.attr('rows', lineHeight)
+    }
   }
-});
+})
 
-$(function () {
-  const $textarea = $('.textarea');
-  const lineHeight = parseInt($textarea.css('lineHeight'));
+window.addEventListener('DOMContentLoaded', function () {
+  const $textarea = $('.textarea')
+  const lineHeight = parseInt($textarea.css('lineHeight'))
   $textarea.on('input', function () {
-    const lines = ($(this).val() + '\n').match(/\n/g).length;
-    $(this).height(lineHeight * lines);
-  });
-});
+    const lines = ($(this).val() + '\n').match(/\n/g).length
+    $(this).height(lineHeight * lines)
+  })
+})
 
-$(function () {
-  const target = $('.textarea_br');
-  const rawTarget = target.get(0);
-  let lineHeight = Number(target.attr('rows'));
-  while (rawTarget.scrollHeight > rawTarget.offsetHeight) {
-    lineHeight++;
-    target.attr('rows', lineHeight);
+window.addEventListener('DOMContentLoaded', function () {
+  const target = $('.textarea_br')
+  const rawTarget = target.get(0)
+  let lineHeight = Number(target.attr('rows'))
+  if (rawTarget) {
+    while (rawTarget.scrollHeight > rawTarget.offsetHeight) {
+      lineHeight++
+      target.attr('rows', lineHeight)
+    }
   }
-});
+})
 
-$(function () {
-  const $textarea = $('.textarea_br');
-  const lineHeight = parseInt($textarea.css('lineHeight'));
+window.addEventListener('DOMContentLoaded', function () {
+  const $textarea = $('.textarea_br')
+  const lineHeight = parseInt($textarea.css('lineHeight'))
   $textarea.on('input', function () {
-    const lines = ($(this).val() + '\n').match(/\n/g).length;
-    $(this).height(lineHeight * lines);
-  });
-});
+    const lines = ($(this).val() + '\n').match(/\n/g).length
+    $(this).height(lineHeight * lines)
+  })
+})
