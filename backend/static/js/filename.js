29f9216ec9302e93474fa5817a48d4d22b49eddc
ANFINII <anfinii19870506@gmail.com>
2023-05-17 16:07:49 +0900

    refactor filename.js

--- backend/static/js/filename.js
+++ backend/static/js/filename.js
@@ -1,8 +1,12 @@
 function handleInputFileChange(inputSelector, displaySelector) {
   $(inputSelector).change(function () {
-    $(displaySelector).val($(this).val().replace('C:\\fakepath\\', ''))
+    const fileNames = []
+    for (let i = 0; i < this.files.length; i++) {
+      fileNames.push(this.files[i].name)
+    }
+    $(displaySelector).val(fileNames.join(', '))
   })
 }
 
-handleInputFileChange('.file_1', '#file_1')
-handleInputFileChange('.file_2', '#file_2')
+handleInputFileChange('.file_1', '#file_1');
+handleInputFileChange('.file_2', '#file_2');
