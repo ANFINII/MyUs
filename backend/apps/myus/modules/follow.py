0d491c65d926288b9a1cb4d6ba23494f442282bf
ANFINII <anfinii19870506@gmail.com>
2023-06-04 09:38:00 +0900

    refactor follow.py

--- backend/apps/myus/modules/follow.py
+++ backend/apps/myus/modules/follow.py
@@ -4,34 +4,18 @@ from apps.myus.modules.contains import NotificationTypeNo
 
 def follow_update_data(follower, following, follow):
     if follower == following:
-        # '自分はフォローできません'
+        # 自分はフォローできません
         pass
     elif follow:
         notification = Notification.objects.filter(type_no=NotificationTypeNo.follow, object_id=follow.id)
         notification.delete()
-        # 'フォローを外しました'
+        # フォローを外しました
         follow.delete()
         is_follow = False
-        #ログインユーザーのフォロー数
-        following_count = Follow.objects.filter(follower=follower.user).count()
-        follower.following_count = following_count
-        follower.save(update_fields=['following_count'])
-        #フォローユーザーのフォロワー数
-        follower_count = Follow.objects.filter(following=following.user).count()
-        following.follower_count = follower_count
-        following.save(update_fields=['follower_count'])
     else:
         follow = Follow.objects.create(follower=follower.user, following=following.user)
         is_follow = True
-        #ログインユーザーのフォロー数
-        following_count = Follow.objects.filter(follower=follower.user).count()
-        follower.following_count = following_count
-        follower.save(update_fields=['following_count'])
-        #フォローユーザーのフォロワー数
-        follower_count = Follow.objects.filter(following=following.user).count()
-        following.follower_count = follower_count
-        following.save(update_fields=['follower_count'])
-        # 'フォローしました'
+        # フォローしました
         if following.user.notificationsetting.is_follow:
             Notification.objects.create(
                 user_from=follower.user,
@@ -40,4 +24,14 @@ def follow_update_data(follower, following, follow):
                 type_name='follow',
                 content_object=follow,
             )
+
+    # ログインユーザーのフォロー数
+    following_count = Follow.objects.filter(follower=follower.user).count()
+    follower.following_count = following_count
+    follower.save(update_fields=['following_count'])
+
+    # フォローユーザーのフォロワー数
+    follower_count = Follow.objects.filter(following=following.user).count()
+    following.follower_count = follower_count
+    following.save(update_fields=['follower_count'])
     return {'is_follow': is_follow, 'follower_count': follower_count}
