e5164655bb9121103d2dfc00d2319e52ad78630d
ANFINII <anfinii19870506@gmail.com>
2023-07-01 10:04:20 +0900

    fix functions

--- frontend/src/components/layout/Main/index.tsx
+++ frontend/src/components/layout/Main/index.tsx
@@ -1,19 +1,19 @@
-import {Query} from 'utils/type'
+import { SearchQuery } from 'types/media'
 import Meta from 'components/layout/Head/Meta'
 
 interface Props {
   title?: string
   hero?: string
-  query?: Query
+  query?: SearchQuery
   children: React.ReactNode
 }
 
 export default function Main(props: Props) {
-  const {title, hero, query, children} = props
+  const { title, hero, query, children } = props
   return (
     <>
       <Meta title={title} />
-      <main className='main'>
+      <main className="main">
         {hero && <h1>{hero}</h1>}
         {query && <section className="search_message">「{query.name}」の検索結果「{query.count}」件</section>}
         {children}
