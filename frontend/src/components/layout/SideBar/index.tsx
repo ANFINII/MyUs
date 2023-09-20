fb08db4cc43918a483f1b58ed811687fc1fca49d
ANFINII <anfinii19870506@gmail.com>
2023-09-20 14:58:55 +0900

    fix sidebar

--- frontend/src/components/layout/SideBar/index.tsx
+++ frontend/src/components/layout/SideBar/index.tsx
@@ -14,43 +14,43 @@ export default function SideBar() {
     <aside className="sidebar">
       <nav className="sidebar_nav">
         <ul>
-          <li className={'sidebar_color' + (router.pathname === '/video' ? ' ' + 'active' : '')}>
-            <Link href="/video" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/video' ? ' ' + 'active' : '')}>
+            <Link href="/media/video" className="pjax_button">
               <IconVideo />
               <p className="sidebar_text">Video</p>
             </Link>
           </li>
 
-          <li className={'sidebar_color' + (router.pathname === '/music' ? ' ' + 'active' : '')}>
-            <Link href="/music" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/music' ? ' ' + 'active' : '')}>
+            <Link href="/media/music" className="pjax_button">
               <IconMusic />
               <p className="sidebar_text">Music</p>
             </Link>
           </li>
 
-          <li className={'sidebar_color' + (router.pathname === '/comic' ? ' ' + 'active' : '')}>
-            <Link href="/comic" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/comic' ? ' ' + 'active' : '')}>
+            <Link href="/media/comic" className="pjax_button">
               <IconComic />
               <p className="sidebar_text">Comic</p>
             </Link>
           </li>
 
-          <li className={'sidebar_color' + (router.pathname === '/picture' ? ' ' + 'active' : '')}>
-            <Link href="/picture" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/picture' ? ' ' + 'active' : '')}>
+            <Link href="/media/picture" className="pjax_button">
               <IconPicture />
               <p className="sidebar_text">Picture</p>
             </Link>
           </li>
 
-          <li className={'sidebar_color' + (router.pathname === '/blog' ? ' ' + 'active' : '')}>
-            <Link href="/blog" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/blog' ? ' ' + 'active' : '')}>
+            <Link href="/media/blog" className="pjax_button">
               <IconBlog />
               <p className="sidebar_text">Blog</p>
             </Link>
           </li>
 
-          <li className={'sidebar_color' + (router.pathname === '/chat' ? ' ' + 'active' : '')}>
-            <Link href="/chat" className="pjax_button">
+          <li className={'sidebar_color' + (router.pathname === '/media/chat' ? ' ' + 'active' : '')}>
+            <Link href="/media/chat" className="pjax_button">
               <IconChat />
               <p className="sidebar_text">Chat</p>
             </Link>
