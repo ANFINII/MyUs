49483b3b3096dc2252a148adca757ec3c587527c
ANFINII <anfinii19870506@gmail.com>
2023-09-20 14:59:05 +0900

    fix 404 page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,17 +1,11 @@
-import Head from 'next/head'
+import Layout from 'components/layout'
 import Footer from 'components/layout/Footer'
 
 export default function Custom404() {
   return (
-    <article className="article_pass">
-      <Head>
-        <title>MyUs404</title>
-      </Head>
-
-      <h1>404 Not Found</h1>
+    <Layout title="404 Not Found">
       <h2 className="login_required">ページが見つかりませんでした!</h2>
-
       <Footer />
-    </article>
+    </Layout>
   )
 }
