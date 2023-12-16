867d1ee6348a33da8ab999f29c033a7ad7d62e96
ANFINII <anfinii19870506@gmail.com>
2023-12-16 22:33:33 +0900

    fix Django 5.0

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,10 +1,10 @@
 """
-ASGI config for config project.
+ASGI config for MyUs project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
