1e0b527eca3e2ad298a55eb8c55d052a81f5def2
ANFINII <anfinii19870506@gmail.com>
2023-12-16 17:02:35 +0900

    fix delete js

--- frontend/src/service/setupProxy.js
+++
@@ -1,8 +0,0 @@
-import proxy from 'http-proxy-middleware'
-
-module.exports = function(app) {
-    const headers  = {
-        "Content-Type": "application/json",
-    }
-    app.use(proxy("/api/v1/", { target: "https://test.com/",changeOrigin: true,secure: false,headers: headers}))
-}
