92485bf32a66b67ba6e79ae00e3c6610c939bf87
ANFINII <anfinii19870506@gmail.com>
2023-12-23 21:30:39 +0900

    fix home

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,31 +1,23 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { apiServer } from 'lib/apiServer'
 import { apiClient } from 'lib/axios'
 import { apiHome } from 'api/uri'
-import { HomeMedia, Search } from 'types/internal/media'
+import { HomeMedia } from 'types/internal/media'
 import Homes from 'components/templates/media/home/list'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const homeMedia = async () => {
-    await apiClient.get(apiHome).then((res) => {
-      res.data
-    })
-  }
+  const homeMedia = await apiServer(req, apiClient, apiHome).then((res) => {
+    return res.data
+  })
   return { props: { homeMedia, ...translations } }
 }
 
 interface Props {
   homeMedia: HomeMedia
-  search?: Search
 }
 
 export default function HomesPage(props: Props) {
   return <Homes {...props} />
 }
-
-// {
-//   /* <Script src="https://vjs.zencdn.net/7.19.2/video.min.js"></Script>
-// <Script src='/pages/api/video_auto.js'></Script>
-// <script>$(function() { $('audio').audioPlayer() })</cript> */
-// }
