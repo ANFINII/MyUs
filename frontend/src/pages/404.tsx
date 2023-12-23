426f4a01286db096a0115fb8af03eda4580bbdaa
ANFINII <anfinii19870506@gmail.com>
2023-12-24 00:20:20 +0900

    fix custom page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,7 +1,7 @@
 import Layout from 'components/layout'
 import Footer from 'components/layout/Footer'
 
-export default function Custom404() {
+export default function Custom404Page() {
   return (
     <Layout title="404 Not Found">
       <h2 className="login_required">ページが見つかりませんでした!</h2>
--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,17 +1,11 @@
-import Head from 'next/head'
+import Layout from 'components/layout'
 import Footer from 'components/layout/Footer'
 
-export default function Custom500() {
+export default function Custom500Page() {
   return (
-    <article className="article_pass">
-      <Head>
-        <title>MyUs500</title>
-      </Head>
-
-      <h1>500 Server Error</h1>
+    <Layout title="500 Server Error">
       <h2 className="login_required">このページは動作しませんでした!</h2>
-
       <Footer />
-    </article>
+    </Layout>
   )
 }
