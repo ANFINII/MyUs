30faa6011fba1f27e534c9175bfc4d81bfeac920
ANFINII <anfinii19870506@gmail.com>
2023-12-23 23:16:23 +0900

    fix get_user_id

--- backend/apps/api/utils.py
+++ backend/apps/api/utils.py
@@ -1,6 +1,7 @@
 import jwt
 from django.conf import settings
-
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 def get_user_id(request):
     token = request.COOKIES.get('user_token')
@@ -10,7 +11,10 @@ def get_user_id(request):
     key = settings.SECRET_KEY
     try:
         payload = jwt.decode(jwt=token, key=key, algorithms=['HS256'])
-        return payload['user_id']
+        user_id = payload['user_id']
+        if not user_id:
+            return Response({'error': '認証されていません!'}, status=HTTP_400_BAD_REQUEST)
+        return user_id
     except jwt.ExpiredSignatureError:
         return None
     except jwt.exceptions.DecodeError:
