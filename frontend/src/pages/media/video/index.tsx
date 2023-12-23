b0d334c483f1feefbeb1302c29835f65e4177970
ANFINII <anfinii19870506@gmail.com>
2023-12-24 00:33:28 +0900

    fix mypage

--- frontend/src/pages/media/video/index.tsx
+++ frontend/src/pages/media/video/index.tsx
@@ -1,14 +1,13 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiServer } from 'lib/apiServer'
 import { apiClient } from 'lib/axios'
 import { apiVideos } from 'api/uri'
 import { Video } from 'types/internal/media'
 import Videos from 'components/templates/media/video/list'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const datas = await apiServer(req, apiClient, apiVideos).then((res) => {
+  const datas = await apiClient.get(apiVideos).then((res) => {
     return res.data
   })
   return { props: { datas, ...translations } }
--- frontend/src/pages/setting/mypage/update.tsx
+++ frontend/src/pages/setting/mypage/update.tsx
@@ -1,30 +1,23 @@
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { apiServer } from 'lib/apiServer'
+import { apiClient } from 'lib/axios'
+import { apiMypage } from 'api/uri'
 import { Mypage } from 'types/internal/auth'
-import MypageUpdate from 'components/templates/setting/mypage/Update'
+import SettingMypageUpdate from 'components/templates/setting/mypage/Update'
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const cookie = context.req?.headers.cookie
-//   const res = await axios.get('/api/mypage', {
-//     headers: {cookie: cookie!}
-//   })
-//   const data: MypageResponse = res.data
-//   return {
-//     props: {mypage: data}
-//   }
-// }
-
-const mypage: Mypage = {
-  id: 1,
-  banner: '/media/users/images_mypage/user_5/MyUs_banner.png',
-  nickname: 'アン',
-  email: 'abc@gmail.com',
-  content: 'MyUs開発者です。',
-  followerCount: 0,
-  followingCount: 1,
-  plan: 'Free',
-  planDate: '2023-12-31T15:00:00Z',
-  isAdvertise: true,
+export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
+  const translations = await serverSideTranslations(locale as string, ['common'])
+  const mypage = await apiServer(req, apiClient, apiMypage).then((res) => {
+    return res.data
+  })
+  return { props: { mypage, ...translations } }
 }
 
-export default function MyPageUpdatePage() {
-  return <MypageUpdate mypage={mypage} />
+interface Props {
+  mypage: Mypage
+}
+
+export default function SettingMypageUpdatePage(props: Props) {
+  return <SettingMypageUpdate {...props} />
 }
