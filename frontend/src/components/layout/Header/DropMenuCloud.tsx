a42fabd7d3cb58d19fe75373a530e5674f169055
ANFINII <anfinii19870506@gmail.com>
2024-01-08 20:37:08 +0900

    fix IconCloud

--- frontend/src/components/layout/Header/DropMenuCloud.tsx
+++ frontend/src/components/layout/Header/DropMenuCloud.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import IconBlog from 'components/parts/Icon/Blog'
 import IconChat from 'components/parts/Icon/Chat'
+import IconCloud from 'components/parts/Icon/Cloud'
 import IconComic from 'components/parts/Icon/Comic'
 import IconMusic from 'components/parts/Icon/Music'
 import IconPicture from 'components/parts/Icon/Picture'
@@ -25,9 +26,7 @@ export default function DropMenuCloud(props: Props) {
     <>
       <label className="drop_back_cover"></label>
       <label className="drop_open">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-cloud-fill" viewBox="0 0 16 16">
-          <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
-        </svg>
+        <IconCloud size="1.5em" />
       </label>
 
       {isOpen && (
