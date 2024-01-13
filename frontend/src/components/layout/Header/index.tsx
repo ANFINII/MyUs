4703867de8e949d4c2484c8341358527c091edf0
ANFINII <anfinii19870506@gmail.com>
2024-01-14 00:55:36 +0900

    fix Header

--- frontend/src/components/layout/Header/index.tsx
+++ frontend/src/components/layout/Header/index.tsx
@@ -1,67 +1,94 @@
 import { useState } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import DropMenuCloud from 'components/layout/Header/DropMenuCloud'
 import DropMenuNotice from 'components/layout/Header/DropMenuNotice'
 import DropMenuProfile from 'components/layout/Header/DropMenuProfile'
 import SideMenu from 'components/layout/Header/SideMenu'
+import IconBell from 'components/parts/Icon/Bell'
+import IconCloud from 'components/parts/Icon/Cloud'
+import IconExclamation from 'components/parts/Icon/Exclamation'
+import IconLightning from 'components/parts/Icon/Lightning'
+import IconList from 'components/parts/Icon/List'
+import IconPerson from 'components/parts/Icon/Person'
 
-export default function Header() {
+interface Props {
+  isAuth?: boolean
+  nickname?: string
+  notificationCount?: number
+}
+
+export default function Header(props: Props) {
+  const { isAuth = false, nickname, notificationCount = 0 } = props
+
   const [search, setSearch] = useState<string>('')
+  const [isOpenSideMenu, setIsOpenSideMenu] = useState<boolean>(false)
   const [isOpenCloud, setIsOpenCloud] = useState<boolean>(false)
   const [isOpenNotice, setIsOpenNotice] = useState<boolean>(false)
   const [isOpenProfile, setIsOpenProfile] = useState<boolean>(false)
 
-  const handleCloud = () => {
-    setIsOpenCloud(!isOpenCloud)
-    setIsOpenNotice(false)
-    setIsOpenProfile(false)
-  }
+  const isActive = (isbool: boolean) => (isbool ? ' ' + 'active' : '')
+  const handleSideMenu = () => setIsOpenSideMenu(!isOpenSideMenu)
+  const handleCloud = () => setIsOpenCloud(!isOpenCloud)
   const handleNotice = () => setIsOpenNotice(!isOpenNotice)
-  const handleProfile = () => setIsOpenProfile(!isOpenProfile)
+  const handleProfile = () => setIsOpenProfile(!isOpenNotice)
+  const handleSideMenuClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e.target === e.currentTarget && setIsOpenSideMenu(false)
+
+  const handleCoverClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      setIsOpenCloud(false)
+      setIsOpenNotice(false)
+      setIsOpenProfile(false)
+    }
+  }
 
   return (
     <header className="header">
       <nav className="header_nav">
-        <div className="header_nav_1">
-          <SideMenu />
-        </div>
+        <div className={'side_nemu_back' + isActive(isOpenSideMenu)} onClick={handleSideMenuClose}></div>
+        <div className={'drop_back_cover' + isActive(isOpenCloud || isOpenNotice || isOpenProfile)} onClick={handleCoverClose}></div>
+
+        <button className="header_nav_1 header_color" onClick={handleSideMenu}>
+          <IconList width="2em" height="1.7em" />
+          <SideMenu isOpen={isOpenSideMenu} setIsOpen={setIsOpenSideMenu} />
+        </button>
 
         <div className="header_nav_2 header_color_MyUs">
           <Link href="/" className="icon_link"></Link>
           <h1>MyUs</h1>
         </div>
 
-        <div className="header_nav_3">
-          <div className="search_container">
-            <div className="searchbar">
-              <form method="GET" action="">
-                <input type="search" name="search" value={search} placeholder="検索..." className="search_input" />
-                <button type="submit" className="search_icon">
-                  <i className="fas fa-search"></i>
-                </button>
-              </form>
-            </div>
+        <div className="header_nav_3 search_container">
+          <div className="searchbar">
+            <form method="GET" action="">
+              <input type="search" name="search" value={search} placeholder="検索..." className="search_input" />
+              <button type="submit" className="search_icon">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
           </div>
         </div>
 
-        <div className="header_nav_4 header_color">
+        <button className="header_nav_4 header_color">
           <Link href="/recommend" className="icon_link"></Link>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
-            <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z" />
-          </svg>
-        </div>
+          <IconLightning size="1.5em" type="fill" />
+        </button>
 
-        <div className="header_nav_5 header_color" onClick={handleCloud}>
+        <button className={'header_nav_5 header_color' + isActive(isOpenCloud)} onClick={handleCloud}>
+          <IconCloud size="1.5em" />
           <DropMenuCloud isOpen={isOpenCloud} setIsOpen={setIsOpenCloud} />
-        </div>
+        </button>
 
-        <div className="header_nav_6 header_color" onClick={handleNotice}>
+        <button className={'header_nav_6 header_color' + isActive(isOpenNotice)} onClick={handleNotice}>
+          <IconBell size="1.5em" className={isActive(notificationCount === 0)} />
+          <IconExclamation size="1.2em" className={isActive(notificationCount === 0)} />
           <DropMenuNotice isOpen={isOpenNotice} setIsOpen={setIsOpenNotice} />
-        </div>
+        </button>
 
-        <div className="header_nav_7" onClick={handleProfile}>
+        <button className={'header_nav_7 header_color' + isActive(isOpenProfile)} onClick={handleProfile}>
+          {isAuth ? <Image src="" title={nickname} width={32} height={32} alt="" /> : <IconPerson size="1.8em" type="circle" />}
           <DropMenuProfile isActive isStaff={false} isOpen={isOpenProfile} setIsOpen={setIsOpenProfile} />
-        </div>
+        </button>
       </nav>
     </header>
   )
