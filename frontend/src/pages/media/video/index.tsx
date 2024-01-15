2f29ac957cef9fe3ec402a5367cf41d1a68a9586
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:11:25 +0900

    fix VideosPage

--- frontend/src/pages/media/video/index.tsx
+++ frontend/src/pages/media/video/index.tsx
@@ -1,15 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiClient } from 'lib/axios'
-import { apiVideos } from 'api/uri'
+import { getVideos } from 'api/media'
 import { Video } from 'types/internal/media'
 import Videos from 'components/templates/media/video/list'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const datas = await apiClient.get(apiVideos).then((res) => {
-    return res.data
-  })
+  const datas = getVideos()
   return { props: { datas, ...translations } }
 }
 
