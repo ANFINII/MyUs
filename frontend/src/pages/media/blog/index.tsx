070873d70d7554d1b9c13df9d1f6a66e39d79920
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:12:43 +0900

    fix BlogsPage

--- frontend/src/pages/media/blog/index.tsx
+++ frontend/src/pages/media/blog/index.tsx
@@ -1,12 +1,19 @@
-import { Search, Picture } from 'types/internal/media'
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getPictures } from 'api/media'
+import { Picture } from 'types/internal/media'
 import Blogs from 'components/templates/media/blog/list'
 
-const search: Search = {
-  name: 'test',
-  count: 0,
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale as string, ['common'])
+  const datas = getPictures()
+  return { props: { datas, ...translations } }
 }
-const datas: Picture[] = []
 
-export default function BlogsPage() {
-  return <Blogs search={search} datas={datas} />
+interface Props {
+  datas: Picture[]
+}
+
+export default function BlogsPage(props: Props) {
+  return <Blogs {...props} />
 }
