36f7aac9f01e83611a930817b0fc2ba95a1c82e7
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:11:50 +0900

    fix PicturesPage

--- frontend/src/pages/media/picture/index.tsx
+++ frontend/src/pages/media/picture/index.tsx
@@ -1,15 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiClient } from 'lib/axios'
-import { apiPictures } from 'api/uri'
+import { getPictures } from 'api/media'
 import { Picture } from 'types/internal/media'
 import Pictures from 'components/templates/media/picture/list'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const datas = await apiClient.get(apiPictures).then((res) => {
-    return res.data
-  })
+  const datas = getPictures()
   return { props: { datas, ...translations } }
 }
 
