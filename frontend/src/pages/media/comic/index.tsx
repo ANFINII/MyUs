904700438eb5c2d4c228c41c46c0f8f7ca302861
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:12:19 +0900

    fix ComicsPage

--- frontend/src/pages/media/comic/index.tsx
+++ frontend/src/pages/media/comic/index.tsx
@@ -1,12 +1,19 @@
-import { Search, Picture } from 'types/internal/media'
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getComics } from 'api/media'
+import { Comic } from 'types/internal/media'
 import Comics from 'components/templates/media/comic/list'
 
-const search: Search = {
-  name: 'test',
-  count: 0,
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale as string, ['common'])
+  const datas = getComics()
+  return { props: { datas, ...translations } }
 }
-const datas: Picture[] = []
 
-export default function ComicsPage() {
-  return <Comics search={search} datas={datas} />
+interface Props {
+  datas: Comic[]
+}
+
+export default function ComicsPage(props: Props) {
+  return <Comics {...props} />
 }
