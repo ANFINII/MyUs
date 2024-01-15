0dbb61092d25115618f9dde96b789ea4cfea65d2
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:12:34 +0900

    fix ChatsPage

--- frontend/src/pages/media/chat/index.tsx
+++ frontend/src/pages/media/chat/index.tsx
@@ -1,12 +1,19 @@
-import { Search, Chat } from 'types/internal/media'
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getChats } from 'api/media'
+import { Chat } from 'types/internal/media'
 import Chats from 'components/templates/media/chat/list'
 
-const search: Search = {
-  name: 'test',
-  count: 0,
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale as string, ['common'])
+  const datas = getChats()
+  return { props: { datas, ...translations } }
 }
-const datas: Chat[] = []
 
-export default function ChatsPage() {
-  return <Chats search={search} datas={datas} />
+interface Props {
+  datas: Chat[]
+}
+
+export default function ChatsPage(props: Props) {
+  return <Chats {...props} />
 }
