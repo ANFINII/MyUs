7bf6bc50c505aa611e6dff6d3d8bb28ada020d72
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:21:23 +0900

    fix getProfile

--- frontend/src/pages/setting/profile/update.tsx
+++ frontend/src/pages/setting/profile/update.tsx
@@ -1,16 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiServer } from 'lib/apiServer'
-import { apiClient } from 'lib/axios'
-import { apiProfile } from 'api/uri'
+import { getProfile } from 'api/user'
 import { UserProfile } from 'types/internal/auth'
 import SettingProfileUpdate from 'components/templates/setting/profile/Update'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const user = await apiServer(req, apiClient, apiProfile).then((res) => {
-    return res.data
-  })
+  const user = getProfile(req)
   return { props: { user, ...translations } }
 }
 
