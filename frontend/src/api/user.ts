b4b81e1a32e098a1ddfe4a1c9eab37fb4587ede6
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:21:39 +0900

    fix getMypage

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,26 @@
+import { NextApiRequestCookies } from 'next/dist/server/api-utils'
+import { IncomingMessage } from 'http'
+import { HttpStatusCode } from 'axios'
+import { apiServer } from 'lib/apiServer'
+import { apiClient } from 'lib/axios'
+import { apiMypage, apiProfile } from './uri'
+
+interface Req extends IncomingMessage {
+  cookies: NextApiRequestCookies
+}
+
+export const getProfile = async (req: Req) => {
+  const data = await apiServer(req, apiClient, apiProfile).then((res) => {
+    if (res.status !== HttpStatusCode.Ok) throw Error
+    return res.data
+  })
+  return data
+}
+
+export const getMypage = async (req: Req) => {
+  const data = await apiServer(req, apiClient, apiMypage).then((res) => {
+    if (res.status !== HttpStatusCode.Ok) throw Error
+    return res.data
+  })
+  return data
+}
--- frontend/src/pages/setting/mypage/index.tsx
+++ frontend/src/pages/setting/mypage/index.tsx
@@ -1,16 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiServer } from 'lib/apiServer'
-import { apiClient } from 'lib/axios'
-import { apiMypage } from 'api/uri'
+import { getMypage } from 'api/user'
 import { Mypage } from 'types/internal/auth'
 import SettingMypage from 'components/templates/setting/mypage'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const mypage = await apiServer(req, apiClient, apiMypage).then((res) => {
-    return res.data
-  })
+  const mypage = getMypage(req)
   return { props: { mypage, ...translations } }
 }
 
