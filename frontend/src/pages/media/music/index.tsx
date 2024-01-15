db584c32ad50224ae78125b4ce6600466f0cbf94
ANFINII <anfinii19870506@gmail.com>
2024-01-16 02:12:07 +0900

    fix MusicsPage

--- frontend/src/pages/media/music/index.tsx
+++ frontend/src/pages/media/music/index.tsx
@@ -1,15 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { apiClient } from 'lib/axios'
-import { apiMusics } from 'api/uri'
+import { getMusics } from 'api/media'
 import { Music } from 'types/internal/media'
 import Musics from 'components/templates/media/music/list'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const datas = await apiClient.get(apiMusics).then((res) => {
-    return res.data
-  })
+  const datas = getMusics()
   return { props: { datas, ...translations } }
 }
 
