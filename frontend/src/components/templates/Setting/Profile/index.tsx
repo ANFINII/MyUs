e8a69304fab16480acd1717f03c91bc41c500e13
ANFINII <anfinii19870506@gmail.com>
2024-01-19 02:16:19 +0900

    fix profile table

--- frontend/src/components/templates/Setting/Profile/index.tsx
+++ frontend/src/components/templates/Setting/Profile/index.tsx
@@ -12,6 +12,8 @@ import Input from 'components/parts/Input'
 import Select from 'components/parts/Input/Select'
 import Textarea from 'components/parts/Input/Textarea'
 import LoginRequired from 'components/parts/LoginRequired'
+import Table from 'components/widgets/Table'
+import TableRow from 'components/widgets/Table/Row'
 
 interface Props {
   user: UserProfile
@@ -48,164 +50,112 @@ export default function Profile(props: Props) {
         )}
 
         {isEdit ? (
-          <table className="table">
-            <tbody>
-              <tr className="table_header">
-                <td className="td_color">アカウント画像</td>
-                <td valign="baseline">
-                  <label htmlFor="account_image_input" className="update_account_image">
-                    <IconPerson size="3.5em" type="square" />
-                    <input type="file" name="image" accept="image/*" id="account_image_input" className="custom-file-input" />
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">メールアドレス</td>
-                <td>
-                  <Input type="text" name="email" value={user.email} maxLength={120} />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">ユーザー名</td>
-                <td>
-                  <Input type="text" name="username" value={user.username} maxLength={30} placeholder="英数字" />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">投稿者名</td>
-                <td>
-                  <Input type="text" name="nickname" value={user.nickname} maxLength={60} />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">名前</td>
-                <td>
-                  <div className="td_name">
-                    <Input type="text" name="last_name" value={user.lastname} placeholder="姓" maxLength={30} />
-                    <Input type="text" name="first_name" value={user.firstname} placeholder="名" maxLength={30} />
+          <Table>
+            <TableRow label="アカウント画像" className="table_header">
+              <label htmlFor="account_image_input" className="update_account_image">
+                <IconPerson size="3.5em" type="square" />
+                <input type="file" name="image" accept="image/*" id="account_image_input" className="custom-file-input" />
+              </label>
+            </TableRow>
+            <TableRow label="メールアドレス">
+              <Input type="text" name="email" value={user.email} maxLength={120} />
+            </TableRow>
+            <TableRow label="ユーザー名">
+              <Input type="text" name="username" value={user.username} maxLength={30} placeholder="英数字" />
+            </TableRow>
+            <TableRow label="投稿者名">
+              <Input type="text" name="nickname" value={user.nickname} maxLength={60} />
+            </TableRow>
+            <TableRow label="名前">
+              <div className="td_name">
+                <Input type="text" name="last_name" value={user.lastname} placeholder="姓" maxLength={30} />
+                <Input type="text" name="first_name" value={user.firstname} placeholder="名" maxLength={30} />
+              </div>
+            </TableRow>
+            <TableRow label="生年月日">
+              <div className="td_birthday">
+                <Select name="year" value={user.year} options={years} placeholder="年" />
+                <Select name="month" value={user.month} options={months} placeholder="月" />
+                <Select name="day" value={user.day} options={days} placeholder="日" />
+              </div>
+            </TableRow>
+            <TableRow isIndent label="年齢">
+              {user.age}歳
+            </TableRow>
+            <TableRow label="性別">
+              <div className="td_gender">
+                {genders.map((gender) => (
+                  <div key={gender.value} className="gender_radio">
+                    <input type="radio" name="gender" value={gender.value} checked={gender.value === userGender} id={`gender_${gender.value}`} onChange={handleGender} />
+                    <label htmlFor={`gender_${gender.value}`}>{gender.key}</label>
                   </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">生年月日</td>
-                <td>
-                  <div className="td_birthday">
-                    <Select name="year" value={user.year} options={years} placeholder="年" />
-                    <Select name="month" value={user.month} options={months} placeholder="月" />
-                    <Select name="day" value={user.day} options={days} placeholder="日" />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">年齢</td>
-                <td className="td_indent">{user.age}歳</td>
-              </tr>
-              <tr>
-                <td className="td_color">性別</td>
-                <td className="td_gender">
-                  {genders.map((gender) => (
-                    <div key={gender.value} className="gender_radio">
-                      <input type="radio" name="gender" value={gender.value} checked={gender.value === userGender} id={`gender_${gender.value}`} onChange={handleGender} />
-                      <label htmlFor={`gender_${gender.value}`}>{gender.key}</label>
-                    </div>
-                  ))}
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">電話番号</td>
-                <td>
-                  <Input type="tel" name="phone" value={user.phone} maxLength={15} required />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">郵便番号</td>
-                <td>
-                  <Input type="tel" name="postal_code" value={user.postalCode} maxLength={8} required />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">住所</td>
-                <td className="td_location">
-                  <Select name="year" value={user.prefecture} options={prefectures} placeholder="都道府県" />
-                  <Input type="text" name="city" value={user.city} placeholder="市区町村" maxLength={255} />
-                  <Input type="text" name="address" value={user.address} placeholder="丁番地" maxLength={255} />
-                  <Input type="text" name="building" value={user.building} placeholder="建物名" maxLength={255} />
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">自己紹介</td>
-                <td>
-                  <Textarea name="introduction" className="textarea_margin">
-                    {user.introduction}
-                  </Textarea>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                ))}
+              </div>
+            </TableRow>
+            <TableRow label="電話番号">
+              <Input type="tel" name="phone" value={user.phone} maxLength={15} required />
+            </TableRow>
+            <TableRow label="郵便番号">
+              <Input type="tel" name="postal_code" value={user.postalCode} maxLength={8} required />
+            </TableRow>
+            <TableRow label="住所">
+              <div className="td_location">
+                <Select name="year" value={user.prefecture} options={prefectures} placeholder="都道府県" />
+                <Input type="text" name="city" value={user.city} placeholder="市区町村" maxLength={255} />
+                <Input type="text" name="address" value={user.address} placeholder="丁番地" maxLength={255} />
+                <Input type="text" name="building" value={user.building} placeholder="建物名" maxLength={255} />
+              </div>
+            </TableRow>
+            <TableRow label="自己紹介">
+              <Textarea name="introduction" className="textarea_margin">
+                {user.introduction}
+              </Textarea>
+            </TableRow>
+          </Table>
         ) : (
-          <table className="table">
-            <tbody>
-              <tr>
-                <td className="td_color td-header">アカウント画像</td>
-                <td>
-                  <label htmlFor="account_image_input" className="account_image">
-                    <a href={user.avatar} data-lightbox="group">
-                      <Image src={user.avatar} title={user.nickname} width={56} height={56} alt="" data-lightbox="group" />
-                    </a>
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">メールアドレス</td>
-                <td className="td_indent">{user.email}</td>
-              </tr>
-              <tr>
-                <td className="td_color">ユーザー名</td>
-                <td className="td_indent">{user.username}</td>
-              </tr>
-              <tr>
-                <td className="td_color">投稿者名</td>
-                <td className="td_indent">{user.nickname}</td>
-              </tr>
-              <tr>
-                <td className="td_color">名前</td>
-                <td className="td_indent">{user.fullname}</td>
-              </tr>
-              <tr>
-                <td className="td_color">生年月日</td>
-                <td className="td_indent">
-                  {user.year}年{user.month}月{user.day}日
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">年齢</td>
-                <td className="td_indent">{user.age}歳</td>
-              </tr>
-              <tr>
-                <td className="td_color">性別</td>
-                <td className="td_indent">{genderMap[user.gender]}</td>
-              </tr>
-              <tr>
-                <td className="td_color">電話番号</td>
-                <td className="td_indent">{user.phone}</td>
-              </tr>
-              <tr>
-                <td className="td_color">郵便番号</td>
-                <td className="td_indent">{user.postalCode}</td>
-              </tr>
-              <tr>
-                <td className="td_color">住所</td>
-                <td className="td_indent">
-                  {user.prefecture} {user.city}
-                  {user.address} {user.building}
-                </td>
-              </tr>
-              <tr>
-                <td className="td_color">自己紹介</td>
-                <td className="td_indent">{user.introduction}</td>
-              </tr>
-            </tbody>
-          </table>
+          <Table>
+            <TableRow label="アカウント画像" className="table_header">
+              <label htmlFor="account_image_input" className="account_image">
+                <a href={user.avatar} data-lightbox="group">
+                  <Image src={user.avatar} title={user.nickname} width={56} height={56} alt="" data-lightbox="group" />
+                </a>
+              </label>
+            </TableRow>
+            <TableRow isIndent label="メールアドレス">
+              {user.email}
+            </TableRow>
+            <TableRow isIndent label="ユーザー名">
+              {user.username}
+            </TableRow>
+            <TableRow isIndent label="投稿者名">
+              {user.nickname}
+            </TableRow>
+            <TableRow isIndent label="名前">
+              {user.fullname}
+            </TableRow>
+            <TableRow isIndent label="生年月日">
+              {user.year}年{user.month}月{user.day}日
+            </TableRow>
+            <TableRow isIndent label="年齢">
+              {user.age}歳
+            </TableRow>
+            <TableRow isIndent label="性別">
+              {genderMap[user.gender]}
+            </TableRow>
+            <TableRow isIndent label="電話番号">
+              {user.phone}
+            </TableRow>
+            <TableRow isIndent label="郵便番号">
+              {user.postalCode}
+            </TableRow>
+            <TableRow isIndent label="住所">
+              {user.prefecture} {user.city}
+              {user.address} {user.building}
+            </TableRow>
+            <TableRow isIndent label="自己紹介">
+              {user.introduction}
+            </TableRow>
+          </Table>
         )}
       </LoginRequired>
     </Main>
