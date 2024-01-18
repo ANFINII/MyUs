1dd7b553094625bffe2cd5ad607d977a91faff43
ANFINII <anfinii19870506@gmail.com>
2024-01-19 02:34:23 +0900

    fix Knowledge

--- frontend/src/components/templates/menu/knowledge.tsx
+++ frontend/src/components/templates/menu/knowledge.tsx
@@ -1,60 +1,48 @@
 import Main from 'components/layout/Main'
+import Table from 'components/widgets/Table'
+import TableRow from 'components/widgets/Table/Row'
 
 export default function Knowledge() {
   return (
     <Main title="Knowledge Base" type="table">
-      <table className="table">
-        <tbody>
-          <tr>
-            <td className="td_color">MyUs</td>
-            <td className="td_indent">MyUsは総合SNS投稿サイトです。是非いろいろなアイディアを投稿して楽しみましょう!</td>
-          </tr>
-          <tr>
-            <td className="td_color">Video</td>
-            <td className="td_indent">動画コンテンツを投稿できる機能です。</td>
-          </tr>
-          <tr>
-            <td className="td_color">Music</td>
-            <td className="td_indent">音楽、音声、オーディオブックなどを投稿できる機能です。</td>
-          </tr>
-          <tr>
-            <td className="td_color">Comic</td>
-            <td className="td_indent">漫画を投稿できる機能です。</td>
-          </tr>
-          <tr>
-            <td className="td_color">Picture</td>
-            <td className="td_indent">写真、画像、イラストなどが投稿できる機能です。</td>
-          </tr>
-          <tr>
-            <td className="td_color">Blog</td>
-            <td className="td_indent">ブログが投稿できる機能です。具体的にはMyUsに投稿したまとめ、漫画、小説を投稿しても面白いかも知れません。</td>
-          </tr>
-          <tr>
-            <td className="td_color">Chat</td>
-            <td className="td_indent">世界中の方とお話できる機能です。掲示板のように使用することもできます。</td>
-          </tr>
-          <tr>
-            <td className="td_color">ToDo</td>
-            <td className="td_indent">ToDoを管理できる機能です。メモ替りに使用する事もできます。</td>
-          </tr>
-          <tr>
-            <td className="td_color">マイページ</td>
-            <td className="td_indent">自分のページを充実させてフォロワーを増やしてみましょう。</td>
-          </tr>
-          <tr>
-            <td className="td_color">フォロー</td>
-            <td className="td_indent">フォローしている人やフォロワーさんを確認できる機能です。</td>
-          </tr>
-          <tr>
-            <td className="td_color">検索タグ</td>
-            <td className="td_indent">良く検索するワードを20件まで表示できます。投稿管理で表示順序を変更できます。</td>
-          </tr>
-          <tr>
-            <td className="td_color">投稿管理</td>
-            <td className="td_indent">投稿した作品などを編集、非公開、削除ができます。タグ付けもこちらで出来ます。</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table>
+        <TableRow isIndent label="MyUs">
+          MyUsは総合SNS投稿サイトです。是非いろいろなアイディアを投稿して楽しみましょう!
+        </TableRow>
+        <TableRow isIndent label="Video">
+          動画コンテンツを投稿できる機能です。
+        </TableRow>
+        <TableRow isIndent label="Music">
+          音楽、音声、オーディオブックなどを投稿できる機能です。
+        </TableRow>
+        <TableRow isIndent label="Comic">
+          漫画を投稿できる機能です。
+        </TableRow>
+        <TableRow isIndent label="Picture">
+          写真、画像、イラストなどが投稿できる機能です。
+        </TableRow>
+        <TableRow isIndent label="Blog">
+          ブログが投稿できる機能です。具体的にはMyUsに投稿したまとめ、漫画、小説を投稿しても面白いかも知れません。
+        </TableRow>
+        <TableRow isIndent label="Chat">
+          世界中の方とお話できる機能です。掲示板のように使用することもできます。
+        </TableRow>
+        <TableRow isIndent label="ToDo">
+          ToDoを管理できる機能です。メモ替りに使用する事もできます。
+        </TableRow>
+        <TableRow isIndent label="マイページ">
+          自分のページを充実させてフォロワーを増やしてみましょう。
+        </TableRow>
+        <TableRow isIndent label="フォロー">
+          フォローしている人やフォロワーさんを確認できる機能です。
+        </TableRow>
+        <TableRow isIndent label="検索タグ">
+          良く検索するワードを20件まで表示できます。投稿管理で表示順序を変更できます。
+        </TableRow>
+        <TableRow isIndent label="投稿管理">
+          投稿した作品などを編集、非公開、削除ができます。タグ付けもこちらで出来ます。
+        </TableRow>
+      </Table>
     </Main>
   )
 }
