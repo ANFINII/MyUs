04e1d06443910cf6788b27b67bba4a835f97a75c
ANFINII <anfinii19870506@gmail.com>
2024-01-19 14:37:55 +0900

    fix get_user_id

--- backend/apps/api/services/user.py
+++ backend/apps/api/services/user.py
@@ -1,12 +1,12 @@
 import jwt
 from django.conf import settings
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 
-def get_user_id(request):
+def get_user_id(request) -> int | None:
     token = request.COOKIES.get('user_token')
     if not token:
-        return None
+        return Response({'error': '認証Tokenがありません!'}, status=HTTP_400_BAD_REQUEST)
 
     key = settings.SECRET_KEY
     try:
@@ -16,6 +16,6 @@ def get_user_id(request):
             return Response({'error': '認証されていません!'}, status=HTTP_400_BAD_REQUEST)
         return user_id
     except jwt.ExpiredSignatureError:
-        return None
+        return Response({'error': '認証エラーが発生しました!'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
     except jwt.exceptions.DecodeError:
-        return None
+        return Response({'error': '認証エラーが発生しました!'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
