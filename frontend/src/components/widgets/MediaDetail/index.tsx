312874d51801b2b7feeba4848f77f1dad05e635c
ANFINII <anfinii19870506@gmail.com>
2024-01-25 11:33:09 +0900

    feat MediaDetail

---
+++ frontend/src/components/widgets/MediaDetail/index.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  publish: boolean
+  children: React.ReactNode
+}
+
+export default function MediaDetail(props: Props) {
+  const { publish, children } = props
+
+  return <article className="article_detail">{publish ? <>{children}</> : <h2 className="unpublished">非公開に設定されてます!</h2>}</article>
+}
