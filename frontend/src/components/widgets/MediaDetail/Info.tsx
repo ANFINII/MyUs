b3649248b15f9800cfd4b0349a8a57a249181285
ANFINII <anfinii19870506@gmail.com>
2024-01-31 23:11:55 +0900

    fix

---
+++ frontend/src/components/widgets/MediaDetail/Info.tsx
@@ -0,0 +1,25 @@
+import ContentTitle from '../Common/ContentTitle'
+
+interface Props {
+  imageUrl?: string
+  nickname?: string
+}
+
+export default function MediaDetailInfo(props: Props) {
+  const { imageUrl, nickname } = props
+
+  return (
+    <section className="section_detail">
+      <div className="main_decolation">
+        <a href="{% url 'myus:blog_detail' item.pk item.title %}" className="content_side">
+          <figure className="content_size">
+            <img src="" className="radius_10" />
+          </figure>
+          <div className="author_space_side">
+            <ContentTitle title="" nickname="" read={1} totalLike={1} created="" />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
