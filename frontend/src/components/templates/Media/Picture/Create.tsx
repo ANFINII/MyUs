49560ae13cc6a4e85c4f628352fa17cfaddf52b2
ANFINII <anfinii19870506@gmail.com>
2024-02-05 00:48:29 +0900

    fix PictureCreate

--- frontend/src/components/templates/Media/Picture/Create.tsx
+++ frontend/src/components/templates/Media/Picture/Create.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { postPictureCreate } from 'api/media/post'
+import { CreatePcture } from 'types/internal/media'
 import Main from 'components/layout/Main'
 import Button from 'components/parts/Button'
 import Input from 'components/parts/Input'
@@ -12,20 +16,40 @@ interface Props {
 export default function PictureCreate(props: Props) {
   const { isAuth } = props
 
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [create, setCreate] = useState<CreatePcture>({ title: '', content: '' })
+
+  const handleTitle = (title: string) => setCreate({ ...create, title })
+  const handleContent = (content: string) => setCreate({ ...create, content })
+  const handleFile = (image: File) => setCreate({ ...create, image })
+
+  const handleForm = async () => {
+    setIsLoading(true)
+    try {
+      const picture = await postPictureCreate(create)
+      router.push(`media/picture/${picture.id}`)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <Main title="Picture">
       <LoginRequired isAuth={isAuth}>
         <form method="POST" action="" encType="multipart/form-data">
           <p className="mv_16">タイトル</p>
-          <Input name="title" required />
+          <Input name="title" value={create.title} onChange={handleTitle} required />
 
           <p className="mv_16">内容</p>
-          <Textarea name="content" required></Textarea>
+          <Textarea name="content" value={create.content} onChange={handleContent} required></Textarea>
 
           <p className="mv_16">画像</p>
-          <InputFile id="file_1" accept="image/*" required />
+          <InputFile id="file_1" accept="image/*" onChange={handleFile} required />
 
-          <Button green type="submit" name="作成する" className="mt_32" />
+          <Button green type="button" name="作成する" className="mt_32" loading={isLoading} onClick={handleForm} />
         </form>
       </LoginRequired>
     </Main>
