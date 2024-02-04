992ce665d2255883980918c712306d0c37d7ecb8
ANFINII <anfinii19870506@gmail.com>
2024-02-05 00:41:00 +0900

    fix InputFile

--- frontend/src/components/parts/Input/File/index.tsx
+++ frontend/src/components/parts/Input/File/index.tsx
@@ -1,24 +1,29 @@
-import { useState, useRef } from 'react'
+import { useRef, useState } from 'react'
 import style from 'components/parts/Input/Input.module.scss'
 
 interface Props {
   id?: string
   className?: string
   accept?: string
+  onChange?: (file: File) => void
   required?: boolean
 }
 
 export default function InputFile(props: Props) {
-  const { id, className, accept, required } = props
+  const { id, className, accept, onChange, required } = props
 
   const inputEl = useRef<HTMLInputElement>(null)
   const [fileName, setFileName] = useState('')
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFileName(e.target.value.replace('C:\\fakepath\\', ''))
-  }
-
   const handleClick = () => inputEl.current?.click()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      setFileName(file.name)
+      onChange && onChange(file)
+    }
+  }
 
   return (
     <>
