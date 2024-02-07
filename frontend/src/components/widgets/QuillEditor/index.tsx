c08081d8adbe3a5a8f3544c7ed9d3d3e02fd5c76
ANFINII <anfinii19870506@gmail.com>
2024-02-07 14:45:11 +0900

    fix quill editor

--- frontend/src/components/widgets/QuillEditor/index.tsx
+++ frontend/src/components/widgets/QuillEditor/index.tsx
@@ -1,81 +1,47 @@
-import { useState, useRef, useMemo } from 'react'
-import { renderToString } from 'react-dom/server'
+import React, { useCallback } from 'react'
 import ReactQuill from 'react-quill'
-import Image from 'next/image'
-import Editor, { RenderListFunction, mentionUser } from './modules/quill-setting'
-import 'react-quill/dist/quill.snow.scss'
-
-export async function showUsers(search: string, mentionUsers: mentionUser[]): Promise<mentionUser[]> {
-  return mentionUsers.filter((user) => user.value.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-}
+import 'react-quill/dist/quill.snow.css'
+import 'quill-mention'
+import 'quill-mention/dist/quill.mention.css'
+import { MentionUser } from 'types/internal/timeline'
 
 interface Props {
-  forwardedRef: React.LegacyRef<ReactQuill>
-  placeholder: string
-  value: string
-  onChange: (value: string) => void
+  reference?: React.LegacyRef<ReactQuill>
+  users?: MentionUser[]
+  value?: string
+  onChange?: (value: string) => void
 }
 
-export default function QuillEditor(props: Props): JSX.Element {
-  const { forwardedRef, placeholder, value, onChange } = props
+export default function QuillEditor(props: Props) {
+  const { reference, users, value, onChange } = props
 
-  const getUsers = [
-    {
-      id: 1,
-      username: 'an okina',
-      avatar: '/',
+  const modules = {
+    mention: {
+      allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+      mentionDenotationChars: ['@'],
+      source: useCallback(async (search: string, renderItem: (users: MentionUser[] | undefined, search: string) => void, mentionChar: string) => {
+        let mentionUsers: MentionUser[] | undefined
+        if (mentionChar === '@') {
+          mentionUsers = users
+        }
+        if (search.length === 0) {
+          renderItem(mentionUsers, search)
+        } else if (mentionUsers) {
+          const matches = mentionUsers?.filter((item) => item.value.toLowerCase().includes(search.toLowerCase()))
+          renderItem(matches, search)
+        }
+      }, []),
     },
-    {
-      id: 2,
-      username: 'an okina2',
-      avatar: '/',
-    },
-  ]
-
-  const rootDivRef = useRef<HTMLDivElement>(null)
-  const [isFocused, setIsFocused] = useState(false)
-
-  const renderMentionItem = (item: mentionUser): JSX.Element => {
-    return (
-      <div className="quill_avatar">
-        <Image src={item.avatar} alt={item.value} width={32} height={32} />
-        <span>{item.value}</span>
-      </div>
-    )
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }, { background: [] }, { align: [] }],
+      [{ indent: '-1' }, { indent: '+1' }, { list: 'ordered' }, { list: 'bullet' }],
+      ['bold', 'italic', 'underline', 'strike', 'formula', { script: 'super' }, { script: 'sub' }],
+      ['code-block', 'blockquote', 'link', 'image'],
+    ],
   }
 
-  const modules = useMemo(
-    () => ({
-      magicUrl: true,
-      blotFormatter: {},
-      mention: {
-        allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-        mentionDenotationChars: ['@'],
-        source: async function (search: string, renderList: RenderListFunction): Promise<void> {
-          const mentionUsers = getUsers.map((user) => ({
-            id: user.id,
-            value: user.username,
-            avatar: user.avatar,
-          }))
-          const matchUsers = await showUsers(search, mentionUsers as mentionUser[])
-          renderList(matchUsers)
-        },
-        renderItem: function (item: mentionUser): string {
-          return renderToString(renderMentionItem(item))
-        },
-      },
-      toolbar: { container: [] },
-    }),
-    [getUsers],
-  )
+  const formats = ['header', 'color', 'background', 'align', 'indent', 'list', 'bold', 'italic', 'underline', 'strike', 'formula', 'script', 'code-block', 'blockquote', 'link', 'image', 'mention']
 
-  const formats = ['mention', 'bold', 'blockquote', 'link', 'code-block']
-  const handleFocus = (): void => setIsFocused(true)
-  const handleBlur = (): void => setIsFocused(false)
-
-  return (
-    <div className={`${isFocused ? 'focused-border' : ''}`} ref={rootDivRef}>
-      <Editor forwardedRef={forwardedRef} placeholder={placeholder} value={value} modules={modules} formats={formats} onChange={onChange} onFocus={handleFocus} onBlur={handleBlur} />
-    </div>
-  )
+  return <ReactQuill theme="snow" ref={reference} modules={modules} formats={formats} value={value} onChange={onChange} />
 }
