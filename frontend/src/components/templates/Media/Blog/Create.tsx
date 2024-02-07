2b3842fbc4f08b61ff4b8f6d341b4e85d441ac34
ANFINII <anfinii19870506@gmail.com>
2024-02-07 14:45:35 +0900

    fix BlogCreate

--- frontend/src/components/templates/Media/Blog/Create.tsx
+++ frontend/src/components/templates/Media/Blog/Create.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
+import { MentionUser } from 'types/internal/timeline'
 import Main from 'components/layout/Main'
 import Button from 'components/parts/Button'
 import Input from 'components/parts/Input'
@@ -5,12 +8,27 @@ import InputFile from 'components/parts/Input/File'
 import Textarea from 'components/parts/Input/Textarea'
 import LoginRequired from 'components/parts/LoginRequired'
 
+const QuillEditor = dynamic(() => import('components/widgets/QuillEditor'), { ssr: false })
+
 interface Props {
   isAuth: boolean
 }
 
 export default function BlogCreate(props: Props) {
   const { isAuth } = props
+
+  const [content, setContent] = useState('')
+
+  const users: MentionUser[] = [
+    { id: 1, value: 'an' },
+    { id: 2, value: 'souhi' },
+    { id: 3, value: 'keima' },
+  ]
+
+  const handleChange = (value: string) => {
+    const trimmedValue = value.trim()
+    trimmedValue !== '<p><br></p>' && setContent(value)
+  }
 
   return (
     <Main title="Blog">
@@ -25,8 +43,9 @@ export default function BlogCreate(props: Props) {
         <InputFile id="file_1" accept="image/*" required />
 
         <p className="mv_16">本文</p>
-        {/* <p>{{ form.media }}{{ form.richtext }}</p> */}
-        <Textarea name="content" id="content" required></Textarea>
+        <div className="blog">
+          <QuillEditor value={content} users={users} onChange={handleChange} />
+        </div>
 
         <Button green type="submit" name="作成する" className="mt_32" />
       </LoginRequired>
