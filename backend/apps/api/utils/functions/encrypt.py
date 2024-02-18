67999ebb80d0bc463fac6f44a28f5072a1bd4ef7
ANFINII <anfinii19870506@gmail.com>
2024-02-18 22:37:17 +0900

    fix encrypt

---
+++ backend/apps/api/utils/functions/encrypt.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+
+
+def create_key():
+    key = Fernet.generate_key()
+    return key.decode('utf-8')
+
+
+def encrypt(key: str, data: str):
+    fernet = Fernet(bytes(key, 'utf-8'))
+    encrypted = fernet.encrypt(bytes(data, 'utf-8'))
+    return encrypted.decode('utf-8')
+
+
+def decrypt(key: str, data: str):
+    fernet = Fernet(bytes(key, 'utf-8'))
+    decrypted = fernet.decrypt(bytes(data, 'utf-8'))
+    return decrypted.decode('utf-8')
