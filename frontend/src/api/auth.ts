89ca29604653dfff46b0e795f92468aa06d4cfaa
ANFINII <anfinii19870506@gmail.com>
2024-02-18 22:38:03 +0900

    fix postLogin

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,7 +3,7 @@ import { apiServer } from 'lib/apiServer'
 import { apiClient } from 'lib/axios'
 import { Req } from 'types/global/next'
 import { LoginRequest } from 'types/internal/auth'
-import { apiAuth, apiLogin, apiLogout } from './uri'
+import { apiAuth, apiUser, apiLogin, apiLogout } from './uri'
 
 export const getAuth = async (req: Req) => {
   const data = await apiServer(req, apiClient, apiAuth).then((res) => {
@@ -12,11 +12,16 @@ export const getAuth = async (req: Req) => {
   return data
 }
 
+export const getUser = async () => {
+  const data = await apiClient.get(apiUser).then((res) => {
+    return res.status === HttpStatusCode.Ok
+  })
+  return data
+}
+
 export const postLogin = async (request: LoginRequest) => {
   const data = await apiClient.post(apiLogin, request).then((res) => {
-    if (res.status !== HttpStatusCode.NoContent) {
-      return res.data
-    }
+    return res.data
   })
   return data
 }
