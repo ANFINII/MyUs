3351001ba595229fe9b54645d717a96d3b7edd7c
ANFINII <anfinii19870506@gmail.com>
2024-02-20 09:20:50 +0900

    fix user

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,27 +1,50 @@
+import { HttpStatusCode } from 'axios'
 import { apiServer } from 'lib/apiServer'
 import { apiClient } from 'lib/axios'
 import { Req } from 'types/global/next'
 import { UserProfile, Mypage } from 'types/internal/auth'
 import { Notification } from 'types/internal/auth'
+import { camelSnake } from 'utils/functions/convertCase'
 import { apiProfile, apiMypage, apiNotification } from './uri'
 
 export const getProfile = async (req: Req) => {
   const data = await apiServer(req, apiClient, apiProfile).then((res) => {
-    return res.data as UserProfile
+    if (res.status === HttpStatusCode.Ok) {
+      return res.data as UserProfile
+    } else {
+      return {}
+    }
   })
   return data
 }
 
-export const getMypage = async (req: Req) => {
+export const postProfile = async (request: UserProfile) => {
+  const data = await apiClient.post(apiProfile, camelSnake(request)).then((res) => {
+    if (res.status !== HttpStatusCode.NoContent) {
+      return res.data
+    }
+  })
+  return data
+}
+
+export const getServerMypage = async (req: Req) => {
   const data = await apiServer(req, apiClient, apiMypage).then((res) => {
-    return res.data as Mypage
+    if (res.status === HttpStatusCode.Ok) {
+      return res.data as Mypage
+    } else {
+      return {}
+    }
   })
   return data
 }
 
-export const getNotification = async (req: Req) => {
+export const getServerNotification = async (req: Req) => {
   const data = await apiServer(req, apiClient, apiNotification).then((res) => {
-    return res.data as Notification
+    if (res.status === HttpStatusCode.Ok) {
+      return res.data as Notification
+    } else {
+      return {}
+    }
   })
   return data
 }
