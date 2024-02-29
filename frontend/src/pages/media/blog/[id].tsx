82ff8c295e468cbca12e519972cb7b1f990e3ebc
ANFINII <anfinii19870506@gmail.com>
2024-02-29 23:58:31 +0900

    fix

--- frontend/src/pages/media/blog/[id].tsx
+++ frontend/src/pages/media/blog/[id].tsx
@@ -1,12 +1,13 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { getBlog } from 'api/media/get'
+import { getServerBlog } from 'api/media/get'
 import { Blog } from 'types/internal/media'
 import BlogDetail from 'components/templates/media/blog/detail'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale, params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale, req, query }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const data = await getBlog(Number(params?.id))
+  // const data = await getBlog(Number(query.id))
+  const data = await getServerBlog(req, Number(query.id))
   return { props: { data, ...translations } }
 }
 
