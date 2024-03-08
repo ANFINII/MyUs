fcac0dcd57af91d94f7969e5cd1376dee17a7f6b
ANFINII <anfinii19870506@gmail.com>
2024-03-09 07:56:01 +0900

    ButtonSquare

--- frontend/src/components/parts/Button/Square/index.tsx
+++ frontend/src/components/parts/Button/Square/index.tsx
@@ -1,34 +1,22 @@
+import clsx from 'clsx'
 import style from 'components/parts/Button/Square/Square.module.scss'
 
-interface Color {
-  emerald?: boolean
-  red?: boolean
-  sakura?: boolean
-}
+type ButtonColor = 'sakura' | 'emerald' | 'red'
 
-interface Props extends Color {
+interface Props {
+  name: string
+  color?: ButtonColor
   type?: 'submit' | 'reset' | 'button'
   value?: string
-  name?: string
   className?: string
-  onClick?(): void
+  onClick?: () => void
 }
 
 export default function ButtonSquare(props: Props) {
-  const { emerald, sakura, red } = props
-  const { type = 'button', value, name, className, onClick } = props
-
-  const colorCheck = (name: string, isColor?: boolean) => {
-    return isColor ? ' ' + style[name] : ''
-  }
+  const { name, color = 'sakura', type = 'button', className = '' } = props
 
   return (
-    <button
-      type={type}
-      value={value}
-      onClick={onClick}
-      className={style.button + colorCheck('emerald', emerald) + colorCheck('sakura', sakura) + colorCheck('red', red) + (className ? ' ' + className : '')}
-    >
+    <button {...props} type={type} className={clsx(style.button, style[color], className)}>
       {name}
     </button>
   )
