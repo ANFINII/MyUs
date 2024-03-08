5d99c805d6238803ce9f3ad1078722e894d7ec57
ANFINII <anfinii19870506@gmail.com>
2024-03-08 14:22:34 +0900

    fix getAddressForm

--- frontend/src/lib/apiServer.ts
+++ frontend/src/lib/apiServer.ts
@@ -1,7 +1,13 @@
-import { AxiosInstance } from 'axios'
+import { AxiosInstance, AxiosResponse } from 'axios'
 import { Req } from 'types/global/next'
 
-export const apiServer = async (req: Req, client: AxiosInstance, url: string, query?: Record<string, string>, header?: Record<string, string>) => {
+export const apiServer = async (
+  req: Req,
+  client: AxiosInstance,
+  url: string,
+  query?: Record<string, string>,
+  header?: Record<string, string>,
+): Promise<AxiosResponse> => {
   const cookie = req.headers.cookie
   const headers = { cookie, ...(header && { ...header }) }
   const config = { headers, ...(query && { params: query }) }
