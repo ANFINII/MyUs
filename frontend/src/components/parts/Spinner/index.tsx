3a9d5f8bd6f6228deb0a6a0ac6bf7e262823d3c1
ANFINII <anfinii19870506@gmail.com>
2024-03-08 12:24:12 +0900

    fix spinner

--- frontend/src/components/parts/Spinner/index.tsx
+++ frontend/src/components/parts/Spinner/index.tsx
@@ -1,41 +1,16 @@
+import clsx from 'clsx'
 import style from 'components/parts/Spinner/Spinner.module.scss'
 
-interface Color {
-  blue?: boolean
-  purple?: boolean
-  red?: boolean
-  green?: boolean
-}
+export type SpinnerColor = 'white' | 'gray' | 'blue'
 
-interface Props extends Color {
+interface Props {
+  color?: SpinnerColor
   size?: 's' | 'm' | 'l'
   className?: string
 }
 
 export default function Spinner(props: Props): JSX.Element {
-  const { blue, purple, red, green, size = 'm', className } = props
+  const { color = 'white', size = 'm', className } = props
 
-  const colorCheck = (name: string, isColor?: boolean): string => {
-    return isColor ? ' ' + style[name] : ''
-  }
-
-  const sizeCheck = (name: string): string => {
-    return size === name ? ' ' + style[name] : ''
-  }
-
-  return (
-    <span
-      className={
-        style.spinner +
-        colorCheck('blue', blue) +
-        colorCheck('purple', purple) +
-        colorCheck('red', red) +
-        colorCheck('green', green) +
-        sizeCheck('s') +
-        sizeCheck('m') +
-        sizeCheck('l') +
-        (className ? ' ' + className : '')
-      }
-    ></span>
-  )
+  return <span className={clsx(style.spinner, style[color], style[size], className ? className : '')}></span>
 }
