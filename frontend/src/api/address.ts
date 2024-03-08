6ea71b2a52012126139576e518b20233de7ceef5
ANFINII <anfinii19870506@gmail.com>
2024-03-08 14:44:02 +0900

    fix

--- frontend/src/api/address.ts
+++ frontend/src/api/address.ts
@@ -1,13 +1,9 @@
-import { HttpStatusCode } from 'axios'
 import { apiAddressClient } from 'lib/axios'
-import { AddressResult } from 'types/internal/address'
+import { Address, AddressResult } from 'types/internal/address'
 import { apiAddress } from './uri'
 
 // 住所検索APIリファレンス: http://zipcloud.ibsnet.co.jp/doc/api
-export const getAddressForm = async (zipcode: string) => {
-  const data = await apiAddressClient.get(apiAddress, { params: { zipcode } }).then((res) => {
-    if (res.status !== HttpStatusCode.Ok) throw Error
-    return res.data.results[0] as AddressResult
-  })
-  return data
+export const getAddressForm = async (zipcode: string): Promise<AddressResult> => {
+  const data = (await apiAddressClient.get(apiAddress, { params: { zipcode } })) as Address
+  return data.results[0]
 }
