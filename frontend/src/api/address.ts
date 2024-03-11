dc8e69768e690722476672714a7a3429def34cc3
ANFINII <anfinii19870506@gmail.com>
2024-03-11 18:35:55 +0900

    fix getAddressForm

--- frontend/src/api/address.ts
+++ frontend/src/api/address.ts
@@ -4,6 +4,8 @@ import { apiAddress } from './uri'
 
 // 住所検索APIリファレンス: http://zipcloud.ibsnet.co.jp/doc/api
 export const getAddressForm = async (zipcode: string): Promise<AddressResult> => {
-  const data = (await apiAddressClient.get(apiAddress, { params: { zipcode } })) as Address
+  const data = await apiAddressClient.get(apiAddress, { params: { zipcode } }).then((res) => {
+    return res.data as Address
+  })
   return data.results[0]
 }
