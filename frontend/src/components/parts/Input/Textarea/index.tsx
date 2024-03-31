d6e216ea1a4eab1f58ec6cfa5477922e4cfac73f
ANFINII <anfinii19870506@gmail.com>
2024-03-31 16:11:40 +0900

    fix Textarea

--- frontend/src/components/parts/Input/Textarea/index.tsx
+++ frontend/src/components/parts/Input/Textarea/index.tsx
@@ -3,30 +3,47 @@ import clsx from 'clsx'
 import style from 'components/parts/Input/Textarea/Textarea.module.scss'
 
 interface Props {
+  label?: string
+  helpText?: string
   name?: string
   value?: string
   placeholder?: string
   required?: boolean
   disabled?: boolean
+  error?: boolean
   className?: string
-  children?: React.ReactNode
   onChange?: (value: string) => void
+  children?: React.ReactNode
 }
 
 export default function Textarea(props: Props) {
-  const { className = '', children, onChange } = props
+  const { label, helpText, className = '', onChange, children, required, error } = props
 
   const [rows, setRows] = useState(1)
+  const [isValue, setIsValue] = useState<boolean>(false)
+
+  const isRequired = required && !isValue
+  const isHelpText = !isRequired && error
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const height = e.target.value.split('\n').length
     setRows(height)
+    setIsValue(e.target.value !== '')
     onChange && onChange(e.target.value)
   }
 
   return (
-    <textarea {...props} rows={rows} onChange={handleChange} className={clsx(style.textarea, className)}>
-      {children}
-    </textarea>
+    <div className={className}>
+      {label && (
+        <label htmlFor={label} className={style.label}>
+          {label}
+        </label>
+      )}
+      <textarea {...props} id={label} rows={rows} onChange={handleChange} className={clsx(style.textarea, isRequired && style.error)}>
+        {children}
+      </textarea>
+      {isRequired && <p className={style.help_text}>※必須入力です！</p>}
+      {isHelpText && <p className={style.help_text}>{helpText}</p>}
+    </div>
   )
 }
