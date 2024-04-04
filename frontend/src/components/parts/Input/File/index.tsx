f48a5fa6f43e10bb206aec83945c178e5fa07cf7
ANFINII <anfinii19870506@gmail.com>
2024-04-04 23:35:48 +0900

    fix InputFile

--- frontend/src/components/parts/Input/File/index.tsx
+++ frontend/src/components/parts/Input/File/index.tsx
@@ -9,25 +9,27 @@ interface Props {
   className?: string
   error?: boolean
   required?: boolean
-  onChange?: (file: File) => void
+  multiple?: boolean
+  onChange?: (files: File | File[]) => void
 }
 
 export default function InputFile(props: Props) {
-  const { label, errorText, accept, className = '', error = false, required = false, onChange } = props
+  const { label, errorText, accept, className = '', error = false, required = false, multiple, onChange } = props
 
   const inputEl = useRef<HTMLInputElement>(null)
-  const [fileName, setFileName] = useState('')
+  const [fileNames, setFileNames] = useState<string[]>([])
 
-  const isRequired = required && fileName === ''
+  const isRequired = required && fileNames.length === 0
   const isErrorText = !isRequired && error
 
   const handleClick = () => inputEl.current?.click()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
-    if (file) {
-      setFileName(file.name)
-      onChange && onChange(file)
+    const files = e.target.files
+    if (files) {
+      const names = Array.from(files).map((file) => file.name)
+      setFileNames(names)
+      onChange && onChange(multiple ? Array.from(files) : files[0])
     }
   }
 
@@ -38,8 +40,8 @@ export default function InputFile(props: Props) {
           {label}
         </label>
       )}
-      <input id={label} ref={inputEl} type="file" accept={accept} onChange={handleChange} hidden />
-      <input placeholder="ファイル選択..." value={fileName} required={required} onClick={handleClick} className={clsx(style.input, isRequired && style.error)} />
+      <input id={label} ref={inputEl} type="file" accept={accept} onChange={handleChange} multiple={multiple} hidden />
+      <input placeholder="ファイル選択..." value={fileNames.join(', ')} required={required} onClick={handleClick} className={clsx(style.input, isRequired && style.error)} />
       {isRequired && <p className={style.error_text}>※必須入力です！</p>}
       {isErrorText && <p className={style.error_text}>{errorText}</p>}
     </div>
