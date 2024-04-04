385a9cf08e0ac19cd7b5442d5a0384e596cafe6c
ANFINII <anfinii19870506@gmail.com>
2024-04-04 23:36:17 +0900

    fix ComicCreate

--- frontend/src/components/templates/Media/Comic/Create.tsx
+++ frontend/src/components/templates/Media/Comic/Create.tsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { postComicCreate } from 'api/media/post'
+import { ComicIn } from 'types/internal/media'
 import Main from 'components/layout/Main'
 import Button from 'components/parts/Button'
 import Input from 'components/parts/Input'
+import InputFile from 'components/parts/Input/File'
 import Textarea from 'components/parts/Input/Textarea'
 import LoginRequired from 'components/parts/LoginRequired'
-
-const now = new Date()
-const year = now.getFullYear()
 
 interface Props {
   isAuth: boolean
@@ -14,20 +16,51 @@ interface Props {
 export default function ComicCreate(props: Props) {
   const { isAuth } = props
 
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isRequired, setIsRequired] = useState<boolean>(false)
+  const [data, setData] = useState<ComicIn>({ title: '', content: '' })
+
+  const handleTitle = (title: string) => setData({ ...data, title })
+  const handleContent = (content: string) => setData({ ...data, content })
+  const handleFile = (files: File | File[]) => Array.isArray(files) || setData({ ...data, image: files })
+  const handleMultiFile = (files: File | File[]) => Array.isArray(files) && setData({ ...data, images: files })
+
+  const handleForm = async () => {
+    const title = data.title
+    const content = data.content
+    const image = data.image
+    const images = data.images
+    if (!(title && content && image && images)) {
+      setIsRequired(true)
+      return
+    }
+
+    setIsLoading(true)
+    const request = { title, content, image, images }
+    try {
+      const data = await postComicCreate(request)
+      router.push(`/media/comic/${data.id}`)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <Main title="Comic">
       <LoginRequired isAuth={isAuth}>
         <form method="POST" action="" encType="multipart/form-data">
-          <p className="mv_16">タイトル</p>
-          <Input name="title" required />
+          <Input label="タイトル" className="mt_16" required={isRequired} onChange={handleTitle} />
 
-          <p className="mv_16">内容</p>
-          <Textarea name="content" required />
+          <Textarea label="内容" className="mt_16" required={isRequired} onChange={handleContent} />
 
-          <p className="mv_16">期間</p>
-          <Input name="period" placeholder={`${year}-12-31`} required />
+          <InputFile label="サムネイル" accept="image/*" className="mt_16" required={isRequired} onChange={handleFile} />
 
-          <Button color="green" name="作成する" className="mt_32" />
+          <InputFile label="ページ画像" accept="image/*" className="mt_16" required={isRequired} multiple onChange={handleMultiFile} />
+
+          <Button color="green" name="作成する" loading={isLoading} onClick={handleForm} className="mt_32" />
         </form>
       </LoginRequired>
     </Main>
