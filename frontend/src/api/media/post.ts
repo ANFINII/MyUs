616fccb5f1291dae7ae989d7f1af74fcdb9b27b7
ANFINII <anfinii19870506@gmail.com>
2024-04-06 12:18:04 +0900

    fix create api

--- frontend/src/api/media/post.ts
+++ frontend/src/api/media/post.ts
@@ -1,7 +1,24 @@
 import { HttpStatusCode } from 'axios'
 import { apiClient, apiFormClient } from 'lib/axios'
-import { apiPictureCreate, apiBlogCreate, apiChatCreate, apiComicCreate } from 'api/uri'
-import { Picture, Blog, Comic, ComicIn, PctureIn, BlogIn } from 'types/internal/media'
+import { apiPictureCreate, apiBlogCreate, apiChatCreate, apiComicCreate, apiVideoCreate, apiMusicCreate } from 'api/uri'
+import { Video, Music, Comic, Picture, Blog, Chat } from 'types/internal/media'
+import { VideoIn, MusicIn, ComicIn, PictureIn, BlogIn, ChatIn } from 'types/internal/media'
+
+export const postVideoCreate = async (request: VideoIn) => {
+  const data = await apiFormClient.post(apiVideoCreate, request).then((res) => {
+    if (res.status !== HttpStatusCode.Created) throw Error
+    return res.data as Video
+  })
+  return data
+}
+
+export const postMusicCreate = async (request: MusicIn) => {
+  const data = await apiFormClient.post(apiMusicCreate, request).then((res) => {
+    if (res.status !== HttpStatusCode.Created) throw Error
+    return res.data as Music
+  })
+  return data
+}
 
 export const postComicCreate = async (request: ComicIn) => {
   const data = await apiFormClient.post(apiComicCreate, request).then((res) => {
@@ -11,7 +28,7 @@ export const postComicCreate = async (request: ComicIn) => {
   return data
 }
 
-export const postPictureCreate = async (request: PctureIn) => {
+export const postPictureCreate = async (request: PictureIn) => {
   const data = await apiFormClient.post(apiPictureCreate, request).then((res) => {
     if (res.status !== HttpStatusCode.Created) throw Error
     return res.data as Picture
@@ -27,10 +44,10 @@ export const postBlogCreate = async (request: BlogIn) => {
   return data
 }
 
-export const postChatCreate = async (request: any) => {
+export const postChatCreate = async (request: ChatIn) => {
   const data = await apiClient.post(apiChatCreate, request).then((res) => {
     if (res.status !== HttpStatusCode.Created) throw Error
-    return res.data as Blog
+    return res.data as Chat
   })
   return data
 }
