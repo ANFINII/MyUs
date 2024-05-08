376d38d74937ec0fc0851d1547cb7e8e68b7129a
ANFINII <anfinii19870506@gmail.com>
2024-05-09 00:47:50 +0900

    fix

--- frontend/src/utils/constants/common.ts
+++ frontend/src/utils/constants/common.ts
@@ -1,3 +1,3 @@
-export const isEmpty = (obj?: Record<string, any>): boolean => {
+export const isEmpty = (obj: Record<string, any>): boolean => {
   return obj === undefined || obj === null || Object.keys(obj).length === 0
 }
