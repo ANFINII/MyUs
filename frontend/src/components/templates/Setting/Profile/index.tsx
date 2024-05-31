52233e49c76820fb1d9cb14ac72938c509bf6657
ANFINII <anfinii19870506@gmail.com>
2024-06-01 00:32:19 +0900

    fix SettingProfile

--- frontend/src/components/templates/Setting/Profile/index.tsx
+++ frontend/src/components/templates/Setting/Profile/index.tsx
@@ -32,26 +32,26 @@ export default function SettingProfile(props: Props) {
   const [isRequired, setIsRequired] = useState<boolean>(false)
   const { years, months, days } = selectDate()
   const [message, setMessage] = useState<string>('')
-  const [data, setData] = useState<UserProfile>(user)
+  const [values, setValues] = useState<UserProfile>(user)
 
-  const reset = () => setData(user)
+  const reset = () => setValues(user)
   const handleEdit = () => setIsEdit(!isEdit)
-  const handleEmail = (email: string) => setData({ ...data, email })
-  const handleUsername = (username: string) => setData({ ...data, username })
-  const handleNickname = (nickname: string) => setData({ ...data, nickname })
-  const handleLastname = (lastname: string) => setData({ ...data, lastname })
-  const handleFirstname = (firstname: string) => setData({ ...data, firstname })
-  const handleYear = (year: string) => setData({ ...data, year: Number(year) })
-  const handleMonth = (month: string) => setData({ ...data, month: Number(month) })
-  const handleDay = (day: string) => setData({ ...data, day: Number(day) })
-  const handleGender = (e: ChangeEvent<HTMLInputElement>) => setData({ ...data, gender: e.target.value as Gender })
-  const handlePhone = (phone: string) => setData({ ...data, phone })
-  const handlePostalCode = (postalCode: string) => setData({ ...data, postalCode })
-  const handlePrefecture = (prefecture: string) => setData({ ...data, prefecture })
-  const handleCity = (city: string) => setData({ ...data, city })
-  const handleStreet = (street: string) => setData({ ...data, street })
-  const handleBuilding = (building: string) => setData({ ...data, building })
-  const handleIntroduction = (introduction: string) => setData({ ...data, introduction })
+  const handleEmail = (email: string) => setValues({ ...values, email })
+  const handleUsername = (username: string) => setValues({ ...values, username })
+  const handleNickname = (nickname: string) => setValues({ ...values, nickname })
+  const handleLastname = (lastname: string) => setValues({ ...values, lastname })
+  const handleFirstname = (firstname: string) => setValues({ ...values, firstname })
+  const handleYear = (year: string) => setValues({ ...values, year: Number(year) })
+  const handleMonth = (month: string) => setValues({ ...values, month: Number(month) })
+  const handleDay = (day: string) => setValues({ ...values, day: Number(day) })
+  const handleGender = (e: ChangeEvent<HTMLInputElement>) => setValues({ ...values, gender: e.target.value as Gender })
+  const handlePhone = (phone: string) => setValues({ ...values, phone })
+  const handlePostalCode = (postalCode: string) => setValues({ ...values, postalCode })
+  const handlePrefecture = (prefecture: string) => setValues({ ...values, prefecture })
+  const handleCity = (city: string) => setValues({ ...values, city })
+  const handleStreet = (street: string) => setValues({ ...values, street })
+  const handleBuilding = (building: string) => setValues({ ...values, building })
+  const handleIntroduction = (introduction: string) => setValues({ ...values, introduction })
 
   const handleBack = () => {
     reset()
@@ -60,20 +60,21 @@ export default function SettingProfile(props: Props) {
   }
 
   const handleAutoAddress = async () => {
-    const address = await getAddressForm(data.postalCode)
-    setData({ ...data, prefecture: address.address1, city: address.address2, street: address.address3, building: '' })
+    const address = await getAddressForm(values.postalCode)
+    setValues({ ...values, prefecture: address.address1, city: address.address2, street: address.address3, building: '' })
   }
 
   const handlSubmit = async () => {
-    if (!(data.email && data.username && data.nickname && data.lastname && data.firstname && data.phone && data.postalCode)) {
+    const { email, username, nickname, lastname, firstname, phone, postalCode } = values
+    if (!(email && username && nickname && lastname && firstname && phone && postalCode)) {
       setIsRequired(true)
       return
     }
     setIsLoading(true)
     await new Promise((resolve) => setTimeout(resolve, 1000))
     try {
-      const resData = await postProfile(data)
-      resData?.message ? setMessage(resData.message) : handleEdit()
+      const data = await postProfile(values)
+      data?.message ? setMessage(data.message) : handleEdit()
       handleEdit()
       setIsRequired(false)
     } catch (e) {
@@ -85,7 +86,7 @@ export default function SettingProfile(props: Props) {
 
   return (
     <Main title="アカウント設定" type="table">
-      <LoginRequired isAuth={!isEmpty(data)}>
+      <LoginRequired isAuth={!isEmpty(values)}>
         {isEdit ? (
           <div className="button_group">
             <Button color="green" size="s" name="登録" loading={isLoading} onClick={handlSubmit} />
@@ -114,60 +115,60 @@ export default function SettingProfile(props: Props) {
               </label>
             </TableRow>
             <TableRow label="メールアドレス">
-              <Input value={data.email} maxLength={120} required={isRequired} onChange={handleEmail} />
+              <Input value={values.email} maxLength={120} required={isRequired} onChange={handleEmail} />
             </TableRow>
             <TableRow label="ユーザー名">
-              <Input value={data.username} maxLength={30} placeholder="英数字" required={isRequired} onChange={handleUsername} />
+              <Input value={values.username} maxLength={30} placeholder="英数字" required={isRequired} onChange={handleUsername} />
             </TableRow>
             <TableRow label="投稿者名">
-              <Input value={data.nickname} maxLength={60} required={isRequired} onChange={handleNickname} />
+              <Input value={values.nickname} maxLength={60} required={isRequired} onChange={handleNickname} />
             </TableRow>
             <TableRow label="名前">
               <div className="td_name">
-                <Input value={data.lastname} placeholder="姓" maxLength={30} required={isRequired} onChange={handleLastname} />
-                <Input value={data.firstname} placeholder="名" maxLength={30} required={isRequired} onChange={handleFirstname} />
+                <Input value={values.lastname} placeholder="姓" maxLength={30} required={isRequired} onChange={handleLastname} />
+                <Input value={values.firstname} placeholder="名" maxLength={30} required={isRequired} onChange={handleFirstname} />
               </div>
             </TableRow>
             <TableRow label="生年月日">
               <div className="td_birthday">
-                <Select value={data.year} options={years} placeholder="年" onChange={handleYear} />
-                <Select value={data.month} options={months} placeholder="月" onChange={handleMonth} />
-                <Select value={data.day} options={days} placeholder="日" onChange={handleDay} />
+                <Select value={values.year} options={years} placeholder="年" onChange={handleYear} />
+                <Select value={values.month} options={months} placeholder="月" onChange={handleMonth} />
+                <Select value={values.day} options={days} placeholder="日" onChange={handleDay} />
               </div>
             </TableRow>
             <TableRow isIndent label="年齢">
-              {data.age}歳
+              {values.age}歳
             </TableRow>
             <TableRow label="性別">
               <div className="td_gender">
                 {genders.map((gender) => (
                   <div key={gender.value} className="gender_radio">
-                    <input type="radio" value={gender.value} checked={gender.value === data.gender} id={`gender_${gender.value}`} onChange={handleGender} />
+                    <input type="radio" value={gender.value} checked={gender.value === values.gender} id={`gender_${gender.value}`} onChange={handleGender} />
                     <label htmlFor={`gender_${gender.value}`}>{gender.key}</label>
                   </div>
                 ))}
               </div>
             </TableRow>
             <TableRow label="電話番号">
-              <Input type="tel" value={data.phone} maxLength={15} required={isRequired} onChange={handlePhone} />
+              <Input type="tel" value={values.phone} maxLength={15} required={isRequired} onChange={handlePhone} />
             </TableRow>
             <TableRow label="郵便番号">
               <div className="d_flex">
-                <Input type="tel" value={data.postalCode} maxLength={8} className="mr_2" required={isRequired} onChange={handlePostalCode} />
+                <Input type="tel" value={values.postalCode} maxLength={8} className="mr_2" required={isRequired} onChange={handlePostalCode} />
                 <Button name="住所自動入力" onClick={handleAutoAddress} />
               </div>
             </TableRow>
             <TableRow label="住所">
               <div className="td_location">
-                <Select value={data.prefecture} options={prefectures} placeholder="都道府県" onChange={handlePrefecture} />
-                <Input value={data.city} placeholder="市区町村" maxLength={255} onChange={handleCity} />
-                <Input value={data.street} placeholder="町名番地" maxLength={255} onChange={handleStreet} />
-                <Input value={data.building} placeholder="建物名" maxLength={255} onChange={handleBuilding} />
+                <Select value={values.prefecture} options={prefectures} placeholder="都道府県" onChange={handlePrefecture} />
+                <Input value={values.city} placeholder="市区町村" maxLength={255} onChange={handleCity} />
+                <Input value={values.street} placeholder="町名番地" maxLength={255} onChange={handleStreet} />
+                <Input value={values.building} placeholder="建物名" maxLength={255} onChange={handleBuilding} />
               </div>
             </TableRow>
             <TableRow label="自己紹介">
-              <Textarea className="textarea_margin" value={data.introduction} onChange={handleIntroduction}>
-                {data.introduction}
+              <Textarea className="textarea_margin" value={values.introduction} onChange={handleIntroduction}>
+                {values.introduction}
               </Textarea>
             </TableRow>
           </Table>
@@ -175,44 +176,44 @@ export default function SettingProfile(props: Props) {
           <Table>
             <TableRow label="アカウント画像" className="table_header">
               <label htmlFor="account_image_input" className="account_image">
-                <a href={data.avatar} data-lightbox="group">
-                  <ExImage src={data.avatar} title={data.nickname} size="56" data-lightbox="group" />
+                <a href={values.avatar} values-lightbox="group">
+                  <ExImage src={values.avatar} title={values.nickname} size="56" values-lightbox="group" />
                 </a>
               </label>
             </TableRow>
             <TableRow isIndent label="メールアドレス">
-              {data.email}
+              {values.email}
             </TableRow>
             <TableRow isIndent label="ユーザー名">
-              {data.username}
+              {values.username}
             </TableRow>
             <TableRow isIndent label="投稿者名">
-              {data.nickname}
+              {values.nickname}
             </TableRow>
             <TableRow isIndent label="名前">
-              {data.fullname}
+              {values.fullname}
             </TableRow>
             <TableRow isIndent label="生年月日">
-              {data.year}年{data.month}月{data.day}日
+              {values.year}年{values.month}月{values.day}日
             </TableRow>
             <TableRow isIndent label="年齢">
-              {data.age}歳
+              {values.age}歳
             </TableRow>
             <TableRow isIndent label="性別">
-              {genderMap[data.gender]}
+              {genderMap[values.gender]}
             </TableRow>
             <TableRow isIndent label="電話番号">
-              {data.phone}
+              {values.phone}
             </TableRow>
             <TableRow isIndent label="郵便番号">
-              {data.postalCode}
+              {values.postalCode}
             </TableRow>
             <TableRow isIndent label="住所">
-              {data.prefecture} {data.city}
-              {data.street} {data.building}
+              {values.prefecture} {values.city}
+              {values.street} {values.building}
             </TableRow>
             <TableRow isIndent label="自己紹介">
-              {data.introduction}
+              {values.introduction}
             </TableRow>
           </Table>
         )}
