a80ae0a5ff438e3da63cdb5f3157395829fbf9b2
ANFINII <anfinii19870506@gmail.com>
2024-07-08 18:25:16 +0900

    feat useNewDatas

---
+++ frontend/src/components/hooks/useNewList.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+interface OutProps<T> {
+  search?: string
+  newDatas: T
+}
+
+interface Props<T> {
+  datas: T
+  getDatas: (search?: string) => Promise<T>
+}
+
+export const useNewDatas = <T,>(props: Props<T>): OutProps<T> => {
+  const { datas, getDatas } = props
+
+  const router = useRouter()
+  const [newDatas, setNewDatas] = useState<T>(datas)
+
+  const search = router.query.search as string
+  useEffect(() => {
+    const fetchDatas = async (search?: string) => {
+      const newDatas = await getDatas(search)
+      setNewDatas(newDatas)
+    }
+    fetchDatas(search)
+  }, [search])
+
+  return { search, newDatas }
+}
