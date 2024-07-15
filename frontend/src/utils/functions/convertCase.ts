0a4992ae61139acfe2d120f09ded099a102cc32d
ANFINII <anfinii19870506@gmail.com>
2024-07-15 21:29:23 +0900

    fix camelSnake and snakeCamel

--- frontend/src/utils/functions/convertCase.ts
+++ frontend/src/utils/functions/convertCase.ts
@@ -2,14 +2,15 @@ type AnyObject = { [key: string]: any }
 
 const snakeCase = (s: string) => s.replace(/([A-Z])/g, '_$1').toLowerCase()
 
-const transformValue = (value: any) => {
-  if (typeof value === 'object' && value !== null) {
-    return camelSnake(value)
-  }
-  return value
-}
-
 export const camelSnake = <T extends AnyObject>(obj: AnyObject): T => {
+  const transformValue = (value: any) => {
+    if (value instanceof File) {
+      return value
+    } else if (typeof value === 'object' && value !== null) {
+      return camelSnake(value)
+    }
+    return value
+  }
   const result: AnyObject = Array.isArray(obj) ? [] : {}
   Object.entries(obj).forEach(([key, value]) => {
     result[snakeCase(key)] = transformValue(value)
@@ -17,18 +18,18 @@ export const camelSnake = <T extends AnyObject>(obj: AnyObject): T => {
   return result as T
 }
 
-const camelase = (s: string) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''))
+const camelCase = (s: string) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''))
 
 export const snakeCamel = <T extends AnyObject>(obj: AnyObject): T => {
-  const result: AnyObject = Array.isArray(obj) ? [] : {}
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      let value = obj[key]
-      if (typeof value === 'object' && value !== null) {
-        value = snakeCamel(value)
-      }
-      result[camelase(key)] = value
+  const transformValue = (value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      return snakeCamel(value)
     }
+    return value
   }
+  const result: AnyObject = Array.isArray(obj) ? [] : {}
+  Object.entries(obj).forEach(([key, value]) => {
+    result[camelCase(key)] = transformValue(value)
+  })
   return result as T
 }
