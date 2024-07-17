903c67d077a0b6f7d46b4d4beac7fd9f0022e17a
ANFINII <anfinii19870506@gmail.com>
2024-07-17 14:23:03 +0900

    fix LightBox

--- frontend/src/components/widgets/LightBox/index.tsx
+++ frontend/src/components/widgets/LightBox/index.tsx
@@ -17,15 +17,18 @@ export default function LightBox(props: Props) {
   const [open, setOpen] = useState<boolean>(false)
 
   const handleToggle = () => setOpen(!open)
+  const handleClose = () => setOpen(false)
 
   return (
     <>
       <ExImage {...props} onClick={handleToggle} />
-      <div className={open ? style.light_box : 'd_none'}>
+      <div className={open ? style.light_box : 'd_none'} onClick={handleClose}>
         <div className={style.image_box}>
-          <ExImage src={src} title={title} />
-          <div className={style.cross}>
-            <IconCross size="2em" type="large" onClick={handleToggle} />
+          <div>
+            <ExImage src={src} title={title} />
+            <div className={style.cross}>
+              <IconCross size="2em" type="large" onClick={handleToggle} />
+            </div>
           </div>
         </div>
       </div>
