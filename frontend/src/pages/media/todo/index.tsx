1fa348f29e6c8ac86bbf631d122e20f5baaf0981
ANFINII <anfinii19870506@gmail.com>
2024-08-19 21:53:01 +0900

    fix TodosPage

--- frontend/src/pages/media/todo/index.tsx
+++ frontend/src/pages/media/todo/index.tsx
@@ -1,12 +1,12 @@
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { getTodos } from 'api/internal/media/list'
+import { getServerTodos } from 'api/internal/media/list'
 import { Todo } from 'types/internal/media'
 import Todos from 'components/templates/media/todo/list'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const datas = await getTodos()
+  const datas = await getServerTodos(req)
   return { props: { datas, ...translations } }
 }
 
