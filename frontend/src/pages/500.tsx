be626cf549647773a142dfcfd79ceb9b3eddfa4a
ANFINII <anfinii19870506@gmail.com>
2024-08-21 18:07:56 +0900

    fix Custom404Page and Custom500Page

--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,10 +1,22 @@
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Footer from 'components/layout/Footer'
 import Main from 'components/layout/Main'
+import Button from 'components/parts/Button'
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale as string, ['common'])
+  return { props: translations }
+}
 
 export default function Custom500Page() {
+  const router = useRouter()
+
   return (
     <Main title="500 Server Error">
-      <h2 className="login_required">このページは動作しませんでした!</h2>
+      <h2 className="login_required mt_20">このページは動作しませんでした</h2>
+      <Button name="戻る" className="mt_24 w_80" onClick={() => router.back()} />
       <Footer />
     </Main>
   )
