7dfa712d098ddf9ba3dce7ecd9976d01d64250e2
ANFINII <anfinii19870506@gmail.com>
2024-08-21 18:08:17 +0900

    fix convertCase

--- frontend/src/utils/functions/convertCase.ts
+++ frontend/src/utils/functions/convertCase.ts
@@ -3,17 +3,9 @@ type AnyObject = { [key: string]: any }
 const snakeCase = (s: string) => s.replace(/([A-Z])/g, '_$1').toLowerCase()
 
 export const camelSnake = <T extends AnyObject>(obj: AnyObject): T => {
-  const transformValue = (value: any) => {
-    if (value instanceof File) {
-      return value
-    } else if (typeof value === 'object' && value !== null) {
-      return camelSnake(value)
-    }
-    return value
-  }
   const result: AnyObject = Array.isArray(obj) ? [] : {}
   Object.entries(obj).forEach(([key, value]) => {
-    result[snakeCase(key)] = transformValue(value)
+    result[snakeCase(key)] = value
   })
   return result as T
 }
@@ -21,15 +13,9 @@ export const camelSnake = <T extends AnyObject>(obj: AnyObject): T => {
 const camelCase = (s: string) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''))
 
 export const snakeCamel = <T extends AnyObject>(obj: AnyObject): T => {
-  const transformValue = (value: any) => {
-    if (typeof value === 'object' && value !== null) {
-      return snakeCamel(value)
-    }
-    return value
-  }
   const result: AnyObject = Array.isArray(obj) ? [] : {}
   Object.entries(obj).forEach(([key, value]) => {
-    result[camelCase(key)] = transformValue(value)
+    result[camelCase(key)] = value
   })
   return result as T
 }
