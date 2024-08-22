34734f183519250d46d0a74c3b5faf8fea04c84d
ANFINII <anfinii19870506@gmail.com>
2024-08-22 13:48:07 +0900

    fix convertCase

--- frontend/src/utils/functions/convertCase.ts
+++ frontend/src/utils/functions/convertCase.ts
@@ -1,21 +1,36 @@
 type AnyObject = { [key: string]: any }
 
 const snakeCase = (s: string) => s.replace(/([A-Z])/g, '_$1').toLowerCase()
+const camelCase = (s: string) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''))
 
 export const camelSnake = <T extends AnyObject>(obj: AnyObject): T => {
   const result: AnyObject = Array.isArray(obj) ? [] : {}
-  Object.entries(obj).forEach(([key, value]) => {
-    result[snakeCase(key)] = value
-  })
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key]
+      if (typeof window !== 'undefined' && value instanceof File) {
+        result[snakeCase(key)] = value
+      } else if (typeof value === 'object' && value !== null) {
+        result[snakeCase(key)] = camelSnake(value)
+      } else {
+        result[snakeCase(key)] = value
+      }
+    }
+  }
   return result as T
 }
 
-const camelCase = (s: string) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''))
-
 export const snakeCamel = <T extends AnyObject>(obj: AnyObject): T => {
   const result: AnyObject = Array.isArray(obj) ? [] : {}
-  Object.entries(obj).forEach(([key, value]) => {
-    result[camelCase(key)] = value
-  })
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key]
+      if (typeof value === 'object' && value !== null) {
+        result[camelCase(key)] = snakeCamel(value)
+      } else {
+        result[camelCase(key)] = value
+      }
+    }
+  }
   return result as T
 }
