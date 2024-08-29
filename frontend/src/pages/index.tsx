d71f324a4cdfd528ba2dc4ef2c08e1d92b17670f
ANFINII <anfinii19870506@gmail.com>
2024-08-29 10:54:39 +0900

    fix

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,19 @@
-import { GetServerSideProps } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { getHome } from 'api/internal/media/list'
-import { MediaHome } from 'types/internal/media'
-import Homes from 'components/templates/media/home/list'
+// import { GetServerSideProps } from 'next'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+// import { getHome } from 'api/internal/media/list'
+// import { MediaHome } from 'types/internal/media'
+// import Homes from 'components/templates/media/home/list'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const translations = await serverSideTranslations(locale as string, ['common'])
-  const mediaHome = await getHome()
-  return { props: { mediaHome, ...translations } }
-}
+// export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+//   const translations = await serverSideTranslations(locale as string, ['common'])
+//   const mediaHome = await getHome()
+//   return { props: { mediaHome, ...translations } }
+// }
 
-interface Props {
-  mediaHome: MediaHome
-}
+// interface Props {
+//   mediaHome: MediaHome
+// }
 
-export default function HomesPage(props: Props) {
-  return <Homes {...props} />
-}
+// export default function HomesPage(props: Props) {
+//   return <Homes {...props} />
+// }
