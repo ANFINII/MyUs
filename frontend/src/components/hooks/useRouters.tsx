e9085cacff349cbc7c69dc550a99a95060a0e9c7
ANFINII <anfinii19870506@gmail.com>
2024-08-30 08:56:28 +0900

    fix

--- frontend/src/components/hooks/useRouters.tsx
+++ frontend/src/components/hooks/useRouters.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react'
 import { NextRouter, useRouter } from 'next/router'
 
 interface OutProps {
   router: NextRouter
   handleRouter: (url: string) => Promise<boolean>
-  location?: Location
 }
 
 export default function useRouters(): OutProps {
   const router = useRouter()
-  const [location, setLocation] = useState<Location>()
-  useEffect(() => setLocation(window.location), [])
 
-  const handleRouter = (url: string) => router.push(url)
-  return { router, handleRouter, location }
+  const handleRouter = async (url: string) => {
+    await router.prefetch(url)
+    return router.push(url)
+  }
+
+  return { router, handleRouter }
 }
