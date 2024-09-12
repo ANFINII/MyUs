632719a51914fca3f1ecf9c1d59d944c74949d10
ANFINII <anfinii19870506@gmail.com>
2024-09-13 01:47:38 +0900

    fix ApiResponse status code

---
+++ backend/apps/api/utils/functions/response.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
+
+from apps.api.utils.functions.index import message
+
+
+class ApiResponse(Enum):
+    UNAUTHORIZED = (True, 'Un Authorized', HTTP_401_UNAUTHORIZED)
+    NOT_FOUND = (True, 'Not Found', HTTP_404_NOT_FOUND)
+
+    def run(self):
+        is_error, content, status_code = self.value
+        return Response(message(is_error, content), status=status_code)
