88c241eec5a2056a8fe86c991f74a323054d8937
An Okina <79322275+ANFINII@users.noreply.github.com>
2024-09-16 22:28:10 +0900

    notification関連のapiを実装 (#4)
    * fix notification api
    
    * fix template
    
    * fix Notification type
    
    * fix  getNotification fetch
    
    * fix DropMenuNotice components

--- frontend/src/api/internal/user.ts
+++ frontend/src/api/internal/user.ts
@@ -2,8 +2,8 @@ import { HttpStatusCode } from 'axios'
 import { apiServer } from 'lib/apiServer'
 import { apiClient } from 'lib/axios'
 import { Req } from 'types/global/next'
-import { Follow, SearchTagOut } from 'types/internal/auth'
-import { apiFollow, apiFollower, apiSearchTag } from '../uri'
+import { Follow, NotificationOut, SearchTagOut } from 'types/internal/auth'
+import { apiFollow, apiFollower, apiNotification, apiSearchTag } from '../uri'
 
 export const getSearchTag = async (): Promise<SearchTagOut[]> => {
   const res = await apiClient.get(apiSearchTag)
@@ -31,6 +31,12 @@ export const getServerFollower = async (req: Req): Promise<Follow[]> => {
 
 export const getFollower = async (search?: string): Promise<Follow[]> => {
   const res = await apiClient.get(apiFollower, { params: { search } })
+  if (res.status >= HttpStatusCode.InternalServerError) throw Error
+  return res.data
+}
+
+export const getNotification = async (): Promise<NotificationOut> => {
+  const res = await apiClient.get(apiNotification)
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
