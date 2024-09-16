30df8886c4ffe4877619620bba50cda5cb365b30
ANFINII <anfinii19870506@gmail.com>
2024-09-16 22:26:34 +0900

    fix DropMenuNotice components

--- frontend/src/components/layout/Header/DropMenu/Notice.tsx
+++ frontend/src/components/layout/Header/DropMenu/Notice.tsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import clsx from 'clsx'
+import { getNotification } from 'api/internal/user'
+import { NotificationOut } from 'types/internal/auth'
 import { isActive } from 'utils/functions/common'
+import { useUser } from 'components/hooks/useUser'
+import ExImage from 'components/parts/ExImage'
 import IconBell from 'components/parts/Icon/Bell'
 import DropMenuItem from 'components/parts/NavItem/DropMenuItem'
-// import ExImage from 'components/parts/Image'
 
 interface Props {
   open: boolean
@@ -14,96 +18,93 @@ export default function DropMenuNotice(props: Props) {
   const { open, onClose } = props
 
   const router = useRouter()
+  const { user } = useUser()
+  const [notifications, setNotifications] = useState<NotificationOut>()
+
+  useEffect(() => {
+    if (!user.isActive) return
+    const fetch = async () => {
+      const notifications = await getNotification()
+      setNotifications(notifications)
+    }
+    fetch()
+  }, [user.isActive])
 
   const handleClick = (url: string) => {
     router.push(url)
     onClose()
   }
 
+  const renderElement = (isBool: boolean, element: JSX.Element) => isBool && element
+
+  const readNotification = (read: number, title: string) => {
+    const thresholds = [
+      { limit: 10000, message: '1万回閲覧されました' },
+      { limit: 100000, message: '10万回閲覧されました' },
+      { limit: 1000000, message: '100万回閲覧されました' },
+      { limit: 10000000, message: '1000万回閲覧されました' },
+      { limit: 100000000, message: '1億回閲覧されました' },
+      { limit: 1000000000, message: '10億回閲覧されました' },
+    ]
+    const threshold = thresholds.find((t) => read >= t.limit)
+    return threshold ? (
+      <div className="notification_aria_list_1" title={`${title}が${threshold.message}`}>
+        {title}が{threshold.message}
+      </div>
+    ) : (
+      <></>
+    )
+  }
+
   return (
     <nav className={clsx('drop_menu drop_menu_notice', isActive(open))}>
       <ul>
         <DropMenuItem label="通知設定" icon={<IconBell size="1.5em" />} onClick={() => handleClick('/setting/notification')} />
-        {/* {% for notification in notification_list %} */}
-        <li id="notification_aria_link_{{ notification.id }}" className="drop_menu_list">
-          <object notification-id="{{ notification.id }}" data-csrf="{{ csrf_token }}" className="notification_aria_list">
-            {/* <a href="{% url 'myus:userpage' notification.user_from.nickname %}" data-notification="{{ notification.user_from.nickname }}">
-                <ExImage src="{{ notification.user_from.image }}" title="{{ notification.user_from.nickname }}" className="profile_image"/>
-              </a> */}
-            {/* {% if notification.user_confirmed %} */}
-            <span></span>
-            {/* {% else %} */}
-            <svg xmlns="http://www.w3.org/2000/svg" className="svg-circle" viewBox="0 0 2 2">
-              <circle cx="1" cy="1" r="1"></circle>
-            </svg>
-            {/* {% endif %} */}
-
-            {/* {% if notification.type_name == 'video' %} */}
-            {/* <a href="{% url 'myus:video_detail' notification.content_object.id notification.content_object.title %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_name == 'music' %} */}
-            {/* <a href="{% url 'myus:music_detail' notification.content_object.id notification.content_object.title %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_name == 'comic' %} */}
-            {/* <a href="{% url 'myus:comic_detail' notification.content_object.id notification.content_object.title %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_name == 'picture' %} */}
-            {/* <a href="{% url 'myus:picture_detail' notification.content_object.id notification.content_object.title %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_name == 'blog' %} */}
-            {/* <a href="{% url 'myus:blog_detail' notification.content_object.id notification.content_object.title %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_name == 'chat' %} */}
-            {/* <a href="{% url 'myus:chat_detail' notification.content_object.id %}" className="notification_aria_anker"> */}
-            {/* {% elif notification.type_no >= 8 and notification.type_no <= 10 %} */}
-            {/* <a href="{% url 'myus:userpage' notification.user_from.nickname %}" data="{{ notification.user_from.nickname }}" className="notification_aria_anker pjax_button_userpage"> */}
-            {/* {% endif %} */}
-            {/* {% if notification.type_no >= 1 and notification.type_no <= 7 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.user_from.nickname }}さんが{{ notification.content_object.title }}を投稿しました">
-              {/* {{ notification.content_object.title }} */}
+        {notifications?.datas?.map((notification) => (
+          <li key={notification.id} className="drop_menu_list">
+            <div className="notification_aria_list">
+              <a onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+                <ExImage src={notification.userFrom.avatar} title={notification.userFrom.nickname} className="profile_image" />
+              </a>
+              {!notification.isConfirmed && (
+                <svg xmlns="http://www.w3.org/2000/svg" className="svg-circle" viewBox="0 0 2 2">
+                  <circle cx="1" cy="1" r="1"></circle>
+                </svg>
+              )}
+              {renderElement(notification.typeName === 'video', <a className="notification_aria_anker" onClick={() => handleClick(`/video/detail/${notification.contentObject.id}`)} />)}
+              {renderElement(notification.typeName === 'music', <a className="notification_aria_anker" onClick={() => handleClick(`/music/detail/${notification.contentObject.id}`)} />)}
+              {renderElement(notification.typeName === 'comic', <a className="notification_aria_anker" onClick={() => handleClick(`/comic/detail/${notification.contentObject.id}`)} />)}
+              {renderElement(notification.typeName === 'picture', <a className="notification_aria_anker" onClick={() => handleClick(`/picture/detail/${notification.contentObject.id}`)} />)}
+              {renderElement(notification.typeName === 'blog', <a className="notification_aria_anker" onClick={() => handleClick(`/blog/detail/${notification.contentObject.id}`)} />)}
+              {renderElement(notification.typeName === 'chat', <a className="notification_aria_anker" onClick={() => handleClick(`/chat/detail/${notification.contentObject.id}`)} />)}
+              {notification.typeNo >= 8 && notification.typeNo <= 10 && <a className="notification_aria_anker" onClick={() => handleClick(`/chat/detail/${notification.userFrom.nickname}`)} />}
+              {notification.typeNo >= 1 && notification.typeNo <= 7 && (
+                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                  {notification.contentObject.title}
+                </div>
+              )}
+              {notification.typeNo === 7 && (
+                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}にフォローされました`}>
+                  {notification.userFrom.nickname}にフォローされました
+                </div>
+              )}
+              {notification.typeNo === 8 && (
+                <div className="notification_aria_list_1" title={`${notification.contentObject.text}が${notification.userFrom.nickname}さんにいいねされました`}>
+                  {notification.contentObject.text}が{notification.userFrom.nickname}さんにいいねされました
+                </div>
+              )}
+              {notification.typeNo === 9 && (
+                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんから返信がありました ${notification.contentObject.text}`}>
+                  {notification.contentObject.text}
+                </div>
+              )}
+              {(notification.typeNo === 10, readNotification(notification.contentObject.read, notification.contentObject.title))}
+              <form method="POST">
+                <i title="閉じる" className="bi-x notification_aria_list_2" style={{ fontSize: '1.41em' }}></i>
+              </form>
             </div>
-            {/* {% elif notification.type_no == 8 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.user_from.nickname }}さんにフォローされました">
-              {/* {{ notification.user_from.nickname }}さんにフォローされました */}
-            </div>
-            {/* {% elif notification.type_no == 9 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.text }}が{{ notification.user_from.nickname }}さんにいいねされました">
-              {/* {{ notification.content_object.text }}が{{ notification.user_from.nickname }}さんにいいねされました */}
-            </div>
-            {/* {% elif notification.type_no == 10 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.user_from.nickname }}さんから返信がありました {{ notification.content_object.text }}">
-              {/* {{ notification.content_object.text }} */}
-            </div>
-            {/* {% elif notification.type_no == 11 %} */}
-            {/* {% if  notification.content_object.read >= 1000000000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が10億回閲覧されました">
-              {/* {{ notification.content_object.title }}が10億回閲覧されました */}
-            </div>
-            {/* {% elif  notification.content_object.read >= 100000000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が1億回閲覧されました">
-              {/* {{ notification.content_object.title }}が1億回閲覧されました */}
-            </div>
-            {/* {% elif  notification.content_object.read >= 10000000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が1000万回閲覧されました">
-              {/* {{ notification.content_object.title }}が1000万回閲覧されました */}
-            </div>
-            {/* {% elif  notification.content_object.read >= 1000000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が100万回閲覧されました">
-              {/* {{ notification.content_object.title }}が100万回閲覧されました */}
-            </div>
-            {/* {% elif  notification.content_object.read >= 100000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が10万回閲覧されました">
-              {/* {{ notification.content_object.title }}が10万回閲覧されました */}
-            </div>
-            {/* {% elif  notification.content_object.read >= 10000 %} */}
-            <div className="notification_aria_list_1" title="{{ notification.content_object.title }}が1万回閲覧されました">
-              {/* {{ notification.content_object.title }}が1万回閲覧されました */}
-            </div>
-            {/* {% endif %} */}
-            {/* {% endif %} */}
-            {/* </a> */}
-
-            <form method="POST" action="">
-              {/* <i title="閉じる" className="bi-x notification_aria_list_2" style={{font-size: '1.41em';}}></i> */}
-            </form>
-          </object>
-        </li>
-        {/* {% endfor %} */}
+          </li>
+        ))}
       </ul>
     </nav>
   )
