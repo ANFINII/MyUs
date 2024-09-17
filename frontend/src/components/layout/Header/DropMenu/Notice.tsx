29a28056b7edfa39f29ce69cd5765acaed3fb6d5
ANFINII <anfinii19870506@gmail.com>
2024-09-18 08:36:25 +0900

    fix DropMenuNotice refactor ver1

--- frontend/src/components/layout/Header/DropMenu/Notice.tsx
+++ frontend/src/components/layout/Header/DropMenu/Notice.tsx
@@ -3,10 +3,12 @@ import { useRouter } from 'next/router'
 import clsx from 'clsx'
 import { getNotification } from 'api/internal/user'
 import { NotificationOut } from 'types/internal/auth'
+import { NotificationType } from 'utils/constants/enum'
 import { isActive } from 'utils/functions/common'
 import { useUser } from 'components/hooks/useUser'
 import ExImage from 'components/parts/ExImage'
 import IconBell from 'components/parts/Icon/Bell'
+import IconCircle from 'components/parts/Icon/Circle'
 import DropMenuItem from 'components/parts/NavItem/DropMenuItem'
 
 interface Props {
@@ -61,44 +63,98 @@ export default function DropMenuNotice(props: Props) {
       <ul>
         <DropMenuItem label="通知設定" icon={<IconBell size="1.5em" />} onClick={() => handleClick('/setting/notification')} />
         {notifications?.datas?.map((notification) => (
-          <li key={notification.id} className="drop_menu_list">
+          <li key={notification.id} className="drop_menu_notification">
             <div className="notification_aria_list">
-              <a onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+              <div onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
                 <ExImage src={notification.userFrom.avatar} title={notification.userFrom.nickname} className="profile_image" />
-              </a>
-              {!notification.isConfirmed && (
-                <svg xmlns="http://www.w3.org/2000/svg" className="svg-circle" viewBox="0 0 2 2">
-                  <circle cx="1" cy="1" r="1"></circle>
-                </svg>
+              </div>
+
+              {!notification.isConfirmed && <IconCircle size="6" className="svg-circle" />}
+
+              {renderElement(
+                notification.typeName === NotificationType.Video,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/video/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
               )}
-              {renderElement(notification.typeName === 'video', <a className="notification_aria_anker" onClick={() => handleClick(`/video/detail/${notification.contentObject.id}`)} />)}
-              {renderElement(notification.typeName === 'music', <a className="notification_aria_anker" onClick={() => handleClick(`/music/detail/${notification.contentObject.id}`)} />)}
-              {renderElement(notification.typeName === 'comic', <a className="notification_aria_anker" onClick={() => handleClick(`/comic/detail/${notification.contentObject.id}`)} />)}
-              {renderElement(notification.typeName === 'picture', <a className="notification_aria_anker" onClick={() => handleClick(`/picture/detail/${notification.contentObject.id}`)} />)}
-              {renderElement(notification.typeName === 'blog', <a className="notification_aria_anker" onClick={() => handleClick(`/blog/detail/${notification.contentObject.id}`)} />)}
-              {renderElement(notification.typeName === 'chat', <a className="notification_aria_anker" onClick={() => handleClick(`/chat/detail/${notification.contentObject.id}`)} />)}
-              {notification.typeNo >= 8 && notification.typeNo <= 10 && <a className="notification_aria_anker" onClick={() => handleClick(`/chat/detail/${notification.userFrom.nickname}`)} />}
-              {notification.typeNo >= 1 && notification.typeNo <= 7 && (
-                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
-                  {notification.contentObject.title}
+
+              {renderElement(
+                notification.typeName === NotificationType.Music,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/music/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
+              )}
+
+              {renderElement(
+                notification.typeName === NotificationType.Comic,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/comic/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
+              )}
+
+              {renderElement(
+                notification.typeName === NotificationType.Picture,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/picture/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
+              )}
+
+              {renderElement(
+                notification.typeName === NotificationType.Blog,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/blog/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
+              )}
+
+              {renderElement(
+                notification.typeName === NotificationType.Chat,
+                <div className="notification_aria_anker" onClick={() => handleClick(`/chat/detail/${notification.contentObject.id}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんが${notification.contentObject.title}を投稿しました`}>
+                    {notification.contentObject.title}
+                  </div>
+                </div>,
+              )}
+
+              {notification.typeName === NotificationType.Follow && (
+                <div className="notification_aria_anker" onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}にフォローされました`}>
+                    {notification.userFrom.nickname}にフォローされました
+                  </div>
                 </div>
               )}
-              {notification.typeNo === 7 && (
-                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}にフォローされました`}>
-                  {notification.userFrom.nickname}にフォローされました
+
+              {notification.typeName === NotificationType.Like && (
+                <div className="notification_aria_anker" onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.contentObject.text}が${notification.userFrom.nickname}さんにいいねされました`}>
+                    {notification.contentObject.text}が{notification.userFrom.nickname}さんにいいねされました
+                  </div>
                 </div>
               )}
-              {notification.typeNo === 8 && (
-                <div className="notification_aria_list_1" title={`${notification.contentObject.text}が${notification.userFrom.nickname}さんにいいねされました`}>
-                  {notification.contentObject.text}が{notification.userFrom.nickname}さんにいいねされました
+
+              {notification.typeName === NotificationType.Reply && (
+                <div className="notification_aria_anker" onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+                  <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんから返信がありました ${notification.contentObject.text}`}>
+                    {notification.contentObject.text}
+                  </div>
                 </div>
               )}
-              {notification.typeNo === 9 && (
-                <div className="notification_aria_list_1" title={`${notification.userFrom.nickname}さんから返信がありました ${notification.contentObject.text}`}>
-                  {notification.contentObject.text}
+
+              {notification.typeName === NotificationType.Views && (
+                <div className="notification_aria_anker" onClick={() => handleClick(`/userpage/${notification.userFrom.nickname}`)}>
+                  {readNotification(notification.contentObject.read, notification.contentObject.title)}
                 </div>
               )}
-              {(notification.typeNo === 10, readNotification(notification.contentObject.read, notification.contentObject.title))}
+
               <form method="POST">
                 <i title="閉じる" className="bi-x notification_aria_list_2" style={{ fontSize: '1.41em' }}></i>
               </form>
