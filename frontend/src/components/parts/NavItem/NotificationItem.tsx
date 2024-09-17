6c28dbb9f9d2f63bfa3665f71d51a0907ece663c
ANFINII <anfinii19870506@gmail.com>
2024-09-18 08:37:33 +0900

    feat NotificationItem

---
+++ frontend/src/components/parts/NavItem/NotificationItem.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router'
+import NavItem from 'components/parts/NavItem'
+import ExImage from '../ExImage'
+import IconCircle from '../Icon/Circle'
+
+interface Props {
+  avatar: string
+  nickname: string
+  isConfirmed: boolean
+  children?: React.ReactNode
+  onClick: () => void
+}
+
+export default function NotificationItem(props: Props): JSX.Element {
+  const { avatar, nickname, isConfirmed, children, onClick } = props
+
+  const router = useRouter()
+  const handleRouter = (url: string) => router.push(url)
+
+  return (
+    <NavItem className="drop_menu_notification">
+      <div className="notification_aria_list">
+        <div onClick={() => handleRouter(`/userpage/${nickname}`)}>
+          <ExImage src={avatar} title={nickname} className="profile_image" />
+        </div>
+        {!isConfirmed && <IconCircle size="6" className="svg-circle" />}
+        <div className="notification_aria_anker" onClick={onClick}>
+          {children}
+        </div>
+        <form method="POST">
+          <i title="閉じる" className="bi-x notification_aria_list_2" style={{ fontSize: '1.41em' }}></i>
+        </form>
+      </div>
+    </NavItem>
+  )
+}
