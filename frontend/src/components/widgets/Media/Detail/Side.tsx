38453ab211f363fbb0cc56db8cd86e5b9e023b19
ANFINII <anfinii19870506@gmail.com>
2024-10-06 21:37:00 +0900

    fix MediaDetailSide

---
+++ frontend/src/components/widgets/Media/Detail/Side.tsx
@@ -0,0 +1,31 @@
+import ExImage from 'components/parts/ExImage'
+import ContentTitle from 'components/widgets/Common/ContentTitle'
+
+interface Props {
+  href: string
+  title: string
+  imageUrl: string
+  nickname: string
+  read: number
+  totalLike: number
+  created: string
+}
+
+export default function MediaDetailSide(props: Props) {
+  const { href, title, imageUrl, nickname, read, totalLike, created } = props
+
+  return (
+    <section className="section_detail">
+      <div className="main_decolation">
+        <a href={href} className="content_side">
+          <figure className="content_size">
+            <ExImage src={imageUrl} className="radius_8" />
+          </figure>
+          <div className="author_space_side">
+            <ContentTitle title={title} nickname={nickname} read={read} totalLike={totalLike} created={created} />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
