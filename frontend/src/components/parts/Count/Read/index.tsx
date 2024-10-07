b0aa0a2da3def3c63de0cf76b5075d49e64fd64e
ANFINII <anfinii19870506@gmail.com>
2024-10-08 08:35:53 +0900

    fix CountRead

---
+++ frontend/src/components/parts/Count/Read/index.tsx
@@ -0,0 +1,19 @@
+import IconCaret from 'components/parts/Icon/Caret'
+import style from './Read.module.scss'
+
+interface Props {
+  read?: number
+}
+
+export default function CountRead(props: Props) {
+  const { read } = props
+
+  return (
+    <div className={style.count}>
+      <div className={style.icon}>
+        <IconCaret size="16" className="mr_8" />
+      </div>
+      <span>{read}</span>
+    </div>
+  )
+}
