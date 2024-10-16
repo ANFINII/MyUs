5822394739655fdf4ab59f373e242191a4c05a82
ANFINII <anfinii19870506@gmail.com>
2024-10-16 16:15:23 +0900

    enumの格納ディレクトリの修正

--- backend/apps/api/views/user.py
+++ backend/apps/api/views/user.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from apps.myus.models import SearchTag
 from apps.api.services.notification import get_notification, get_content_object
 from apps.api.services.user import get_user, get_follows, get_followers
-from apps.api.utils.functions.response import ApiResponse
+from apps.api.utils.enum.response import ApiResponse
 from apps.api.utils.functions.user import get_notification_user
 
 
@@ -14,7 +14,7 @@ class UserAPI(APIView):
     def get(self, request):
         user = get_user(request)
         if not user:
-            return ApiResponse.UNAUTHORIZED.run()
+            return ApiResponse.unauthorized.run()
 
         data = {
             'avatar': f'{DOMAIN_URL}{user.image()}' if user.image() else '',
@@ -30,7 +30,7 @@ class SearchTagAPI(APIView):
     def get(self, request):
         user = get_user(request)
         if not user:
-            return ApiResponse.UNAUTHORIZED.run()
+            return ApiResponse.unauthorized.run()
 
         search_tags = SearchTag.objects.filter(author=user).order_by('sequence')[:20]
 
@@ -42,7 +42,7 @@ class FollowAPI(APIView):
     def get(self, request):
         user = get_user(request)
         if not user:
-            return ApiResponse.UNAUTHORIZED.run()
+            return ApiResponse.unauthorized.run()
 
         search = request.query_params.get('search')
         data = get_follows(100, user, search)
@@ -53,7 +53,7 @@ class FollowerAPI(APIView):
     def get(self, request):
         user = get_user(request)
         if not user:
-            return ApiResponse.UNAUTHORIZED.run()
+            return ApiResponse.unauthorized.run()
 
         search = request.query_params.get('search')
         data = get_followers(100, user, search)
@@ -64,7 +64,7 @@ class NotificationAPI(APIView):
     def get(self, request):
         user = get_user(request)
         if not user:
-            return ApiResponse.UNAUTHORIZED.run()
+            return ApiResponse.unauthorized.run()
 
         notification = get_notification(user)
         data = {
