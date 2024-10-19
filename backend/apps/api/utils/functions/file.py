f3baf1bfc489e45863acc89d3bbc4dc92353da9c
ANFINII <anfinii19870506@gmail.com>
2024-10-19 15:56:34 +0900

    uploadパスの関数をまとめる

---
+++ backend/apps/api/utils/functions/file.py
@@ -0,0 +1,22 @@
+def user_image(instance, filename):
+    class_name = type(instance).__name__.lower()
+    return f'images/{class_name}/user_{instance.id}/{filename}'
+
+
+def image_upload(instance, filename):
+    class_name = type(instance).__name__.lower()
+    return f'images/{class_name}/user_{instance.author_id}/object_{instance.id}/{filename}'
+
+
+def video_upload(instance, filename):
+    class_name = type(instance).__name__.lower()
+    return f'videos/{class_name}/user_{instance.author_id}/object_{instance.id}/{filename}'
+
+
+def musics_upload(instance, filename):
+    return f'musics/user_{instance.author_id}/object_{instance.id}/{filename}'
+
+
+def comic_upload(instance, filename):
+    comic = instance.comic
+    return f'comics/user_{comic.author_id}/object_{comic.id}/{filename}'
