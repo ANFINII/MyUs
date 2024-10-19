1f1d6118bd79bf3cebafe339311e43f10e859f9b
An Okina <79322275+ANFINII@users.noreply.github.com>
2024-10-19 15:58:04 +0900

    models.pyをファイル分割する (#7)
    * uploadパスの関数をまとめる
    
    * 既存のmodelsファイルの削除
    
    * initファイルの追加
    
    * models.pyをファイル分割する

---
+++ backend/apps/myus/models/media.py
@@ -0,0 +1,366 @@
+from django.db import models
+from django.db.models import Q
+from django.contrib.contenttypes.fields import GenericRelation
+from django_quill.fields import QuillField
+from apps.myus.models.master import HashTag
+from apps.myus.models.base import MediaModel, MediaManager
+from apps.myus.models.user import User
+from apps.api.utils.functions.file import image_upload, video_upload, musics_upload, comic_upload
+
+
+# Video
+class VideoQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class VideoManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'comment')
+
+class Video(models.Model, MediaModel):
+    """Video"""
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=100)
+    content = models.TextField()
+    image   = models.ImageField(upload_to=image_upload)
+    video   = models.FileField(upload_to=video_upload)
+    convert = models.FileField(upload_to=video_upload)
+    comment = GenericRelation('Comment')
+    hashtag = models.ManyToManyField(HashTag, blank=True)
+    like    = models.ManyToManyField(User, related_name='video_like', blank=True)
+    read    = models.IntegerField(default=0)
+    publish = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = VideoManager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            image = self.image
+            video  = self.video
+            convert = self.convert
+            self.image = None
+            self.video  = None
+            self.convert = None
+            super().save(*args, **kwargs)
+            self.image = image
+            self.video  = video
+            self.convert = convert
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'video'
+        verbose_name_plural = '01 Video'
+
+
+# Music
+class MusicQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query) |
+                Q(lyric__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class MusicManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return MusicQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'comment')
+
+class Music(models.Model, MediaModel):
+    """Music"""
+    author   = models.ForeignKey(User, on_delete=models.CASCADE)
+    title    = models.CharField(max_length=100)
+    content  = models.TextField()
+    lyric    = models.TextField(blank=True)
+    music    = models.FileField(upload_to=musics_upload)
+    comment  = GenericRelation('Comment')
+    hashtag  = models.ManyToManyField(HashTag, blank=True)
+    like     = models.ManyToManyField(User, related_name='music_like', blank=True)
+    read     = models.IntegerField(default=0)
+    download = models.BooleanField(default=True)
+    publish  = models.BooleanField(default=True)
+    created  = models.DateTimeField(auto_now_add=True)
+    updated  = models.DateTimeField(auto_now=True)
+
+    objects = MusicManager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            music = self.music
+            self.music = None
+            super().save(*args, **kwargs)
+            self.music = music
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'music'
+        verbose_name_plural = '02 Music'
+
+
+# Comic
+class ComicQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class ComicManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return ComicQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'comment')
+
+class Comic(models.Model, MediaModel):
+    """Comic"""
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=100)
+    content = models.TextField()
+    image   = models.ImageField(upload_to=image_upload)
+    comment = GenericRelation('Comment')
+    hashtag = models.ManyToManyField(HashTag, blank=True)
+    like    = models.ManyToManyField(User, related_name='comic_like', blank=True)
+    read    = models.IntegerField(default=0)
+    publish = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = ComicManager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            image = self.image
+            self.image = None
+            super().save(*args, **kwargs)
+            self.image = image
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'comic'
+        verbose_name_plural = '03 Comic'
+
+
+class ComicPage(models.Model):
+    """ComicPage"""
+    comic = models.ForeignKey(Comic, on_delete=models.CASCADE, related_name='comic')
+    image = models.ImageField(upload_to=comic_upload)
+    sequence = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["comic", "sequence"], name="lesson_unique")
+        ]
+
+    class Meta:
+        db_table = 'comic_page'
+        verbose_name_plural = '03 ComicPage'
+
+
+# Picture
+class PictureQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class PictureManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return PictureQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'comment')
+
+class Picture(models.Model, MediaModel):
+    """Picture"""
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=100)
+    content = models.TextField()
+    image   = models.ImageField(upload_to=image_upload)
+    comment = GenericRelation('Comment')
+    hashtag = models.ManyToManyField(HashTag, blank=True)
+    like    = models.ManyToManyField(User, related_name='picture_like', blank=True)
+    read    = models.IntegerField(default=0)
+    publish = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = PictureManager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            image = self.image
+            self.image = None
+            super().save(*args, **kwargs)
+            self.image = image
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'picture'
+        verbose_name_plural = '04 Picture'
+
+
+# Blog
+class BlogQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query) |
+                Q(richtext__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class BlogManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return BlogQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'comment')
+
+class Blog(models.Model, MediaModel):
+    """Blog"""
+    author   = models.ForeignKey(User, on_delete=models.CASCADE)
+    title    = models.CharField(max_length=100)
+    content  = models.TextField()
+    richtext = models.TextField()
+    delta    = QuillField()
+    image    = models.ImageField(upload_to=image_upload)
+    comment  = GenericRelation('Comment')
+    hashtag  = models.ManyToManyField(HashTag, blank=True)
+    like     = models.ManyToManyField(User, related_name='blog_like', blank=True)
+    read     = models.IntegerField(default=0)
+    publish  = models.BooleanField(default=True)
+    created  = models.DateTimeField(auto_now_add=True)
+    updated  = models.DateTimeField(auto_now=True)
+
+    objects = BlogManager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            image = self.image
+            self.image = None
+            super().save(*args, **kwargs)
+            self.image = image
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'blog'
+        verbose_name_plural = '05 Blog'
+
+
+# Chat
+class ChatQuerySet(models.QuerySet):
+    def search(self, query=None):
+        qs = self
+        if query:
+            or_lookup = (
+                Q(title__icontains=query) |
+                Q(hashtag__jp_name__icontains=query) |
+                Q(author__nickname__icontains=query) |
+                Q(content__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
+class ChatManager(models.Manager, MediaManager):
+    def get_queryset(self):
+        return ChatQuerySet(self.model, using=self._db).select_related('author').prefetch_related('like', 'message')
+
+class Chat(models.Model):
+    """Chat"""
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=100)
+    content = models.TextField()
+    hashtag = models.ManyToManyField(HashTag, blank=True)
+    like    = models.ManyToManyField(User, related_name='chat_like', blank=True)
+    read    = models.IntegerField(default=0)
+    period  = models.DateField()
+    publish = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = ChatManager()
+
+    def __str__(self):
+        return self.title
+
+    def total_like(self):
+        return self.like.count()
+    total_like.short_description = 'like'
+
+    def thread_count(self):
+        return self.message.filter(parent__isnull=True).count()
+    thread_count.short_description = 'thread'
+
+    def joined_count(self):
+        return self.message.values_list('author').distinct().count()
+    joined_count.short_description = 'joined'
+
+    def score(self):
+        return int(self.read + self.like.count()*10 + self.read*self.like.count()/(self.read+1)*20)
+
+    class Meta:
+        db_table = 'chat'
+        verbose_name_plural = '06 Chat'
+
+
+# Todo
+class Todo(models.Model):
+    """Todo"""
+    priority_type = (('danger', '高'), ('success', '普通'), ('info', '低'))
+    progress_type = (('0', '未着手'), ('1', '進行中'), ('2', '完了'))
+    author   = models.ForeignKey(User, on_delete=models.CASCADE)
+    title    = models.CharField(max_length=100)
+    content  = models.TextField()
+    priority = models.CharField(max_length=10, choices=priority_type)
+    progress = models.CharField(max_length=10, choices=progress_type)
+    comment  = GenericRelation('Comment')
+    duedate  = models.DateField()
+    created  = models.DateTimeField(auto_now_add=True)
+    updated  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def comment_count(self):
+        return self.comment.count()
+    comment_count.short_description = 'comment'
+
+    class Meta:
+        db_table = 'todo'
+        verbose_name_plural = '07 ToDo'
---
+++ backend/apps/myus/models/user.py
@@ -0,0 +1,270 @@
+from datetime import date
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core.mail import send_mail
+from django.core.validators import RegexValidator
+from django.utils import timezone
+from apps.myus.models.master import PlanMaster
+from apps.api.utils.functions.file import user_image
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, nickname, password=None):
+        if not email:
+            raise ValueError('Users must have an email')
+        elif not username:
+            raise ValueError('Users must have an username')
+        elif not nickname:
+            raise ValueError('Users must have an nickname')
+        user = self.model(email=self.normalize_email(email), username=username, nickname=nickname)
+        user.set_password(password)
+        user.is_active=False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, nickname, password):
+        user = self.create_user(email, username, nickname, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    def get_queryset(self):
+        return super(UserManager,self).get_queryset().select_related('mypage')
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User"""
+    img         = '../static/img/user_icon.png'
+    avatar      = models.ImageField(upload_to=user_image, default=img, blank=True, null=True)
+    email       = models.EmailField(max_length=255, unique=True)
+    username    = models.CharField(max_length=20, unique=True)
+    nickname    = models.CharField(max_length=80, unique=True)
+    is_active   = models.BooleanField(default=True)
+    is_staff    = models.BooleanField(default=False)
+    last_login  = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'nickname']
+
+    def __str__(self):
+        return self.nickname
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def full_name(self):
+        return self.profile.last_name + ' ' + self.profile.first_name
+    full_name.short_description = 'name'
+
+    def year(self):
+        if self.birthday:
+            return self.profile.birthday.year
+
+    def month(self):
+        if self.birthday:
+            return self.profile.birthday.month
+
+    def day(self):
+        if self.birthday:
+            return self.profile.birthday.day
+
+    def age(self):
+        if self.profile:
+            birthday = self.profile.birthday
+            DAYS_IN_YEAR = 365.2425
+            return int((date.today() - birthday).days / DAYS_IN_YEAR)
+
+    def gender(self):
+        return self.profile.gender
+
+    def birthday(self):
+        return self.profile.birthday
+
+    def prefecture(self):
+        return self.profile.prefecture
+
+    def city(self):
+        return self.profile.city
+
+    def street(self):
+        return self.profile.street
+
+    def image(self):
+        if self.avatar:
+            return self.avatar.url
+
+    def banner(self):
+        if self.mypage.banner:
+            return self.mypage.banner.url
+
+    def plan(self):
+        return self.user_plan.plan.name
+
+    def plan_start_date(self):
+        return self.user_plan.start_date
+
+    def plan_end_date(self):
+        return self.user_plan.end_date
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            avatar = self.avatar
+            self.avatar = None
+            super().save(*args, **kwargs)
+            self.avatar = avatar
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'user'
+        verbose_name_plural = '001 User'
+        indexes = [
+            models.Index(fields=['email'], name='email_idx'),
+            models.Index(fields=['username'], name='username_idx'),
+            models.Index(fields=['nickname'], name='nickname_idx'),
+        ]
+
+
+class ProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(ProfileManager,self).get_queryset().select_related('user')
+
+class Profile(models.Model):
+    """Profile"""
+    gender_type  = (('0', '男性'), ('1', '女性'), ('2', '秘密'))
+    message      = '電話番号は090-1234-5678の形式で入力する必要があります。最大15桁まで入力できます'
+    phone_no     = RegexValidator(regex=r'\d{2,4}-?\d{2,4}-?\d{3,4}', message=message)
+    user         = models.OneToOneField(User, on_delete=models.CASCADE)
+    last_name    = models.CharField(max_length=50)
+    first_name   = models.CharField(max_length=50)
+    birthday     = models.DateField(blank=True, null=True)
+    gender       = models.CharField(choices=gender_type, max_length=1)
+    phone        = models.CharField(validators=[phone_no], max_length=15, blank=True)
+    country_code = models.CharField(max_length=255, default='JP')
+    postal_code  = models.CharField(max_length=255, blank=True)
+    prefecture   = models.CharField(max_length=255, blank=True)
+    city         = models.CharField(max_length=255, blank=True)
+    street       = models.CharField(max_length=255, blank=True)
+    introduction = models.TextField(blank=True)
+
+    objects = ProfileManager()
+
+    def __str__(self):
+        return self.user.nickname
+
+    class Meta:
+        db_table = 'user_profile'
+        verbose_name_plural = '001 profile'
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    """ユーザー作成時に空のProfileも作成する"""
+    if kwargs['created']:
+        Profile.objects.get_or_create(user=kwargs['instance'])
+
+
+class MyPageManager(models.Manager):
+    def get_queryset(self):
+        return super(MyPageManager,self).get_queryset().select_related('user')
+
+class MyPage(models.Model):
+    """MyPage"""
+    img             = '../static/img/MyUs_banner.png'
+    user            = models.OneToOneField(User, on_delete=models.CASCADE)
+    banner          = models.ImageField(upload_to=user_image, default=img, blank=True, null=True)
+    email           = models.EmailField(max_length=255, blank=True)
+    content         = models.TextField(blank=True)
+    follower_count  = models.IntegerField(verbose_name='follower', default=0)
+    following_count = models.IntegerField(verbose_name='follow', default=0)
+    tag_manager_id  = models.CharField(max_length=10, blank=True)
+    is_advertise    = models.BooleanField(default=True)
+
+    objects = MyPageManager()
+
+    def __str__(self):
+        return self.user.nickname
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            banner = self.banner
+            self.banner = None
+            super().save(*args, **kwargs)
+            self.banner = banner
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'user_mypage'
+        verbose_name_plural = '001 MyPage'
+
+@receiver(post_save, sender=User)
+def create_mypage(sender, **kwargs):
+    """ユーザー作成時に空のMyPageも作成する"""
+    if kwargs['created']:
+        MyPage.objects.get_or_create(user=kwargs['instance'])
+
+
+class UserNotification(models.Model):
+    """UserNotification"""
+    user       = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_video   = models.BooleanField(default=False)
+    is_music   = models.BooleanField(default=False)
+    is_comic   = models.BooleanField(default=False)
+    is_picture = models.BooleanField(default=False)
+    is_blog    = models.BooleanField(default=False)
+    is_chat    = models.BooleanField(default=False)
+    is_follow  = models.BooleanField(default=True)
+    is_reply   = models.BooleanField(default=True)
+    is_like    = models.BooleanField(default=True)
+    is_views   = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.nickname
+
+    class Meta:
+        db_table = 'user_notification'
+        verbose_name_plural = '001 通知設定'
+
+@receiver(post_save, sender=User)
+def create_user_notification(sender, **kwargs):
+    """ユーザー作成時に空のuser_notificationも作成する"""
+    if kwargs['created']:
+        UserNotification.objects.get_or_create(user=kwargs['instance'])
+
+
+class UserPlan(models.Model):
+    """UserPlan"""
+    user         = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_plan')
+    plan         = models.ForeignKey(PlanMaster, on_delete=models.CASCADE, default=1)
+    customer_id  = models.CharField(max_length=255)
+    subscription = models.CharField(max_length=255)
+    is_paid      = models.BooleanField(default=False)
+    start_date   = models.DateTimeField(blank=True, null=True)
+    end_date     = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.plan.name
+
+    class Meta:
+        db_table = 'user_plan'
+        verbose_name_plural = '001 UserPlan'
+
+@receiver(post_save, sender=User)
+def create_user_plan(sender, **kwargs):
+    """ユーザー作成時に空のuser_planも作成する"""
+    if kwargs['created']:
+        UserPlan.objects.get_or_create(user=kwargs['instance'])
