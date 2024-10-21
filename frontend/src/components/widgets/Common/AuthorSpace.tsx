8b2ae66ed19584e2a7d661a3a2a121888489b183
ANFINII <anfinii19870506@gmail.com>
2024-10-22 00:17:19 +0900

    fix AuthorSpace css

--- frontend/src/components/widgets/Common/AuthorSpace.tsx
+++ frontend/src/components/widgets/Common/AuthorSpace.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import ExImage from 'components/parts/ExImage'
+import style from './AuthorSpace.module.scss'
 
 interface Props {
   imageUrl: string
@@ -10,9 +11,9 @@ export default function AuthorSpace(props: Props) {
   const { imageUrl, nickname } = props
 
   return (
-    <object className="author_image_space">
+    <object className={style.author_image_space}>
       <Link href={`/userpage/${nickname}`}>
-        <ExImage src={imageUrl} title={nickname} className="profile_image" size="32" />
+        <ExImage src={imageUrl} title={nickname} className={style.profile_image} size="32" />
       </Link>
     </object>
   )
