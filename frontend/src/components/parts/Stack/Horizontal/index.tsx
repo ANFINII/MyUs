81f1066bd2196924e1a007bba6f283da7c040486
ANFINII <anfinii19870506@gmail.com>
2024-11-03 22:16:14 +0900

    Horizontal and Verticalを修正

--- frontend/src/components/parts/Stack/Horizontal/index.tsx
+++ frontend/src/components/parts/Stack/Horizontal/index.tsx
@@ -1,17 +1,19 @@
+import clsx from 'clsx'
 import styles from './Horizontal.module.scss'
 
 interface Props {
   gap?: string | number
   alignment?: 'start' | 'center' | 'end' | 'stretch'
   wrap?: boolean
+  className?: string
   children: React.ReactNode
 }
 
 export default function Horizontal(props: Props): JSX.Element {
-  const { gap, alignment, wrap, children } = props
+  const { gap, alignment, wrap, className, children } = props
 
   return (
-    <div className={styles.horizontal} style={{ gap: `${Number(gap) * 2}px`, justifyContent: alignment, flexWrap: wrap ? 'wrap' : 'nowrap' }}>
+    <div className={clsx(styles.horizontal, className)} style={{ gap: `${Number(gap) * 2}px`, justifyContent: alignment, flexWrap: wrap ? 'wrap' : 'nowrap' }}>
       {children}
     </div>
   )
--- frontend/src/components/parts/Stack/Vertical/index.tsx
+++ frontend/src/components/parts/Stack/Vertical/index.tsx
@@ -1,16 +1,18 @@
+import clsx from 'clsx'
 import styles from './Vertical.module.scss'
 
 interface Props {
   gap?: string | number
   alignment?: 'start' | 'center' | 'end' | 'stretch'
+  className?: string
   children: React.ReactNode
 }
 
 export default function Vertical(props: Props): JSX.Element {
-  const { gap, alignment, children } = props
+  const { gap, alignment, className, children } = props
 
   return (
-    <div className={styles.vertical} style={{ gap: `${Number(gap) * 2}px`, alignItems: alignment }}>
+    <div className={clsx(styles.vertical, className)} style={{ gap: `${Number(gap) * 2}px`, alignItems: alignment }}>
       {children}
     </div>
   )
