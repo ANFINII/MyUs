940aa32884c40535fd4b3f5d22f6ea0229147529
ANFINII <anfinii19870506@gmail.com>
2024-11-13 16:47:38 +0900

    MediaBaseContentを追加して修正

---
+++ frontend/src/components/widgets/Media/Content/Base.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx'
+import { Media } from 'types/internal/media'
+import { formatTimeAgo } from 'utils/functions/datetime'
+import IconCaret from 'components/parts/Icon/Caret'
+import IconHand from 'components/parts/Icon/Hand'
+import Horizontal from 'components/parts/Stack/Horizontal'
+import Vertical from 'components/parts/Stack/Vertical'
+import style from './MediaContent.module.scss'
+
+interface Props<Media> {
+  media: Media
+}
+
+export default function MediaBaseContent(props: Props<Media>) {
+  const { media } = props
+  const { title, read, like, created, author } = media
+  const { nickname } = author
+
+  return (
+    <Vertical gap="1">
+      <div title={title} className={style.media_title}>
+        {title}
+      </div>
+
+      <Vertical gap="2">
+        <div className={clsx(style.font, style.nickname)}>{nickname}</div>
+
+        <Horizontal gap="4">
+          <div className={style.font}>
+            <IconCaret size="14" className={style.margin} />
+            {read}
+          </div>
+          <div className={style.font}>
+            <IconHand size="14" type="off" className={style.margin} />
+            {like}
+          </div>
+        </Horizontal>
+
+        <time className={style.font}>{formatTimeAgo(created)}</time>
+      </Vertical>
+    </Vertical>
+  )
+}
--- frontend/src/components/widgets/Media/Content/index.tsx
+++ frontend/src/components/widgets/Media/Content/index.tsx
@@ -1,12 +1,8 @@
 import Link from 'next/link'
-import clsx from 'clsx'
 import { Media } from 'types/internal/media'
-import { formatTimeAgo } from 'utils/functions/datetime'
 import AuthorLink from 'components/parts/AuthorLink'
-import IconCaret from 'components/parts/Icon/Caret'
-import IconHand from 'components/parts/Icon/Hand'
 import Horizontal from 'components/parts/Stack/Horizontal'
-import Vertical from 'components/parts/Stack/Vertical'
+import MediaBaseContent from './Base'
 import style from './MediaContent.module.scss'
 
 interface Props<Media> {
@@ -16,7 +12,7 @@ interface Props<Media> {
 
 export default function MediaContent(props: Props<Media>) {
   const { href, media } = props
-  const { title, read, like, created, author } = media
+  const { author } = media
   const { avatar, nickname } = author
 
   return (
@@ -25,28 +21,7 @@ export default function MediaContent(props: Props<Media>) {
       <Link href={href} className={style.link}>
         <Horizontal gap="4" className="p_6">
           <div className='mr_36' />
-          <Vertical gap="1">
-            <div title={title} className={style.media_title}>
-              {title}
-            </div>
-
-            <Vertical gap="2">
-              <div className={clsx(style.font, style.nickname)}>{nickname}</div>
-
-              <Horizontal gap="4">
-                <div className={style.font}>
-                  <IconCaret size="14" className={style.margin} />
-                  {read}
-                </div>
-                <div className={style.font}>
-                  <IconHand size="14" type="off" className={style.margin} />
-                  {like}
-                </div>
-              </Horizontal>
-
-              <time className={style.font}>{formatTimeAgo(created)}</time>
-            </Vertical>
-          </Vertical>
+          <MediaBaseContent media={media} />
         </Horizontal>
       </Link>
     </div>
