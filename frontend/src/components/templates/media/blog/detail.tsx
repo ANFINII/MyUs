38cf998f7fc0ba64ecb53319ba31535787d22e25
ANFINII <anfinii19870506@gmail.com>
2024-11-13 16:56:37 +0900

    MediaSideBlogを追加して修正

--- frontend/src/components/templates/media/blog/detail.tsx
+++ frontend/src/components/templates/media/blog/detail.tsx
@@ -4,7 +4,7 @@ import Divide from 'components/parts/Divide'
 import FormatHtml from 'components/parts/FormatHtml'
 import MediaDetail from 'components/widgets/Media/Detail'
 import MediaDetailCommon from 'components/widgets/Media/Detail/Common'
-import MediaDetailSide from 'components/widgets/Media/Side'
+import MediaSideBlog from 'components/widgets/Media/Side'
 
 interface Props {
   data: BlogDetailOut
@@ -27,18 +27,7 @@ export default function BlogDetail(props: Props) {
             <MediaDetailCommon title={title} content={content} read={read} like={like} commentCount={commentCount} created={created} author={author} user={user} />
           </div>
           <div className="article_detail_section_2">
-            {list.map((data) => (
-              <MediaDetailSide
-                key={data.id}
-                href={`/media/blog/${data.id}`}
-                title={data.title}
-                imageUrl={data.image}
-                nickname={data.author.nickname}
-                read={data.read}
-                like={data.like}
-                created={created}
-              />
-            ))}
+            {list.map((media) => (<MediaSideBlog key={media.id} media={media} />))}
           </div>
         </div>
       </MediaDetail>
--- frontend/src/components/widgets/Media/Side/index.tsx
+++ frontend/src/components/widgets/Media/Side/index.tsx
@@ -1,37 +1,26 @@
+import Link from 'next/link'
+import { Blog } from 'types/internal/media'
 import ExImage from 'components/parts/ExImage'
-// import MediaContent from 'components/widgets/Common/MediaContent'
-// import ContentTitle from 'components/widgets/Common/MediaContent'
+import Horizontal from 'components/parts/Stack/Horizontal'
+import MediaBaseContent from 'components/widgets/Media/Content/Base'
 import style from './Side.module.scss'
 
 interface Props {
-  href: string
-  title: string
-  imageUrl: string
-  nickname: string
-  read: number
-  like: number
-  created: string
+  media: Blog
 }
 
-export default function MediaDetailSide(props: Props) {
-  // const { href, title, imageUrl, nickname, read, like, created } = props
-  const { href, imageUrl } = props
+export default function MediaSideBlog(props: Props) {
+  const { media } = props
+  const { id, image } = media
 
   return (
     <section className={style.media_detail_side}>
-      <div className="main_decolation">
-        <a href={href} className={style.media_content}>
-          <figure className="content_size">
-            <ExImage src={imageUrl} className="radius_8" />
-          </figure>
-          {/* <div className="author_space_side">
-            <ContentTitle title={title} nickname={nickname} read={read} like={like} created={created} />
-          </div> */}
-          {/* <div className={style.media_content}>
-            <MediaContent href={`/media/music/${id}`} media={media} />
-          </div> */}
-        </a>
-      </div>
+      <Link href={`/media/blog/${id}`} className={style.link}>
+        <Horizontal gap="4">
+          <ExImage src={image} width="192" height="108" className="radius_6" />
+          <MediaBaseContent media={media} />
+        </Horizontal>
+      </Link>
     </section>
   )
 }
