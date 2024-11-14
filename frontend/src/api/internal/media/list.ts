90d7915d2c435bc1a3e2780a2014235590ef54bc
ANFINII <anfinii19870506@gmail.com>
2024-11-14 14:32:57 +0900

    cookieHeaderを追加

--- frontend/src/api/internal/media/list.ts
+++ frontend/src/api/internal/media/list.ts
@@ -5,49 +5,49 @@ import { Req } from 'types/global'
 import { MediaHome, Video, Music, Comic, Picture, Blog, Chat, Todo } from 'types/internal/media'
 import { apiHome, apiVideos, apiMusics, apiComics, apiPictures, apiBlogs, apiChats, apiTodos } from 'api/uri'
 
-export const getHome = async (search?: string): Promise<MediaHome> => {
-  const res = await apiClient.get(apiHome, { params: { search } })
+export const getHome = async (search?: string, req?: Req): Promise<MediaHome> => {
+  const res = await apiClient.get(apiHome, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getVideos = async (search?: string): Promise<Video[]> => {
-  const res = await apiClient.get(apiVideos, { params: { search } })
+export const getVideos = async (search?: string, req?: Req): Promise<Video[]> => {
+  const res = await apiClient.get(apiVideos, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getMusics = async (search?: string): Promise<Music[]> => {
-  const res = await apiClient.get(apiMusics, { params: { search } })
+export const getMusics = async (search?: string, req?: Req): Promise<Music[]> => {
+  const res = await apiClient.get(apiMusics, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getComics = async (search?: string): Promise<Comic[]> => {
-  const res = await apiClient.get(apiComics, { params: { search } })
+export const getComics = async (search?: string, req?: Req): Promise<Comic[]> => {
+  const res = await apiClient.get(apiComics, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getPictures = async (search?: string): Promise<Picture[]> => {
-  const res = await apiClient.get(apiPictures, { params: { search } })
+export const getPictures = async (search?: string, req?: Req): Promise<Picture[]> => {
+  const res = await apiClient.get(apiPictures, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getBlogs = async (search?: string): Promise<Blog[]> => {
-  const res = await apiClient.get(apiBlogs, { params: { search } })
+export const getBlogs = async (search?: string, req?: Req): Promise<Blog[]> => {
+  const res = await apiClient.get(apiBlogs, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getChats = async (search?: string): Promise<Chat[]> => {
-  const res = await apiClient.get(apiChats, { params: { search } })
+export const getChats = async (search?: string, req?: Req): Promise<Chat[]> => {
+  const res = await apiClient.get(apiChats, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.InternalServerError) throw Error
   return res.data
 }
 
-export const getTodos = async (req?: Req, search?: string): Promise<Todo[]> => {
+export const getTodos = async (search?: string, req?: Req): Promise<Todo[]> => {
   const res = await apiClient.get(apiTodos, cookieHeader(req, { search }))
   if (res.status >= HttpStatusCode.BadRequest) return []
   return res.data
