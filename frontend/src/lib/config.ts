7dabe8842087f17a1ad1aad6e2a2383eb6372f92
ANFINII <anfinii19870506@gmail.com>
2024-11-23 21:40:26 +0900

    cookieHeaderの修正

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,11 +1,11 @@
-import { Req, Query, Headers, Config } from 'types/global'
+import { Req, Query, Header, Config } from 'types/global'
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const AUTH_NO_PATHS: string[] = ['/account/login', '/account/signup']
 
-export const cookieHeader = (req?: Req, query?: Query, header?: Headers): Config => {
+export const cookieHeader = (req?: Req, query?: Query, header?: Header): Config => {
   const cookie = req?.headers.cookie
-  const headers = { ...(cookie && { cookie }), ...header }
+  const headers = { cookie, ...(header && { ...header }) }
   const config = { headers, ...(query && { params: query }) }
   return config
 }
--- frontend/src/types/global/index.d.ts
+++ frontend/src/types/global/index.d.ts
@@ -2,13 +2,17 @@ import { NextApiRequestCookies } from 'next/dist/server/api-utils'
 import { IncomingMessage } from 'http'
 
 export type Query = Record<string, string | undefined>
-export type Headers = Record<string, string>
+export type Header = Record<string, string>
 
 export interface Req extends IncomingMessage {
   cookies: NextApiRequestCookies
 }
 
+export const CookieHeaders: {
+  cookie: NextApiRequestCookies
+}
+
 export interface Config {
   params?: Query | undefined
-  headers: NextApiRequestCookies
+  headers: CookieHeaders
 }
