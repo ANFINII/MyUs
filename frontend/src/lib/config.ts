6f75a9f6ecc6aa484f0b1e2794e96655f92e2718
ANFINII <anfinii19870506@gmail.com>
2024-11-28 16:41:39 +0900

    fix cookieHeader

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -3,8 +3,9 @@ import { Req, Query, Config } from 'types/global'
 export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const AUTH_NO_PATHS: string[] = ['/account/login', '/account/signup']
 
-export const cookieHeader = (req?: Req, query?: Query): Config => {
-  const cookie = req?.headers.cookie || ''
+export const cookieHeader = (req?: Req, query?: Query): Config | undefined => {
+  const cookie = req?.headers.cookie
+  if (!cookie) return undefined
   const headers = { cookie }
   const config = { headers, ...(query && { params: query }) }
   return config
