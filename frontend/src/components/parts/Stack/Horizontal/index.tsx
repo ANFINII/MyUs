abd1bd02a26efb62bf0ae926ed4eb4dbc2d37776
ANFINII <anfinii19870506@gmail.com>
2024-12-05 19:40:04 +0900

    Horizontalの修正

--- frontend/src/components/parts/Stack/Horizontal/index.tsx
+++ frontend/src/components/parts/Stack/Horizontal/index.tsx
@@ -3,17 +3,17 @@ import style from './Horizontal.module.scss'
 
 interface Props {
   gap?: string | number
-  align?: 'start' | 'center' | 'end' | 'stretch'
+  align?: 'start' | 'center' | 'end' | 'around' | 'between' | 'evenly'
   wrap?: boolean
   className?: string
   children: React.ReactNode
 }
 
 export default function Horizontal(props: Props): JSX.Element {
-  const { gap = 0, align, wrap, className, children } = props
+  const { gap = 0, align = 'start', wrap, className, children } = props
 
   return (
-    <div className={clsx(style.horizontal, className)} style={{ gap: `${Number(gap) * 2}px`, justifyContent: align, flexWrap: wrap ? 'wrap' : 'nowrap' }}>
+    <div className={clsx(style.horizontal, style[`align_${align}`], className)} style={{ gap: `${Number(gap) * 2}px`, flexWrap: wrap ? 'wrap' : 'nowrap' }}>
       {children}
     </div>
   )
