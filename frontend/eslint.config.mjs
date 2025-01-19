59c63d2b17a0ff561b8aea202e7a2c1ff5688923
ANFINII <anfinii19870506@gmail.com>
2025-01-20 01:41:54 +0900

    eslintの修正

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,31 +1,34 @@
-import pluginJs from '@eslint/js'
+import js from '@eslint/js'
 import stylisticTs from '@stylistic/eslint-plugin-ts'
 import tsEsLintPlugin from '@typescript-eslint/eslint-plugin'
 import importPlugin from 'eslint-plugin-import'
 import pluginReact from 'eslint-plugin-react'
-import pluginReactHooks from 'eslint-plugin-react-hooks'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import unusedPlugin from 'eslint-plugin-unused-imports'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,d.ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
     plugins: {
       react: pluginReact,
-      'react-hooks': pluginReactHooks,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       'import': importPlugin,
       'unused-imports': unusedPlugin,
       '@typescript-eslint': tsEsLintPlugin,
       '@stylistic/ts': stylisticTs,
     },
-  },
-  {
     rules: {
       semi: ['error', 'never'], // セミコロンを不要に
       indent: ['error', 2], // インデントを2スペースで強制
