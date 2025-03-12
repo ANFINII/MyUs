24ea89088c747363fccdc101e38e94670038ad2b
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-03-12 13:21:07 +0900

    アカウント系の画面のリファクタ (#19)

--- frontend/src/components/templates/setting/withdrawal/confirm.tsx
+++ frontend/src/components/templates/setting/withdrawal/confirm.tsx
@@ -4,28 +4,32 @@ import Main from 'components/layout/Main'
 import Button from 'components/parts/Button'
 import LoginError from 'components/parts/Error/Login'
 import Input from 'components/parts/Input'
+import Vertical from 'components/parts/Stack/Vertical'
 
 export default function WithdrawalConfirm(): JSX.Element {
   const router = useRouter()
+  const handleBack = () => router.push('/setting/withdrawal')
 
   return (
     <Main title="退会処理">
       <LoginError>
         <article className="article_pass">
           <form method="POST" action="" className="form_account">
-            {/* <form method="POST" action="{% url 'app:withdrawal' %}" className="form_account"> */}
-            <p className="red mb_24">本当に退会しますか？</p>
+            <Vertical gap="8">
+              <p className="red">本当に退会しますか？</p>
+              <Input type="password" name="password2" placeholder="パスワード" minLength={8} maxLength={16} required />
+            </Vertical>
 
-            <Input type="password" name="password2" placeholder="パスワード" minLength={8} maxLength={16} className="mb_16" required />
-
-            <Button color="red" size="l" name="退会する" className="w_full mv_24" />
-
-            <Button color="blue" size="l" name="戻る" className="w_full mb_24" onClick={() => router.push('/setting/withdrawal')} />
+            <div className='mv_40'>
+              <Vertical gap="12">
+                <Button color="red" size="l" name="退会する" />
+                <Button color="blue" size="l" name="戻る" onClick={handleBack} />
+              </Vertical>
+            </div>
           </form>
-
-          <Footer />
         </article>
       </LoginError>
+      <Footer />
     </Main>
   )
 }
