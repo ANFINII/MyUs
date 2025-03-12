f7b2a0e76499534f693256f3d0fa1da1537b44a6
ANFINII <anfinii19870506@gmail.com>
2025-03-12 12:35:46 +0900

    サインアップ画面のリファクタ

--- frontend/src/components/templates/account/signup.tsx
+++ frontend/src/components/templates/account/signup.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import { useRouter } from 'next/router'
 import { SignupIn } from 'types/internal/auth'
 import { postSignup } from 'api/internal/auth'
@@ -11,6 +11,7 @@ import Main from 'components/layout/Main'
 import Button from 'components/parts/Button'
 import Input from 'components/parts/Input'
 import Select from 'components/parts/Input/Select'
+import Vertical from 'components/parts/Stack/Vertical'
 
 const initSignup: SignupIn = {
   email: '',
@@ -36,16 +37,8 @@ export default function Signup(): JSX.Element {
   const [values, setValues] = useState<SignupIn>(initSignup)
 
   const handleBack = () => router.push('/account/login')
-  const handleEmail = (email: string) => setValues({ ...values, email })
-  const handleUsername = (username: string) => setValues({ ...values, username })
-  const handleNickname = (nickname: string) => setValues({ ...values, nickname })
-  const handleLastName = (lastName: string) => setValues({ ...values, lastName })
-  const handleFirstName = (firstName: string) => setValues({ ...values, firstName })
-  const handlePassword1 = (password1: string) => setValues({ ...values, password1 })
-  const handlePassword2 = (password2: string) => setValues({ ...values, password2 })
-  const handleYear = (year: string) => setValues({ ...values, year: Number(year) })
-  const handleMonth = (month: string) => setValues({ ...values, month: Number(month) })
-  const handleDay = (day: string) => setValues({ ...values, day: Number(day) })
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => setValues({ ...values, [e.target.name]: e.target.value })
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => setValues({ ...values, [e.target.name]: e.target.value })
   const handleGender = (e: React.ChangeEvent<HTMLInputElement>) => setValues({ ...values, gender: e.target.value as Gender })
 
   const handleSubmit = async () => {
@@ -78,39 +71,50 @@ export default function Signup(): JSX.Element {
             </ul>
           )}
 
-          <p className="mb_8">名前</p>
-          <div className="name_group">
-            <Input type="text" placeholder="姓" maxLength={30} required={isRequired} onChange={handleLastName} />
-            <Input type="text" placeholder="名" maxLength={30} required={isRequired} onChange={handleFirstName} />
-          </div>
-
-          <Input type="text" placeholder="ユーザー名(英数字)" maxLength={20} className="mb_16" required={isRequired} onChange={handleUsername} />
-          <Input type="text" placeholder="投稿者名" maxLength={80} className="mb_16" required={isRequired} onChange={handleNickname} />
-          <Input type="email" placeholder="メールアドレス" maxLength={255} className="mb_16" required={isRequired} onChange={handleEmail} />
-          <Input type="password" placeholder="パスワード(英数字8~16文字)" minLength={8} maxLength={16} className="mb_16" required={isRequired} onChange={handlePassword1} />
-          <Input type="password" placeholder="パスワード(確認用)" minLength={8} maxLength={16} className="mb_16" required={isRequired} onChange={handlePassword2} />
-
-          <p className="mb_8">生年月日</p>
-          <div className="birthday_group mb_24">
-            <Select value={values.year} options={selectDate.years} placeholder="年" onChange={handleYear} />
-            <Select value={values.month} options={selectDate.months} placeholder="月" onChange={handleMonth} />
-            <Select value={values.day} options={selectDate.days} placeholder="日" onChange={handleDay} />
-          </div>
-
-          <p className="mb_8">性別</p>
-          <div className="td_gender mb_40">
-            {genders.map((gender) => (
-              <div key={gender.value} className="gender_radio">
-                <input type="radio" value={gender.value} checked={gender.value === values.gender} id={`gender_${gender.value}`} onChange={handleGender} />
-                <label htmlFor={`gender_${gender.value}`}>{gender.key}</label>
+          <Vertical gap="8" >
+            <div>
+              <p className="mb_8">名前</p>
+              <div className="name_group">
+                <Input name="lastName" placeholder="姓" maxLength={30} required={isRequired} onChange={handleInput} />
+                <Input name="firstName" placeholder="名" maxLength={30} required={isRequired} onChange={handleInput} />
               </div>
-            ))}
-          </div>
+            </div>
 
-          <Button color="green" size="l" name="アカウント登録" type="submit" className="w_full mb_24" loading={isLoading} onClick={handleSubmit} />
-          <Button color="blue" size="l" name="戻る" className="w_full mb_24" onClick={handleBack} />
+            <Input name="username" placeholder="ユーザー名(英数字)" maxLength={20} required={isRequired} onChange={handleInput} />
+            <Input name="nickname" placeholder="投稿者名" maxLength={80} required={isRequired} onChange={handleInput} />
+            <Input type="email" name="email" placeholder="メールアドレス" maxLength={255} required={isRequired} onChange={handleInput} />
+            <Input type="password" name="password1" placeholder="パスワード(英数字8~16文字)" minLength={8} maxLength={16} required={isRequired} onChange={handleInput} />
+            <Input type="password" name="password2" placeholder="パスワード(確認用)" minLength={8} maxLength={16} required={isRequired} onChange={handleInput} />
+
+            <div>
+              <p className="mb_8">生年月日</p>
+              <div className="birthday_group mb_24">
+                <Select name="year" value={values.year} options={selectDate.years} placeholder="年" onChange={handleSelect} />
+                <Select name="month" value={values.month} options={selectDate.months} placeholder="月" onChange={handleSelect} />
+                <Select name="day" value={values.day} options={selectDate.days} placeholder="日" onChange={handleSelect} />
+              </div>
+            </div>
+
+            <div>
+              <p className="mb_8">性別</p>
+              <div className="td_gender">
+                {genders.map((gender) => (
+                  <div key={gender.value} className="gender_radio">
+                    <input type="radio" value={gender.value} checked={gender.value === values.gender} id={`gender_${gender.value}`} onChange={handleGender} />
+                    <label htmlFor={`gender_${gender.value}`}>{gender.key}</label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </Vertical>
+
+          <div className='mv_40'>
+            <Vertical gap="12">
+              <Button color="green" size="l" name="アカウント登録" type="submit" loading={isLoading} onClick={handleSubmit} />
+              <Button color="blue" size="l" name="戻る" onClick={handleBack} />
+            </Vertical>
+          </div>
         </form>
-        <Footer />
       </article>
       <Footer />
     </Main>
