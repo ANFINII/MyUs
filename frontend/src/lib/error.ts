c8088596ad8af66a57179e3feee934aaa5d7ead7
ANFINII <anfinii19870506@gmail.com>
2025-03-20 22:47:16 +0900

    Result型の実装

---
+++ frontend/src/lib/error.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios'
+import { Result, ok, err } from 'neverthrow'
+
+export interface ApiError {
+  status: number
+  message?: string
+}
+
+export type ApiOut<T> = Result<T, ApiError>
+
+export const apiOut = async <T>(apiCall: Promise<AxiosResponse<T>>): Promise<ApiOut<T>> => {
+  try {
+    const res = await apiCall
+    return ok(res.data)
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      return err({ status: e.response?.status ?? 500, message: e.message })
+    }
+    return err({ status: 500, message: 'Unknown Error' })
+  }
+}
