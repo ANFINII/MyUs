b0d28e9ac02877933f819cb7fcdb50a7355f7858
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-04-24 11:51:24 +0900

    jestを導入 (#45)

---
+++ frontend/jest.config.ts
@@ -0,0 +1,13 @@
+import nextJest from 'next/jest.js'
+import type { Config } from 'jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: { '@/(.*)$': '<rootDir>/src/$1' },
+}
+
+export default createJestConfig(config)
