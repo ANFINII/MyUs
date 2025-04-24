ed8787394cfacf6854aafac0e5470dcd058c113b
ANFINII <anfinii19870506@gmail.com>
2025-04-25 00:59:52 +0900

    SettingProfileをresult型に修正

--- frontend/src/api/internal/setting.ts
+++ frontend/src/api/internal/setting.ts
@@ -1,22 +1,19 @@
 import { HttpStatusCode } from 'axios'
 import { apiClient, apiFormClient } from 'lib/axios/internal'
 import { cookieHeader } from 'lib/config'
+import { ApiOut, apiOut } from 'lib/error'
 import { Req } from 'types/global'
 import { ProfileIn, ProfileOut, MypageIn, MypageOut, UserNotificationIn, UserNotificationOut } from 'types/internal/auth'
 import { MessageOut } from 'types/internal/other'
 import { apiSettingProfile, apiSettingMypage, apiSettingNotification } from 'api/uri'
 import { camelSnake } from 'utils/functions/convertCase'
 
-export const getSettingProfile = async (req?: Req): Promise<ProfileOut> => {
-  const res = await apiClient.get(apiSettingProfile, cookieHeader(req))
-  if (res.status >= HttpStatusCode.InternalServerError) throw Error
-  return res.data
+export const getSettingProfile = async (req?: Req): Promise<ApiOut<ProfileOut>> => {
+  return await apiOut(apiClient.get(apiSettingProfile, cookieHeader(req)))
 }
 
-export const putSettingProfile = async (request: ProfileIn): Promise<MessageOut | void> => {
-  const res = await apiFormClient.put(apiSettingProfile, camelSnake(request))
-  if (res.status >= HttpStatusCode.InternalServerError) throw Error
-  return res.data
+export const putSettingProfile = async (request: ProfileIn): Promise<ApiOut<MessageOut>> => {
+  return await apiOut(apiClient.put(apiSettingProfile, camelSnake(request)))
 }
 
 export const getSettingMypage = async (req?: Req): Promise<MypageOut> => {
