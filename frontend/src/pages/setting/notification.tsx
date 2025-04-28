5b8e004c1e276cd83ee93dfc314bf910400b3e44
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-04-28 13:14:40 +0900

    SettingNotificationPageをresult型に修正 (#54)

--- frontend/src/pages/setting/notification.tsx
+++ frontend/src/pages/setting/notification.tsx
@@ -2,18 +2,26 @@ import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { UserNotificationOut } from 'types/internal/auth'
 import { getSettingNotification } from 'api/internal/setting'
+import ErrorCheck from 'components/widgets/ErrorCheck'
 import SettingNotification from 'components/templates/setting/notification'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale, req }) => {
   const translations = await serverSideTranslations(locale as string, ['common'])
-  const userNotification = await getSettingNotification(req)
+  const ret = await getSettingNotification(req)
+  if (ret.isErr()) return { props: { status: ret.error.status } }
+  const userNotification = ret.value
   return { props: { userNotification, ...translations } }
 }
 
 interface Props {
+  status: number
   userNotification: UserNotificationOut
 }
 
 export default function SettingNotificationPage(props: Props): JSX.Element {
-  return <SettingNotification {...props} />
+  return (
+    <ErrorCheck status={props.status}>
+      <SettingNotification {...props} />
+    </ErrorCheck>
+  )
 }
