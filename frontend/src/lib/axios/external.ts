22d604c1cc54015931fa6c79a75c2af8b52a187b
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-05-06 22:14:03 +0900

    【front】axios externalをリファクタ (#60)
    * axios externalをリファクタ
    
    * tsconfig refactor
    
    * 修正
    
    * 修正

--- frontend/src/lib/axios/external.ts
+++ frontend/src/lib/axios/external.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosInstance } from 'axios'
 import { axiosInterceptor } from '.'
 
-const axiosInstance = (baseURL: string) => {
+const axiosInstance = (baseURL: string, contentType: string) => {
   const client: AxiosInstance = axios.create({
     baseURL,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': contentType },
     timeout: 5000,
   })
   axiosInterceptor(client)
   return client
 }
 
-export const apiAddressClient = axiosInstance('https://zipcloud.ibsnet.co.jp')
+export const apiAddressClient = axiosInstance('https://zipcloud.ibsnet.co.jp', 'application/json')
