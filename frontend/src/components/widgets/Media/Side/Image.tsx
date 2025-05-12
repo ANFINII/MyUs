0a59c7d9bd0a5c707fa270840372620a56b144ce
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-05-12 13:59:26 +0900

    MediaSideImageに修正 (#63)

--- frontend/src/components/widgets/Media/Side/Image.tsx
+++ frontend/src/components/widgets/Media/Side/Image.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link'
-import { Blog } from 'types/internal/media'
+import { Media } from 'types/internal/media'
 import ExImage from 'components/parts/ExImage'
 import Horizontal from 'components/parts/Stack/Horizontal'
 import MediaBaseContent from 'components/widgets/Media/Content/Base'
 import style from './Side.module.scss'
 
 interface Props {
-  media: Blog
+  href: string
+  src: string
+  media: Media
 }
 
-export default function MediaSideBlog(props: Props): JSX.Element {
-  const { media } = props
-  const { id, image } = media
+export default function MediaSideImage(props: Props): JSX.Element {
+  const { href, src, media } = props
 
   return (
     <section>
-      <Link href={`/media/blog/${id}`} className={style.link}>
+      <Link href={href} className={style.link}>
         <Horizontal gap="4">
-          <ExImage src={image} width="192" height="100" className={style.thumbnail} />
+          <ExImage src={src} width="192" height="100" className={style.thumbnail} />
           <MediaBaseContent media={media} />
         </Horizontal>
       </Link>
