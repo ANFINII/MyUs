e0f533db87069d2518cce32b9ccb01dd203a3e39
ANFINII <anfinii19870506@gmail.com>
2025-05-15 01:08:15 +0900

    CommnetDomainのgetをlist[CommentData]に返すように修正

--- myus/api/domain/comment.py
+++ myus/api/domain/comment.py
@@ -1,13 +1,12 @@
-from dataclasses import dataclass
-from enum import Enum, auto
 from django.db.models import Exists, OuterRef, Prefetch
 from api.models.comment import Comment
-
+from api.types.data.comment import CommentData, ReplyData
+from api.utils.functions.user import get_author
 
 
 class CommnetDomain:
     @staticmethod
-    def get(media_type: str, object_id: int, author_id: int) -> list[Comment]:
+    def get(media_type: str, object_id: int, author_id: int) -> list[CommentData]:
         filter_obj = dict(media_type=media_type, object_id=object_id)
         subquery = Comment.objects.filter(id=OuterRef("pk"), like__id=author_id, **filter_obj)
 
@@ -19,4 +18,21 @@ class CommnetDomain:
             .annotate(is_comment_like=Exists(subquery))
         )
 
-        return objs
+        return get_comment_data(objs)
+
+
+def get_comment_data(comments: list[Comment]) -> list[CommentData]:
+    data = [
+        CommentData(
+            id=str(c.id),
+            text=c.text,
+            created=c.created,
+            updated=c.updated,
+            replys=[
+                ReplyData(id=str(r.id), text=r.text, created=r.created, updated=r.updated, author=get_author(r.author))
+                for r in c.reply.all()
+            ],
+            author=get_author(c.author),
+        ) for c in comments
+    ]
+    return data
