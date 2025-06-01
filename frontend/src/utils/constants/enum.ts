d9f1decc22585d2480f1a18bf3fe78d9cd83bfe8
ANFINII <anfinii19870506@gmail.com>
2025-06-01 10:57:21 +0900

    genderMapの実装方法をリファクタ

--- frontend/src/utils/constants/enum.ts
+++ frontend/src/utils/constants/enum.ts
@@ -1,7 +1,13 @@
-export enum Gender {
+export enum GenderType {
   Male = '0',
   Female = '1',
   Secret = '2',
+}
+
+export enum GenderView {
+  Male = '男性',
+  Female = '女性',
+  Secret = '秘密',
 }
 
 export enum NotificationType {
