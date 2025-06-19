a9a5508d3402ad0890fa94aa613f783c5c1a1b15
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-06-19 12:00:44 +0900

    コメントcomponents用のアイコンを追加 (#107)

---
+++ frontend/src/components/parts/Icon/Dots.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  size: string
+  className?: string
+}
+
+export default function IconDots(props: Props): JSX.Element {
+  const { size, className = '' } = props
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" width={size} height={size} className={className}>
+      <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+    </svg>
+  )
+}
