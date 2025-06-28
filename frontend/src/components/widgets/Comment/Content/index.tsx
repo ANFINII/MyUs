396d3f2d8c57a5c1db36497caa7c007a63068853
ANFINII <anfinii19870506@gmail.com>
2025-06-28 13:03:57 +0900

    CommentContentにActionListを追加

--- frontend/src/components/widgets/Comment/Content/index.tsx
+++ frontend/src/components/widgets/Comment/Content/index.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Comment } from 'types/internal/comment'
 import { formatDatetime } from 'utils/functions/datetime'
 import AvatarLink from 'components/parts/Avatar/Link'
 import IconDots from 'components/parts/Icon/Dots'
+import IconEdit from 'components/parts/Icon/Edit'
+import IconTrash from 'components/parts/Icon/Trash'
 import Horizontal from 'components/parts/Stack/Horizontal'
+import ActionList from 'components/widgets/Comment/ActionList'
 import CommentDeleteModal from 'components/widgets/Modal/CommentDelete'
 import style from './CommentContent.module.scss'
 
@@ -16,9 +19,23 @@ export default function CommentContent(props: Props): JSX.Element {
   const { comment, nickname } = props
   const { author, created, text } = comment
 
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isMenu, setIsMenu] = useState<boolean>(false)
+  const [isModal, setIsModal] = useState<boolean>(false)
+  const actionButtonRef = useRef<HTMLButtonElement>(null)
 
-  const handleModal = () => setIsOpen(!isOpen)
+  const handleMenu = () => setIsMenu(!isMenu)
+  const handleModal = () => setIsModal(!isModal)
+
+  const handleEdit = () => {
+    // TODO: Implement edit functionality
+  }
+
+  const handleDelete = () => setIsModal(true)
+
+  const actionItems = [
+    { icon: <IconEdit size="16" />, label: '編集', onClick: handleEdit },
+    { icon: <IconTrash size="16" />, label: '削除', onClick: handleDelete, danger: true },
+  ]
 
   return (
     <>
@@ -34,20 +51,17 @@ export default function CommentContent(props: Props): JSX.Element {
           </div>
         </Horizontal>
         {comment.author.nickname === nickname && (
-          <>
-            <button className={style.edit_button} onClick={handleModal}>
+          <div className={style.action_wrap}>
+            <button ref={actionButtonRef} className={style.action_button} onClick={handleMenu}>
               <IconDots size="18" />
             </button>
-            {/* <div className={style.edit_button}>
-              <span className={style.edit_button_update}>編集</span>
-              <span className={style.edit_button_delete} onClick={handleModal}>
-                削除
-              </span>
-            </div> */}
-          </>
+            <div className={style.action_list}>
+              <ActionList triggerRef={actionButtonRef} open={isMenu} onClose={handleMenu} items={actionItems} />
+            </div>
+          </div>
         )}
       </Horizontal>
-      <CommentDeleteModal open={isOpen} onClose={handleModal} onAction={() => {}} comment={comment} />
+      <CommentDeleteModal open={isModal} onClose={handleModal} onAction={() => {}} comment={comment} />
     </>
   )
 }
