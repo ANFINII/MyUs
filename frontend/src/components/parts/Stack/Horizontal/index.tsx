0685f23cf3dee0274c4a01be5f202673e3385ff0
ANFINII <anfinii19870506@gmail.com>
2025-07-03 11:29:05 +0900

    Verticalのpropsをリファクタ

--- frontend/src/components/parts/Stack/Horizontal/index.tsx
+++ frontend/src/components/parts/Stack/Horizontal/index.tsx
@@ -4,17 +4,17 @@ import style from './Horizontal.module.scss'
 interface Props {
   gap?: string
   justify?: 'start' | 'center' | 'end' | 'around' | 'between' | 'evenly'
-  wrap?: boolean
   full?: boolean
+  wrap?: boolean
   className?: string
   children: React.ReactNode
 }
 
 export default function Horizontal(props: Props): JSX.Element {
-  const { gap = '0', justify = 'start', wrap = false, full, className, children } = props
+  const { gap = '0', justify = 'start', full, wrap = false, className, children } = props
 
   return (
-    <div className={clsx(style.horizontal, style[`justify_${justify}`], wrap && style.wrap, full && style.full, className)} style={{ gap: `${Number(gap) * 2}px` }}>
+    <div className={clsx(style.horizontal, style[justify], full && style.full, wrap && style.wrap, className)} style={{ gap: `${Number(gap) * 2}px` }}>
       {children}
     </div>
   )
