cd6df327a811a39274378c5398c834c37bf627d8
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-07-03 11:33:56 +0900

    Verticalのpropsをリファクタ (#133)

--- frontend/src/components/parts/Stack/Vertical/index.tsx
+++ frontend/src/components/parts/Stack/Vertical/index.tsx
@@ -2,17 +2,19 @@ import clsx from 'clsx'
 import style from './Vertical.module.scss'
 
 interface Props {
-  gap?: string | number
-  align?: 'start' | 'center' | 'end' | 'around' | 'between' | 'evenly'
+  gap?: string
+  align?: 'start' | 'center' | 'end' | 'baseline' | 'stretch'
+  full?: boolean
+  wrap?: boolean
   className?: string
   children: React.ReactNode
 }
 
 export default function Vertical(props: Props): JSX.Element {
-  const { gap = 0, align, className, children } = props
+  const { gap = '0', align = 'stretch', full, wrap = false, className, children } = props
 
   return (
-    <div className={clsx(style.vertical, style[`align_${align}`], className)} style={{ gap: `${Number(gap) * 2}px`, alignItems: align }}>
+    <div className={clsx(style.vertical, style[align], full && style.full, wrap && style.wrap, className)} style={{ gap: `${Number(gap) * 2}px` }}>
       {children}
     </div>
   )
