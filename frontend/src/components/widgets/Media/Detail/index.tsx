0fadcd23a6aa9bb712b1df010741b5d5f5f7d2d8
ANFINII <anfinii19870506@gmail.com>
2025-07-05 23:26:06 +0900

    media_detail不要なcssを削除

--- frontend/src/components/widgets/Media/Detail/index.tsx
+++ frontend/src/components/widgets/Media/Detail/index.tsx
@@ -1,5 +1,3 @@
-import style from './MediaDetail.module.scss'
-
 interface Props {
   publish: boolean
   children: React.ReactNode
@@ -9,7 +7,7 @@ export default function MediaDetail(props: Props): JSX.Element {
   const { publish, children } = props
 
   return (
-    <article className={style.media_detail}>
+    <article>
       {publish ? <>{children}</> : <h2 className="unpublished">非公開に設定されてます!</h2>}
     </article>
   )
