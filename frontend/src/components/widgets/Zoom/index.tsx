485d29e2f40c121326fddbe2a343e132c3230dd6
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-07-09 21:36:02 +0900

    Zoom componentsを修正 (#157)

--- frontend/src/components/widgets/Zoom/index.tsx
+++ frontend/src/components/widgets/Zoom/index.tsx
@@ -1,20 +1,23 @@
+import clsx from 'clsx'
 import IconCaret from 'components/parts/Icon/Caret'
 import style from './Zoom.module.scss'
 
 interface Props {
   isView: boolean
   onView: () => void
+  size?: 's' | 'm' | 'l'
+  content: string
 }
 
 export default function Zoom(props: Props): JSX.Element {
-  const { isView, onView } = props
+  const { isView, onView, size = 'm', content } = props
 
   return (
-    <label className={style.zoom} onClick={onView}>
+    <label className={clsx(style.zoom, isView && style.active, style[size])} onClick={onView}>
       <div className={style.icon}>
         <IconCaret size="16" type={isView ? 'down' : 'right'} />
       </div>
-      <p className="us_none">{isView ? '縮小表示' : '拡大表示'}</p>
+      <p className="us_none">{content}</p>
     </label>
   )
 }
