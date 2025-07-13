2b547ef584d2388b3aaaeb9031fa41d620d5f38c
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-07-13 19:18:27 +0900

    capitalizeを実装 (#167)

--- frontend/src/utils/functions/common.ts
+++ frontend/src/utils/functions/common.ts
@@ -5,3 +5,8 @@ export const isActive = (isbool: boolean) => (isbool ? 'active' : '')
 export const searchParams = (query: ParsedUrlQuery): { search?: string } => ({
   search: typeof query.search === 'string' && query.search ? query.search : undefined,
 })
+
+export const capitalize = (str: string): string => {
+  if (!str) return str
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
