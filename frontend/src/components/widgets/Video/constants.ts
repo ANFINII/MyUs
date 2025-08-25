49be75f99458ae2b1bc899e84b5e3c300a6ecc41
ANFINII <anfinii19870506@gmail.com>
2025-08-26 01:26:07 +0900

    修正

---
+++ frontend/src/components/widgets/Video/constants.ts
@@ -0,0 +1,7 @@
+export const PLAYBACK_RATES = [2, 1.75, 1.5, 1.25, 1, 0.75, 0.5]
+export const MENU_DELAY_MS = 100
+export const MENU_UPDATE_DELAY_MS = 10
+export const SPEED_MENU = '速度'
+export const QUALITY_MENU = '画質'
+export const STANDARD_SPEED = '標準'
+export const AUTO_QUALITY = '自動'
