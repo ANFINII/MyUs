7eb2573a3a96a0f4a79fae9dcde4ece244623e2d
An Okina <79322275+ANFINII@users.noreply.github.com>
2025-10-30 23:28:44 +0900

    SearchTagDomainの実装 (#291)

---
+++ myus/api/domain/serach_tag.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import Enum
+from api.models.user import User
+from api.utils.functions.index import set_attr
+from api.models.users import SearchTag
+
+
+class SortType(Enum):
+    CREATED = "date_joined"
+    NICKNAME = "nickname"
+
+
+@dataclass(frozen=True, slots=True)
+class SortOption:
+    is_asc: bool = False
+    sort_type: SortType = SortType.CREATED
+
+
+class SearchTagDomain:
+    @classmethod
+    def get(cls, author_id: int) -> list[SearchTag]:
+        return SearchTag.objects.filter(author_id=author_id).order_by("sequence")[:20]
+
+    @classmethod
+    def create(cls, **kwargs) -> SearchTag:
+       return SearchTag.objects.create(**kwargs)
+
+    @classmethod
+    def update(cls, search_tag: SearchTag, **kwargs) -> None:
+        if not kwargs:
+            return
+
+        [set_attr(search_tag, key, value) for key, value in kwargs.items()]
+        SearchTag.save(update_fields=list[str](kwargs.keys()))
