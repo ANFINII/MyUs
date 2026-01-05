2743e6190f883a9fb49088983802964f5cf51166
An Okina <79322275+ANFINII@users.noreply.github.com>
2026-01-05 19:53:57 +0900

    mediaのcreateのパスを修正 (#325)

--- frontend/src/api/internal/media/create.ts
+++ frontend/src/api/internal/media/create.ts
@@ -2,28 +2,28 @@ import { apiClient } from 'lib/axios/internal'
 import { apiOut, ApiOut } from 'lib/error'
 import { Video, Music, Comic, Picture, Blog, Chat } from 'types/internal/media'
 import { VideoIn, MusicIn, ComicIn, PictureIn, BlogIn, ChatIn } from 'types/internal/media'
-import { apiVideoCreate, apiMusicCreate, apiComicCreate, apiPictureCreate, apiBlogCreate, apiChatCreate } from 'api/uri'
+import { apiVideos, apiMusics, apiComics, apiPictures, apiBlogs, apiChats } from 'api/uri'
 
 export const postVideoCreate = async (request: VideoIn): Promise<ApiOut<Video>> => {
-  return await apiOut(apiClient('form').post(apiVideoCreate, request))
+  return await apiOut(apiClient('form').post(apiVideos, request))
 }
 
 export const postMusicCreate = async (request: MusicIn): Promise<ApiOut<Music>> => {
-  return await apiOut(apiClient('form').post(apiMusicCreate, request))
+  return await apiOut(apiClient('form').post(apiMusics, request))
 }
 
 export const postComicCreate = async (request: ComicIn): Promise<ApiOut<Comic>> => {
-  return await apiOut(apiClient('form').post(apiComicCreate, request))
+  return await apiOut(apiClient('form').post(apiComics, request))
 }
 
 export const postPictureCreate = async (request: PictureIn): Promise<ApiOut<Picture>> => {
-  return await apiOut(apiClient('form').post(apiPictureCreate, request))
+  return await apiOut(apiClient('form').post(apiPictures, request))
 }
 
 export const postBlogCreate = async (request: BlogIn): Promise<ApiOut<Blog>> => {
-  return await apiOut(apiClient('form').post(apiBlogCreate, request))
+  return await apiOut(apiClient('form').post(apiBlogs, request))
 }
 
 export const postChatCreate = async (request: ChatIn): Promise<ApiOut<Chat>> => {
-  return await apiOut(apiClient('form').post(apiChatCreate, request))
+  return await apiOut(apiClient('form').post(apiChats, request))
 }
