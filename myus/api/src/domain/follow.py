b78471679f6419aa57d33afa5f4b24656ee4fbf3
ANFINII <anfinii19870506@gmail.com>
2026-01-14 17:57:35 +0900

    修正

--- myus/api/src/domain/follow.py
+++ myus/api/src/domain/follow.py
@@ -9,15 +9,10 @@ from api.utils.functions.index import set_attr
 from api.utils.functions.search import get_q_list
 
 
-class FollowType(Enum):
-    FOLLOWS = "follows"
-    FOLLOWERS = "followers"
-
-
 @dataclass(frozen=True, slots=True)
 class FilterOption:
-    follow_type: FollowType = FollowType.FOLLOWS
-    user_id: int = 0
+    follower_id: int = 0
+    following_id: int = 0
     search: str = ""
 
 
@@ -41,29 +36,36 @@ class FollowDomain:
 
     @classmethod
     def get_ids(cls, filter: FilterOption, sort: SortOption, limit: int | None = None) -> list[int]:
-        qs = Follow.objects.filter(is_follow=True)
-        if filter.follow_type == FollowType.FOLLOWS:
-            qs = qs.filter(follower_id=filter.user_id)
-            if filter.search:
-                q_list = get_q_list(filter.search)
-                query = reduce(and_, [
-                    Q(following__nickname__icontains=q) |
-                    Q(following__profile__introduction__icontains=q) for q in q_list
-                ])
-                qs = qs.filter(query)
-        if filter.follow_type == FollowType.FOLLOWERS:
-            qs = qs.filter(following_id=filter.user_id)
-            if filter.search:
-                q_list = get_q_list(filter.search)
-                query = reduce(and_, [
-                    Q(follower__nickname__icontains=q) |
-                    Q(follower__profile__introduction__icontains=q) for q in q_list
-                ])
-                qs = qs.filter(query)
+        q_list: list[Q] = []
+
+        if filter.follower_id:
+            q_list.append(Q(follower_id=filter.follower_id))
+        if filter.following_id:
+            q_list.append(Q(following_id=filter.following_id))
+
+        is_pair = filter.follower_id and filter.following_id
+        if not is_pair:
+            q_list.append(Q(is_follow=True))
+
+        if filter.search and filter.follower_id and not is_pair:
+            search_list = get_q_list(filter.search)
+            query = reduce(and_, [
+                Q(following__nickname__icontains=q) |
+                Q(following__profile__introduction__icontains=q) for q in search_list
+            ])
+            q_list.append(query)
+
+        if filter.search and filter.following_id and not is_pair:
+            search_list = get_q_list(filter.search)
+            query = reduce(and_, [
+                Q(follower__nickname__icontains=q) |
+                Q(follower__profile__introduction__icontains=q) for q in search_list
+            ])
+            q_list.append(query)
 
         field_name = sort.sort_type.value
         order_by_key = field_name if sort.is_asc else f"-{field_name}"
-        qs = qs.order_by(order_by_key)
+        qs = Follow.objects.filter(*q_list).order_by(order_by_key)
 
         if limit:
             qs = qs[:limit]
@@ -94,8 +96,8 @@ class FollowDomain:
     @classmethod
     def count(cls, filter: FilterOption) -> int:
         qs = Follow.objects.filter(is_follow=True)
-        if filter.follow_type == FollowType.FOLLOWS:
-            qs = qs.filter(follower_id=filter.user_id)
-        else:
-            qs = qs.filter(following_id=filter.user_id)
+        if filter.follower_id:
+            qs = qs.filter(follower_id=filter.follower_id)
+        if filter.following_id:
+            qs = qs.filter(following_id=filter.following_id)
         return qs.count()
