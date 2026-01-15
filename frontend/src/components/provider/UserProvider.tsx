95879b1ab4ab73684838ce80c6482072edf5831a
An Okina <79322275+ANFINII@users.noreply.github.com>
2026-01-15 21:05:53 +0900

    Avatarの更新を修正 (#333)

--- frontend/src/components/provider/UserProvider.tsx
+++ frontend/src/components/provider/UserProvider.tsx
@@ -4,8 +4,8 @@ import { getUser } from 'api/internal/user'
 
 export interface UserContextType {
   user: UserMe
+  resetUser: () => void
   updateUser: () => Promise<void>
-  resetUser: () => Promise<void>
 }
 
 export const UserContext = createContext<UserContextType | undefined>(undefined)
@@ -21,19 +21,18 @@ export function UserProvider(props: Props): React.JSX.Element {
 
   const [user, setUser] = useState<UserMe>(initUser)
 
-  const resetUser = async () => setUser(initUser)
+  const resetUser = useCallback(() => setUser(initUser), [])
 
   const updateUser = useCallback(async () => {
     const ret = await getUser()
     if (ret.isErr()) return resetUser()
     setUser(ret.value)
-  }, [])
+  }, [resetUser])
 
   useEffect(() => {
-    const fetchUser = async () => updateUser()
-    fetchUser()
+    updateUser()
   }, [updateUser])
 
-  const value = { user, updateUser, resetUser }
+  const value = { user, resetUser, updateUser }
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
