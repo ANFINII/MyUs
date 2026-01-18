52b50d34fe870ced360b3da6856fc8187bb40b90
ANFINII <anfinii19870506@gmail.com>
2026-01-18 13:29:30 +0900

    修正

---
+++ myus/api/db/models/channel.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django_ulid.models import ulid
+from api.db.models.user import User
+from api.utils.functions.file import channel_image
+
+
+class Channel(models.Model):
+    """Channel"""
+    avater      = "../static/img/channel_icon.png"
+    id          = models.BigAutoField(primary_key=True)
+    ulid        = models.CharField(max_length=26, unique=True, editable=False, default=ulid.new)
+    owner       = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    avatar      = models.ImageField(upload_to=channel_image, default=avater, blank=True)
+    name        = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_default  = models.BooleanField(default=False)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "channel"
+        verbose_name_plural = "Channel"
