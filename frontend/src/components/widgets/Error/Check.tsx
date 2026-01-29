e184b2b3eb83722dc06089860a37b42edda98818
An Okina <79322275+ANFINII@users.noreply.github.com>
2026-01-29 17:57:50 +0900

    ErrorCheckをリファクタ (#362)

--- frontend/src/components/widgets/Error/Check.tsx
+++ frontend/src/components/widgets/Error/Check.tsx
@@ -1,6 +1,7 @@
-import Custom404 from 'components/widgets/Error/404'
-import Custom500 from 'components/widgets/Error/500'
+import Custom404 from 'components/widgets/Error/Custom404'
+import Custom500 from 'components/widgets/Error/Custom500'
 import Unauthorized from 'components/widgets/Error/Unauthorized'
+import Unexpected from './Unexpected'
 
 interface Props {
   status: number
@@ -11,7 +12,8 @@ export default function ErrorCheck(props: Props): React.JSX.Element {
   const { status, children } = props
 
   if (status >= 500) return <Custom500 />
-  if (status >= 404) return <Custom404 />
+  if (status === 404) return <Custom404 />
   if (status === 401) return <Unauthorized />
+  if (status >= 400) return <Unexpected />
   return <>{children}</>
 }
--- frontend/src/components/widgets/Error/Unexpected.tsx
+++ frontend/src/components/widgets/Error/Unexpected.tsx
@@ -2,7 +2,7 @@ import Footer from 'components/layout/Footer'
 import Main from 'components/layout/Main'
 import BackError from 'components/parts/Error/Back'
 
-export default function Error(): React.JSX.Element {
+export default function Unexpected(): React.JSX.Element {
   return (
     <Main title="Unexpected Error">
       <BackError content="予期せぬエラーが発生しました" />
