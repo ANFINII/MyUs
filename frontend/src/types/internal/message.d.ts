afc3afe1255b53d0fb3d324bf7f5c50db5804b90
ANFINII <anfinii19870506@gmail.com>
2026-04-20 09:39:51 +0900

    修正

--- frontend/src/types/internal/message.d.ts
+++ frontend/src/types/internal/message.d.ts
@@ -1,16 +1,5 @@
 import { Author } from 'types/internal/user'
 
-export interface MessageCreateIn {
-  chatUlid: string
-  text: string
-  parentUlid?: string
-}
-
-export interface MessageUpdateIn {
-  chatUlid: string
-  text: string
-}
-
 export interface ChatMessage {
   ulid: string
   text: string
@@ -22,4 +11,15 @@ export interface ChatMessage {
 
 export interface ChatReply extends ChatMessage {
   parentUlid: string
+}
+
+export interface MessageCreateIn {
+  chatUlid: string
+  text: string
+  parentUlid?: string
+}
+
+export interface MessageUpdateIn {
+  chatUlid: string
+  text: string
 }
