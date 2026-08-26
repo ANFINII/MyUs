4e9f8a48ac4851d51c8f5d1d0215d27b00ce5cbf
ANFINII <anfinii19870506@gmail.com>
2026-08-26 17:27:33 +0900

    修正

--- myus/app/ai/analytics.py
+++ myus/app/ai/analytics.py
@@ -7,7 +7,7 @@ import pandas as pd
 # DB情報の設定
 con = mysql.connector.connect(
     host = os.environ["MYSQL_HOST"],
-    port = int(os.environ["MYSQL_PORT"]),
+    port = os.environ["MYSQL_PORT"],
     user = os.environ["MYSQL_USER"],
     password = os.environ["MYSQL_PASSWORD"],
     database = os.environ["MYSQL_DATABASE"],
